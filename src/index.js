ad18086cfa8e0ad28791847a9ff558e3492ec41b
Star Noob <van.kol@mail.ru>
2023-05-10 23:37:02 +0300

    added id generator, fixed item unique code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
-import {createElement} from './utils.js';
+import { createRoot } from 'react-dom/client';
 import App from './app.js';
 import Store from './store.js';
+import { createElement, nextId } from './utils.js';
+
+const list = [
+  {code: 1, title: 'Название элемента'},
+  {code: 2, title: 'Некий объект'},
+  {code: 3, title: 'Заголовок'},
+  {code: 4, title: 'Очень длинное название элемента из семи слов'},
+  {code: 5, title: 'Запись'},
+  {code: 6, title: 'Шестая запись'},
+  {code: 7, title: 'Седьмая запись'},
+];
+
+// может здесь это не очень, но счетчику так или иначе гдето надо задать текущее значение, или вызвать n раз
+for (let i = 0; i < list.length; i++)
+  list[i].code = nextId();
 
 const store = new Store({
-  list: [
-    {code: 1, title: 'Название элемента'},
-    {code: 2, title: 'Некий объект'},
-    {code: 3, title: 'Заголовок'},
-    {code: 4, title: 'Очень длинное название элемента из семи слов'},
-    {code: 5, title: 'Запись'},
-    {code: 6, title: 'Шестая запись'},
-    {code: 7, title: 'Седьмая запись'},
-  ]
+  list
 });
 
 const root = createRoot(document.getElementById('root'));
