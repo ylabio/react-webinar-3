49f3abfc09089337f4fb9086d112e70205518ebc
Alexandr Gusletsov <chegeadaun@yandex.ru>
2023-05-11 01:04:33 +0300

    1st commit

--- src/utils.js
+++ src/utils.js
@@ -2,27 +2,37 @@ const propNames = new Set(['id', 'className', 'textContent', 'onclick']);
 
 /**
  * Создание элемента со свойствами и вложенными элементами
+ * @param id {number} id тега
  * @param name {String} Название HTML тега
  * @param props {Object} Свойства и атрибуты элемента
  * @param children {...Node} Вложенные элементы
  * @returns {HTMLElement}
  */
 export function createElement(name, props = {}, ...children) {
-  const element = document.createElement(name);
+	const element = document.createElement(name);
 
-  // Назначение свойств и атрибутов
-  for (const name of Object.keys(props)) {
-    if (propNames.has(name)) {
-      element[name] = props[name];
-    } else {
-      element.setAttribute(name, props[name]);
-    }
-  }
+	// Назначение свойств и атрибутов
+	for (const name of Object.keys(props)) {
+		if (propNames.has(name)) {
+			element[name] = props[name];
+		} else {
+			element.setAttribute(name, props[name]);
+		}
+	}
 
-  // Вставка вложенных элементов
-  for (const child of children) {
-    element.append(child);
-  }
+	// Вставка вложенных элементов
+	for (const child of children) {
+		element.append(child);
+	}
 
-  return element;
+	return element;
+}
+
+export const generateRandomString = () => {
+	return Math.random().toString(36).substring(2, 15);
+}
+
+export function sklonenie(number, txt) {
+	var cases = [2, 0, 1, 1, 1, 2];
+	return txt[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
 }
