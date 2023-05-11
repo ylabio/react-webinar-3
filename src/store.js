c6a9432f7c77875bc28dc5ae727022f8b30d8a4f
heavylad <babay.pavel@gmail.com>
2023-05-11 22:46:39 +0300

    first completed homework - fix 1

--- src/store.js
+++ src/store.js
@@ -83,10 +83,9 @@ class Store {
       ...this.state,
       list: this.state.list.map((item) => {
         const selected = item.code === code && !item.selected;
-        const selectСounter =
-          selected && !item.selected
-            ? (item.selectСounter += 1)
-            : item.selectСounter;
+        const selectСounter = selected
+          ? (item.selectСounter += 1)
+          : item.selectСounter;
         return { ...item, selected, selectСounter };
       }),
     });
