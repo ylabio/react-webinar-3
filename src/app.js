edc4e51287becc837ec00d323ee9bacfdf504bb3
IvanE <sportsmenq@mail.ru>
2023-05-11 19:52:15 +0500

    add homework_1

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createElement} from './utils.js';
 import './styles.css';
+import plural from 'plural-ru';
 
 /**
  * Приложение
@@ -16,24 +17,28 @@ function App({store}) {
       <div className='App-head'>
         <h1>Приложение на чистом JS</h1>
       </div>
-      <div className='App-controls'>
+      <div className='App-controls' data-code={list.length}>
         <button onClick={() => store.addItem()}>Добавить</button>
       </div>
       <div className='App-center'>
         <div className='List'>{
           list.map(item =>
-            <div key={item.code} className='List-item'>
-              <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                   onClick={() => store.selectItem(item.code)}>
-                <div className='Item-code'>{item.code}</div>
-                <div className='Item-title'>{item.title}</div>
-                <div className='Item-actions'>
-                  <button onClick={() => store.deleteItem(item.code)}>
-                    Удалить
-                  </button>
+              <div key={item.code} className='List-item'>
+                <div className={'Item' + (item.selected  ? ' Item_selected' : '')}
+                     onClick={() => store.selectItem(item.code)}>
+                  <div className='Item-code'>{item.code}</div>
+                  <div className='Item-title'>{item.title}</div>
+                  <div className='Item-text'>
+                      {item.count === 0 ? '' :
+                          `Выделяли  ${plural(item.count, '%d раз', '%d раза', '%d раз' )} `}
+                  </div>
+                  <div className='Item-actions'>
+                    <button onClick={() => store.deleteItem(item.code)}>
+                      Удалить
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
           )}
         </div>
       </div>
--- src/store.js
+++ src/store.js
@@ -2,78 +2,93 @@
  * Хранилище состояния приложения
  */
 class Store {
-  constructor(initState = {}) {
-    this.state = initState;
-    this.listeners = []; // Слушатели изменений состояния
-  }
-
-  /**
-   * Подписка слушателя на изменения состояния
-   * @param listener {Function}
-   * @returns {Function} Функция отписки
-   */
-  subscribe(listener) {
-    this.listeners.push(listener);
-    // Возвращается функция для удаления добавленного слушателя
-    return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
+    constructor(initState = {}) {
+        this.state = initState;
+        this.listeners = []; // Слушатели изменений состояния
+        this.newCode = !this.state.list.length ? 0 : this.state.list.length; // Новый код
     }
-  }
 
-  /**
-   * Выбор состояния
-   * @returns {Object}
-   */
-  getState() {
-    return this.state;
-  }
-
-  /**
-   * Установка состояния
-   * @param newState {Object}
-   */
-  setState(newState) {
-    this.state = newState;
-    // Вызываем всех слушателей
-    for (const listener of this.listeners) listener();
-  }
-
-  /**
-   * Добавление новой записи
-   */
-  addItem() {
-    this.setState({
-      ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
-    })
-  };
-
-  /**
-   * Удаление записи по коду
-   * @param code
-   */
-  deleteItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
-
-  /**
-   * Выделение записи по коду
-   * @param code
-   */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
-          item.selected = !item.selected;
+    /**
+     * Подписка слушателя на изменения состояния
+     * @param listener {Function}
+     * @returns {Function} Функция отписки
+     */
+    subscribe(listener) {
+        this.listeners.push(listener);
+        // Возвращается функция для удаления добавленного слушателя
+        return () => {
+            this.listeners = this.listeners.filter(item => item !== listener);
         }
-        return item;
-      })
-    })
-  }
+    }
+
+
+    /**
+     * Выбор состояния
+     * @returns {Object}
+     */
+    getState() {
+        return this.state;
+    }
+
+    /**
+     * Установка состояния
+     * @param newState {Object}
+     */
+    setState(newState) {
+        this.state = newState;
+        // Вызываем всех слушателей
+        for (const listener of this.listeners) listener();
+    }
+
+    /**
+     * Генерация уникального кода
+     */
+    codeGenerator() {
+        return this.newCode++;
+    }
+
+    /**
+     * Добавление новой записи
+     */
+    addItem() {
+        this.codeGenerator();
+        this.setState({
+            ...this.state,
+            list: [...this.state.list, {code: this.newCode, title: 'Новая запись', count: 0}]
+        })
+    };
+
+    /**
+     * Удаление записи по коду
+     * @param code
+     */
+    deleteItem(code) {
+        this.setState({
+            ...this.state,
+            list: this.state.list.filter(item => item.code !== code)
+        })
+    };
+
+    /**
+     * Выделение записи по коду
+     * @param code
+     */
+    selectItem(code) {
+        this.setState({
+            ...this.state,
+            list: this.state.list.map(item => {
+                if (item.code === code) {
+                    item.selected = !item.selected;
+                    // Инкрементирование выделений
+                    !!item.selected ? item.count++ : '';
+                } else {
+                    // Сброс выделений записей
+                    item.selected = false;
+                }
+                return item;
+            })
+        })
+    }
 }
 
 export default Store;
