7baf85e4d1f8f38e9e6f4991530ae736b3f6ce0c
millhakeem <ctrlhakeem@yandex.ru>
2023-05-11 14:37:46 +0700

    Выполнил задания

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { createElement } from './utils.js';
+import {createRoot} from 'react-dom/client';
+import {createElement} from './utils.js';
 import App from './app.js';
 import Store from './store.js';
 
 const store = new Store({
-	list: [
-		{ code: 1, title: 'Название элемента', selectedCounter: 0 },
-		{ code: 2, title: 'Некий объект', selectedCounter: 0 },
-		{ code: 3, title: 'Заголовок', selectedCounter: 0 },
-		{ code: 4, title: 'Очень длинное название элемента из семи слов', selectedCounter: 0 },
-		{ code: 5, title: 'Запись', selectedCounter: 0 },
-		{ code: 6, title: 'Шестая запись', selectedCounter: 0 },
-		{ code: 7, title: 'Седьмая запись', selectedCounter: 0 },
-	],
-	codeCounter: 8,
+  list: [
+    {code: 1, title: 'Название элемента'},
+    {code: 2, title: 'Некий объект'},
+    {code: 3, title: 'Заголовок'},
+    {code: 4, title: 'Очень длинное название элемента из семи слов'},
+    {code: 5, title: 'Запись'},
+    {code: 6, title: 'Шестая запись'},
+    {code: 7, title: 'Седьмая запись'},
+  ]
 });
 
 const root = createRoot(document.getElementById('root'));
 
 store.subscribe(() => {
-	root.render(<App store={store} />);
+  root.render(<App store={store}/>);
 });
 
 // Первый рендер приложения
-root.render(<App store={store} />);
+root.render(<App store={store}/>);
