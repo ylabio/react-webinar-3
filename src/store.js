3dd4ab832ebcc02ae4d376dd9ab034c3d30f2bde
millhakeem <ctrlhakeem@yandex.ru>
2023-05-11 14:19:59 +0700

    Выполнил задания

--- src/store.js
+++ src/store.js
@@ -2,78 +2,95 @@
  * Хранилище состояния приложения
  */
 class Store {
-  constructor(initState = {}) {
-    this.state = initState;
-    this.listeners = []; // Слушатели изменений состояния
-  }
+	constructor(initState = {}) {
+		this.state = initState;
+		this.listeners = []; // Слушатели изменений состояния
+	}
 
-  /**
-   * Подписка слушателя на изменения состояния
-   * @param listener {Function}
-   * @returns {Function} Функция отписки
-   */
-  subscribe(listener) {
-    this.listeners.push(listener);
-    // Возвращается функция для удаления добавленного слушателя
-    return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    }
-  }
+	/**
+	 * Подписка слушателя на изменения состояния
+	 * @param listener {Function}
+	 * @returns {Function} Функция отписки
+	 */
+	subscribe(listener) {
+		this.listeners.push(listener);
+		// Возвращается функция для удаления добавленного слушателя
+		return () => {
+			this.listeners = this.listeners.filter((item) => item !== listener);
+		};
+	}
 
-  /**
-   * Выбор состояния
-   * @returns {Object}
-   */
-  getState() {
-    return this.state;
-  }
+	/**
+	 * Выбор состояния
+	 * @returns {Object}
+	 */
+	getState() {
+		return this.state;
+	}
 
-  /**
-   * Установка состояния
-   * @param newState {Object}
-   */
-  setState(newState) {
-    this.state = newState;
-    // Вызываем всех слушателей
-    for (const listener of this.listeners) listener();
-  }
+	/**
+	 * Установка состояния
+	 * @param newState {Object}
+	 */
+	setState(newState) {
+		this.state = newState;
+		// Вызываем всех слушателей
+		for (const listener of this.listeners) listener();
+	}
 
-  /**
-   * Добавление новой записи
-   */
-  addItem() {
-    this.setState({
-      ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
-    })
-  };
+	/**
+	 * Добавление новой записи
+	 */
+	addItem() {
+		this.setState({
+			...this.state,
+			list: [
+				...this.state.list,
+				{
+					code: this.state.codeCounter,
+					title: prompt('Введите название', 'Новая запись'),
+					selectedCounter: 0,
+				},
+			],
+			codeCounter: this.state.codeCounter + 1,
+		});
+	}
 
-  /**
-   * Удаление записи по коду
-   * @param code
-   */
-  deleteItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
+	/**
+	 * Удаление записи по коду
+	 * @param code
+	 */
+	deleteItem(code) {
+		this.setState({
+			...this.state,
+			list: this.state.list.filter((item) => item.code !== code),
+		});
+	}
 
-  /**
-   * Выделение записи по коду
-   * @param code
-   */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
-          item.selected = !item.selected;
-        }
-        return item;
-      })
-    })
-  }
+	/**
+	 * Выделение записи по коду
+	 * @param code
+	 */
+	selectItem(code) {
+		this.setState({
+			...this.state,
+			list: [
+				...this.state.list.map((item) => {
+					if (item.code === code) {
+						return {
+							...item,
+							selectedCounter: !item.selected
+								? item.selectedCounter + 1
+								: item.selectedCounter,
+							selected: !item.selected,
+						};
+					} else {
+						return { ...item, selected: false };
+					}
+				}),
+			],
+		});
+	}
 }
 
 export default Store;
--- src/utils.js
+++ src/utils.js
@@ -8,21 +8,29 @@ const propNames = new Set(['id', 'className', 'textContent', 'onclick']);
  * @returns {HTMLElement}
  */
 export function createElement(name, props = {}, ...children) {
-  const element = document.createElement(name);
+	const element = document.createElement(name);
 
-  // Назначение свойств и атрибутов
-  for (const name of Object.keys(props)) {
-    if (propNames.has(name)) {
-      element[name] = props[name];
-    } else {
-      element.setAttribute(name, props[name]);
-    }
-  }
+	// Назначение свойств и атрибутов
+	for (const name of Object.keys(props)) {
+		if (propNames.has(name)) {
+			element[name] = props[name];
+		} else {
+			element.setAttribute(name, props[name]);
+		}
+	}
 
-  // Вставка вложенных элементов
-  for (const child of children) {
-    element.append(child);
-  }
+	// Вставка вложенных элементов
+	for (const child of children) {
+		element.append(child);
+	}
 
-  return element;
+	return element;
 }
+
+// Принимаем число и возвращаем слово в правильной форме
+export const getWordForm = (number) => {
+	if (number % 10 >= 2 && number % 10 <= 4 && (number % 100 < 10 || number % 100 >= 20)) {
+		return 'раза';
+	}
+	return 'раз';
+};
