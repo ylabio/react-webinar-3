781fbb36b700967e01c3291c680c44c928217933
Zu <zukotan32@gmail.com>
2023-05-11 20:23:34 +0300

    fix selection and adding new objects, implement counter

--- src/utils.js
+++ src/utils.js
@@ -26,3 +26,8 @@ export function createElement(name, props = {}, ...children) {
 
   return element;
 }
+
+export function declension(num, words) {
+  const cases = [2, 0, 1, 1, 1, 2];
+  return words[num % 100 > 4 && num % 100 < 20 ? 2 : cases[Math.min(num % 10, 5)]];
+}
