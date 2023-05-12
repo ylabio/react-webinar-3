605f232166be1811cc8f0ec3e6f5add5012a068d
Aidaar <aidar21mamatov@gmail.com>
2023-05-12 20:22:47 +0600

    fixed selectItem

--- src/app.js
+++ src/app.js
@@ -21,11 +21,11 @@ function App({store}) {
       </div>
       <div className='App-center'>
         <div className='List'>{
-          list.map(item =>
+          list.map((item, index) =>
             <div key={item.code} className='List-item'>
               <div className={'Item' + (item.selected ? ' Item_selected' : '')}
                    onClick={() => store.selectItem(item.code)}>
-                <div className='Item-code'>{item.code}</div>
+                <div className='Item-code'>{index + 1}</div>
                 <div className='Item-title'>{item.title}</div>
                 <div className='Item-actions'>
                   <button onClick={() => store.deleteItem(item.code)}>
