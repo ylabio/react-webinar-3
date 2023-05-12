2704d03feba68a210d076b9bec5811d437940ffa
Dmitry Kudryavtsev <splcell2013@gmail.com>
2023-05-12 14:27:00 +0700

    Задание к первому уроку

--- src/app.js
+++ src/app.js
@@ -12,29 +12,37 @@ function App({store}) {
   const list = store.getState().list;
 
   return (
-    <div className='App'>
-      <div className='App-head'>
+    <div className="App">
+      <div className="App-head">
         <h1>Приложение на чистом JS</h1>
       </div>
-      <div className='App-controls'>
+      <div className="App-controls">
         <button onClick={() => store.addItem()}>Добавить</button>
       </div>
-      <div className='App-center'>
-        <div className='List'>{
-          list.map(item =>
-            <div key={item.code} className='List-item'>
-              <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                   onClick={() => store.selectItem(item.code)}>
-                <div className='Item-code'>{item.code}</div>
-                <div className='Item-title'>{item.title}</div>
-                <div className='Item-actions'>
+      <div className="App-center">
+        <div className="List">
+          {list.map((item) => (
+            <div key={item.code} className="List-item">
+              <div
+                className={"Item" + (item.selected ? " Item_selected" : "")}
+                onClick={() => store.selectItem(item.code)}
+              >
+                <div className="Item-code">{item.code}</div>
+                <div className="Item-title">{item.title}</div>
+                <div className="Item-title">
+                  {" "}
+                  {item.count > 0 && item.selected
+                    ? `Выделяли ${item.count} раз(а)`
+                    : null}
+                </div>
+                <div className="Item-actions">
                   <button onClick={() => store.deleteItem(item.code)}>
                     Удалить
                   </button>
                 </div>
               </div>
             </div>
-          )}
+          ))}
         </div>
       </div>
     </div>
