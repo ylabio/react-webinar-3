aff8f61de2998242e209eb8ec39f431fb992611a
Alexandra <sasha_huliganka@mail.ru>
2023-05-12 21:59:37 +0300

    Доработаны выделение записей, создание новых записей, показано количество выделений записей

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {createElement} from './utils.js';
 import './styles.css';
 
 /**
@@ -27,6 +26,9 @@ function App({store}) {
                    onClick={() => store.selectItem(item.code)}>
                 <div className='Item-code'>{item.code}</div>
                 <div className='Item-title'>{item.title}</div>
+                  <div className="Item-count">
+                      {item.count === 0 ? "" : `Выделяли ${item.count} раз(а)`}
+                  </div>
                 <div className='Item-actions'>
                   <button onClick={() => store.deleteItem(item.code)}>
                     Удалить
