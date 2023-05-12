cf0a31d3ed1a5e12e91a5801a5d82d22fff467ce
Vladislav Glotov <81396749+4LTon@users.noreply.github.com>
2023-05-12 23:53:02 +0300

    Add files via upload
    feat:
    1) При выделении записи, выделение с других снимается; клик по выделенной записи снимает выделение;
    2) Генерация уникального кода для новых записей в рамках сессии;
    3) Вывод кол-ва совершенных выделений;

---
+++ app.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {createElement} from './utils.js';
+import './styles.css';
+
+/**
+ * Приложение
+ * @param store {Store} Хранилище состояния приложения
+ * @returns {React.ReactElement}
+ */
+function App({store}) {
+
+  const list = store.getState().list;
+
+  /**
+   * Функция возвращает правильное слокнение слова "раз" в зависимости от числа
+   * @param number
+   * @return {string}
+   */
+  const getNumberEnding = number => {
+      if (number % 10 === 1 && number % 100 !== 11) {
+          return 'раз';
+      } else if ([2, 3, 4].includes(number % 10) && ![12, 13, 14].includes(number % 100)) {
+          return 'раза';
+      }
+
+      return 'раз';
+  };
+
+  return (
+    <div className='App'>
+      <div className='App-head'>
+        <h1>Приложение на чистом JS</h1>
+      </div>
+      <div className='App-controls'>
+        <button onClick={() => store.addItem()}>Добавить</button>
+      </div>
+      <div className='App-center'>
+        <div className='List'>{
+          list.map(item =>
+            <div key={item.code} className='List-item'>
+              <div className={'Item' + (item.selected ? ' Item_selected' : '')}
+                   onClick={() => store.selectItem(item.code)}>
+                <div className='Item-code'>{item.code}</div>
+                <div className='Item-title'>
+                    {item.title} {item.selectedCount > 0 && `| Выделяли ${item.selectedCount} ${getNumberEnding(item.selectedCount)}`}
+                </div>
+                <div className='Item-actions'>
+                  <button onClick={() => store.deleteItem(item.code)}>
+                    Удалить
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ store.js
@@ -0,0 +1,93 @@
+/**
+ * Хранилище состояния приложения
+ */
+class Store {
+  constructor(initState = {}) {
+    this.state = {
+      ...initState,
+      list: initState.list.map(item => ({...item, selectedCount: 0, selected: false})), // Добавляем свойство selectedCount и selected в каждый объект списка
+      totalCounter: 7 // Количество элементов при инициализации списка (необходимо для получение нового кода элемента)
+    };
+    this.listeners = []; // Слушатели изменений состояния
+  }
+
+  /**
+   * Подписка слушателя на изменения состояния
+   * @param listener {Function}
+   * @returns {Function} Функция отписки
+   */
+  subscribe(listener) {
+    this.listeners.push(listener);
+    // Возвращается функция для удаления добавленного слушателя
+    return () => {
+      this.listeners = this.listeners.filter(item => item !== listener);
+    }
+  }
+
+  /**
+   * Выбор состояния
+   * @returns {Object}
+   */
+  getState() {
+    return this.state;
+  }
+
+  /**
+   * Установка состояния
+   * @param newState {Object}
+   */
+  setState(newState) {
+    this.state = newState;
+    // Вызываем всех слушателей
+    for (const listener of this.listeners) listener();
+  }
+
+  /**
+   * Добавление новой записи
+   */
+  addItem() {
+    this.setState({
+      ...this.state,
+      totalCounter: ++this.state.totalCounter,
+      list: [
+          ...this.state.list,
+          {code: this.state.totalCounter, title: 'Новая запись', selectedCount: 0}
+      ]
+    })
+  };
+
+  /**
+   * Удаление записи по коду
+   * @param code
+   */
+  deleteItem(code) {
+    this.setState({
+      ...this.state,
+      list: this.state.list.filter(item => item.code !== code)
+    })
+  };
+
+  /**
+   * Выделение записи по коду
+   * @param code
+   */
+  selectItem(code) {
+    this.setState({
+      ...this.state,
+      list: this.state.list.map(item => {
+        if (item.code === code) {
+          if (item.selected === false) {
+            ++item.selectedCount;
+          }
+          item.selected = !item.selected;
+        }
+        else {
+          item.selected = false;
+        }
+        return item;
+      })
+    })
+  }
+}
+
+export default Store;
