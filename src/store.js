3a1f9a978b89b46d72055893a201aeff680694b6
Julia Latysheva <astreyajulia@gmail.com>
2023-05-12 20:22:49 +0300

    ADD: Задача 1 - При выделении элемента, выделение у других убирается

--- src/store.js
+++ src/store.js
@@ -2,88 +2,90 @@
  * Хранилище состояния приложения
  */
 class Store {
-  constructor(initState = {}) {
-    this.state = initState;
-    this.listeners = []; // Слушатели изменений состояния
-    this.lastCode = 7; // Последний созданный ид записи. FIXME - обнулить
-  }
-
-  /**
-   * Подписка слушателя на изменения состояния
-   * @param listener {Function}
-   * @returns {Function} Функция отписки
-   */
-  subscribe(listener) {
-    this.listeners.push(listener);
-    // Возвращается функция для удаления добавленного слушателя
-    return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
+    constructor(initState = {}) {
+        this.state = initState;
+        this.listeners = []; // Слушатели изменений состояния
+        this.lastCode = 7; // Последний созданный ид записи. FIXME - обнулить
     }
-  }
 
-  /**
-   * Выбор состояния
-   * @returns {Object}
-   */
-  getState() {
-    return this.state;
-  }
-
-  /**
-   * Установка состояния
-   * @param newState {Object}
-   */
-  setState(newState) {
-    this.state = newState;
-    // Вызываем всех слушателей
-    for (const listener of this.listeners) listener();
-  }
-
-  /**
-   * Обновление счетчика последней записи
-   */
-  updateCounter() {
-    this.lastCode = this.lastCode + 1
-  }
-
-  /**
-   * Добавление новой записи
-   */
-  addItem() {
-    this.updateCounter(); // обновляем счетчик
-    this.setState({
-      ...this.state,
-      list: [...this.state.list, {code: this.lastCode, title: 'Новая запись', selectCount: 0}],
-    })
-  };
-
-  /**
-   * Удаление записи по коду
-   * @param code
-   */
-  deleteItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
-
-  /**
-   * Выделение записи по коду
-   * @param code
-   */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
-          item.selected = !item.selected;
-          item.selectCount = item.selected ? item.selectCount + 1 : item.selectCount; // Увеличивает счетчик выделений
+    /**
+     * Подписка слушателя на изменения состояния
+     * @param listener {Function}
+     * @returns {Function} Функция отписки
+     */
+    subscribe(listener) {
+        this.listeners.push(listener);
+        // Возвращается функция для удаления добавленного слушателя
+        return () => {
+            this.listeners = this.listeners.filter(item => item !== listener);
         }
-        return item;
-      })
-    })
-  }
+    }
+
+    /**
+     * Выбор состояния
+     * @returns {Object}
+     */
+    getState() {
+        return this.state;
+    }
+
+    /**
+     * Установка состояния
+     * @param newState {Object}
+     */
+    setState(newState) {
+        this.state = newState;
+        // Вызываем всех слушателей
+        for (const listener of this.listeners) listener();
+    }
+
+    /**
+     * Обновление счетчика последней записи
+     */
+    updateCounter() {
+        this.lastCode = this.lastCode + 1
+    }
+
+    /**
+     * Добавление новой записи
+     */
+    addItem() {
+        this.updateCounter(); // обновляем счетчик
+        this.setState({
+            ...this.state,
+            list: [...this.state.list, {code: this.lastCode, title: 'Новая запись', selectCount: 0}],
+        })
+    };
+
+    /**
+     * Удаление записи по коду
+     * @param code
+     */
+    deleteItem(code) {
+        this.setState({
+            ...this.state,
+            list: this.state.list.filter(item => item.code !== code)
+        })
+    };
+
+    /**
+     * Выделение записи по коду
+     * @param code
+     */
+    selectItem(code) {
+        this.setState({
+            ...this.state,
+            list: this.state.list.map(item => {
+                if (item.code === code) {
+                    item.selected = !item.selected;
+                    item.selectCount = item.selected ? item.selectCount + 1 : item.selectCount; // Увеличивает счетчик выделений
+                } else if (item.code !== code) { // Убираем выделение с других записей
+                    item.selected = false;
+                }
+                return item;
+            })
+        })
+    }
 }
 
 export default Store;
--- src/utils.js
+++ src/utils.js
@@ -8,23 +8,23 @@ const propNames = new Set(['id', 'className', 'textContent', 'onclick']);
  * @returns {HTMLElement}
  */
 export function createElement(name, props = {}, ...children) {
-  const element = document.createElement(name);
+    const element = document.createElement(name);
 
-  // Назначение свойств и атрибутов
-  for (const name of Object.keys(props)) {
-    if (propNames.has(name)) {
-      element[name] = props[name];
-    } else {
-      element.setAttribute(name, props[name]);
+    // Назначение свойств и атрибутов
+    for (const name of Object.keys(props)) {
+        if (propNames.has(name)) {
+            element[name] = props[name];
+        } else {
+            element.setAttribute(name, props[name]);
+        }
     }
-  }
 
-  // Вставка вложенных элементов
-  for (const child of children) {
-    element.append(child);
-  }
+    // Вставка вложенных элементов
+    for (const child of children) {
+        element.append(child);
+    }
 
-  return element;
+    return element;
 }
 
 /** Возвращает числительное
@@ -35,21 +35,21 @@ export function createElement(name, props = {}, ...children) {
  * @param count - числительное для чисел 0 и от 5 до 20 включительно
  * например: {single: "год", multi: "года", count: "лет"}
  */
-export const getAmount = (col, { single, multi, count }) => {
-  while (col > 20) {
-    col = col.toString().slice(-1);
-    col = parseInt(col, 10);
-  }
+export const getAmount = (col, {single, multi, count}) => {
+    while (col > 20) {
+        col = col.toString().slice(-1);
+        col = parseInt(col, 10);
+    }
 
-  if (col === 1) {
-    return single;
-  }
-  if (col > 1 && col <= 4) {
-    return multi;
-  }
-  if (col >= 5 && col <= 20) {
-    return count;
-  }
+    if (col === 1) {
+        return single;
+    }
+    if (col > 1 && col <= 4) {
+        return multi;
+    }
+    if (col >= 5 && col <= 20) {
+        return count;
+    }
 
-  return count; // если col === 0
+    return count; // если col === 0
 };
