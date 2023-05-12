603a89ff5155f69b1419b191884f8f65908d6ecf
PrettyFlacko777 <aababochkin45@gmail.com>
2023-05-12 17:18:18 +0300

    done

--- src/store.js
+++ src/store.js
@@ -16,8 +16,8 @@ class Store {
     this.listeners.push(listener);
     // Возвращается функция для удаления добавленного слушателя
     return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    }
+      this.listeners = this.listeners.filter((item) => item !== listener);
+    };
   }
 
   /**
@@ -32,6 +32,7 @@ class Store {
    * Установка состояния
    * @param newState {Object}
    */
+
   setState(newState) {
     this.state = newState;
     // Вызываем всех слушателей
@@ -41,38 +42,58 @@ class Store {
   /**
    * Добавление новой записи
    */
+
   addItem() {
+    this.uniqeId();
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
-    })
-  };
+      list: [
+        ...this.state.list,
+        {
+          code: this.state.id,
+          title: "Новая запись",
+          count: 0,
+        },
+      ],
+    });
+  }
 
   /**
    * Удаление записи по коду
    * @param code
    */
+
   deleteItem(code) {
     this.setState({
       ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
+      list: this.state.list.filter((item) => item.code !== code),
+    });
+  }
 
   /**
    * Выделение записи по коду
    * @param code
    */
   selectItem(code) {
+    const newList = this.state.list.map((item) => {
+      if (item.code === code && !item.selected) {
+        item.selected = !item.selected;
+        if (item.selected === true) {
+          item.count++;
+        }
+      } else {
+        item.selected = false;
+      }
+      return item;
+    });
+    this.setState({ ...this.state, list: newList });
+  }
+
+  uniqeId() {
     this.setState({
       ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
-          item.selected = !item.selected;
-        }
-        return item;
-      })
-    })
+      id: this.state.id + 1,
+    });
   }
 }
 
