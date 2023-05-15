c191eb506cfd5664341def8538c8bf69225ad50f
Anna <inzhevatkinaav@gmail.com>
2023-05-15 12:38:09 +0300

    Homework ~ to |

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ function App({store}) {
                 <div className='Item-code'>{item.code}</div>
                 <div className='Item-title'>
                   {item.title}
-                  {item.selectCounter && item.selectCounter !== 0 ? ` ~ Выделяли ${item.selectCounter} раз` : ''}
+                  {item.selectCounter && item.selectCounter !== 0 ? ` | Выделяли ${item.selectCounter} раз` : ''}
                 </div>
                 <div className='Item-actions'>
                   <button onClick={() => store.deleteItem(item.code)}>
