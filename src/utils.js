9dcca93a4f243add03bb7d282faf0412dbb4dd63
Ermak Anna <ermak-anna86@yandex.ru>
2023-05-17 20:46:50 +0300

    новая ветка

--- src/utils.js
+++ src/utils.js
@@ -10,7 +10,7 @@
 export function plural(value, variants = {}, locale = 'ru-RU') {
   // Получаем фурму кодовой строкой: 'zero', 'one', 'two', 'few', 'many', 'other'
   // В русском языке 3 формы: 'one', 'few', 'many', и 'other' для дробных
-  // В английском 2 формы: 'one', 'other'
+
   const key = new Intl.PluralRules(locale).select(value);
   // Возвращаем вариант по ключу, если он есть
   return variants[key] || '';
