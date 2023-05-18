1e6eedb244243ac52f4748b506ea778259edf9ef
Irina <irene7744@gmail.com>
2023-05-18 21:30:55 +0300

    add closing modal on click outside

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from 'prop-types';
 import './style.css';
 
 function Modal({isOpen, onClose, children, title}){
+
+  useEffect( () => {
+    const onClickOutside = (e) => {
+      if (e.target.className === 'Modal opened'){
+        onClose();
+      }
+    }
+
+    window.addEventListener('click', onClickOutside, false );
+
+    return () => window.removeEventListener( 'click', onClickOutside );
+  }, [] );
+
   return (
     <div className={`Modal ${isOpen && 'opened'}`}>
       <div className='Modal-content'>
