f2b84df6ccb5541cfe1dee88c2e2fa0b7942e568
Resmart <128597148+DunaevVladimir@users.noreply.github.com>
2023-05-18 12:40:24 +0300

    lecture-2

--- src/app.js
+++ src/app.js
@@ -1,44 +1,41 @@
-import React from 'react';
-import { createElement } from './utils.js';
-import './styles.css';
+import React, {useCallback} from 'react';
+import List from "./components/list";
+import Controls from "./components/controls";
+import Head from "./components/head";
+import PageLayout from "./components/page-layout";
 
 /**
  * Приложение
  * @param store {Store} Хранилище состояния приложения
  * @returns {React.ReactElement}
  */
-function App({ store }) {
+function App({store}) {
 
-	const list = store.getState().list;
+  const list = store.getState().list;
 
-	return (
-		<div className='App'>
-			<div className='App-head'>
-				<h1>Приложение на чистом JS</h1>
-			</div>
-			<div className='App-controls'>
-				<button onClick={() => store.addItem()}>Добавить</button>
-			</div>
-			<div className='App-center'>
-				<div className='List'>{
-					list.map(item =>
-						<div key={item.code} className='List-item'>
-							<div className={'Item' + (item.selected ? ' Item_selected' : '')}
-								onClick={() => store.selectItem(item.code)}>
-								<div className='Item-code'>{item.code}</div>
-								<div className='Item-title'>{item.title}{item.selections ? ` | Выделяли ${item.selections} раз${item.selections % 10 > 1 && item.selections % 10 < 5 && (item.selections % 100 > 20 || item.selections % 100 < 10) ? 'а' : ''}` : ''}</div> {/* Проверяем количество выделений и в зависимости от цифры добавляем надпись */}
-								<div className='Item-actions'>
-									<button onClick={(event) => store.deleteItem(event, item.code)}>
-										Удалить
-									</button>
-								</div>
-							</div>
-						</div>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  const callbacks = {
+    onDeleteItem: useCallback((code) => {
+      store.deleteItem(code);
+    }, [store]),
+
+    onSelectItem: useCallback((code) => {
+      store.selectItem(code);
+    }, [store]),
+
+    onAddItem: useCallback(() => {
+      store.addItem();
+    }, [store])
+  }
+
+  return (
+    <PageLayout>
+      <Head title='Приложение на чистом JS'/>
+      <Controls onAdd={callbacks.onAddItem}/>
+      <List list={list}
+            onDeleteItem={callbacks.onDeleteItem}
+            onSelectItem={callbacks.onSelectItem}/>
+    </PageLayout>
+  );
 }
 
 export default App;
