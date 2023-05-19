1ba206e52e1c5f97db36a299461228804bbddab6
zPavel391 <golubev33108ai@gmail.com>
2023-05-19 17:54:49 +0200

    init: lecture-2

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import {createElement} from './utils.js';
-import './styles.css';
+import React, {useCallback} from 'react';
+import List from "./components/list";
+import Controls from "./components/controls";
+import Head from "./components/head";
+import PageLayout from "./components/page-layout";
 
 /**
  * Приложение
@@ -11,33 +13,28 @@ function App({store}) {
 
   const list = store.getState().list;
 
+  const callbacks = {
+    onDeleteItem: useCallback((code) => {
+      store.deleteItem(code);
+    }, [store]),
+
+    onSelectItem: useCallback((code) => {
+      store.selectItem(code);
+    }, [store]),
+
+    onAddItem: useCallback(() => {
+      store.addItem();
+    }, [store])
+  }
+
   return (
-    <div className='App'>
-      <div className='App-head'>
-        <h1>Приложение на чистом JS</h1>
-      </div>
-      <div className='App-controls'>
-        <button onClick={() => store.addItem()}>Добавить</button>
-      </div>
-      <div className='App-center'>
-        <div className='List'>{
-          list.map(item =>
-            <div key={item.code} className='List-item'>
-              <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                   onClick={() => store.selectItem(item.code)}>
-                <div className='Item-code'>{item.code}</div>
-                <div className='Item-title'>{item.title}</div>
-                <div className='Item-actions'>
-                  <button onClick={() => store.deleteItem(item.code)}>
-                    Удалить
-                  </button>
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
+    <PageLayout>
+      <Head title='Приложение на чистом JS'/>
+      <Controls onAdd={callbacks.onAddItem}/>
+      <List list={list}
+            onDeleteItem={callbacks.onDeleteItem}
+            onSelectItem={callbacks.onSelectItem}/>
+    </PageLayout>
   );
 }
 
