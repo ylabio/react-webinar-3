1367f21adf57598b8b93ae45eff514d4231f2ec1
Andrey <arkhonfills@list.ru>
2023-05-26 01:07:41 +0300

    list corrected

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -2,25 +2,19 @@ import {memo} from "react";
 import PropTypes from 'prop-types';
 import Item from "../item";
 import './style.css';
-import {cn as bem} from '@bem-react/classname';
-
-function List({list, functionResolver, buttonTitle}){
-
-  const cn = bem('List');
 
 function List({list, renderItem}){
   return (
-    <div className={cn()}>{
+    <div className='List'>{
       list.map(item =>
         <div key={item._id} className='List-item'>
           {renderItem(item)}
-
         </div>
       )}
     </div>
   )
 }
-}
+
 
 List.propTypes = {
   list: PropTypes.arrayOf(PropTypes.shape({
