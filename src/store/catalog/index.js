c18e65dbb914b136c6cd87b10657769c2fd49cae
Mishock <63130553+Mishochek@users.noreply.github.com>
2023-05-27 00:04:40 +0300

    Update index.js

--- src/store/catalog/index.js
+++ src/store/catalog/index.js
@@ -1,27 +1,43 @@
-import {codeGenerator} from "../../utils";
+import { codeGenerator } from "../../utils";
 import StoreModule from "../module";
+
+const baseURL = '/api/v1/articles';
 
 class Catalog extends StoreModule {
 
   constructor(store, name) {
-    super(store, name);
-    this.generateCode = codeGenerator(0)
-  }
+Expand All
+	@@ -10,16 +12,33 @@ class Catalog extends StoreModule {
 
   initState() {
     return {
-      list: []
+      list: [],
+      limit: 10,
+      totalArticlesCount: 0,
+      currentPage: 1,
     }
   }
 
-  async load() {
-    const response = await fetch('/api/v1/articles');
-    const json = await response.json();
+  setCurrentPage(page) {
+    console.log(page)
     this.setState({
-       ...this.getState(),
-       list: json.result.items
+      ...this.getState(),
+      currentPage: page
+    }, 'новая страница товаров');
+    this.load();
+  }
+
+  async load() {
+    const limit = this.getState().limit;
+    const skip = (this.getState().currentPage - 1) * limit;
+
+    const response = await fetch(`${baseURL}?limit=${limit}&skip=${skip}&fields=items(*),count`);
+    const json = await response.json();
+    console.log(json)
+    this.setState({
+      ...this.getState(),
+      list: json.result.items,
+      totalArticlesCount: json.result.count,
     }, 'Загружены товары из АПИ');
   }
 }
-
-export default Catalog;
