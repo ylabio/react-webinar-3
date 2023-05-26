bc6bd297f390e24b3467deec8d1ea4a78facf967
zPavel391 <golubev33108ai@gmail.com>
2023-05-26 13:53:00 +0200

    init: pagination

---
+++ src/components/pagination/index.js
@@ -0,0 +1,36 @@
+import React, {memo} from 'react'
+import { pageResultNumber } from '../../utils';
+import PropTypes from 'prop-types';
+import {cn as bem} from '@bem-react/classname';
+import './style.css'
+
+
+function Pagination({page, lastPage, handleClickPage}) {
+
+  const cn = bem('Pagination');
+  const pageNumbersList = pageResultNumber(page, lastPage)
+  
+  return (  
+    <div className={cn()}>
+      {page > 0 ? (
+        <div className={1 === page ? cn('active') : cn('number')} onClick={() => handleClickPage(1)}>1</div>
+      ) : ''}
+      {pageNumbersList.map((item, index) => (
+        item === '...' ? <div className={cn('text')} key={index}>...</div> : <div className={item === page ? cn('active') : cn('number')} key={index} onClick={() => handleClickPage(item)}>{item}</div>
+      ))}
+      <div className={lastPage === page ? cn('active') : cn('number')} onClick={() => handleClickPage(lastPage)}>{lastPage}</div>
+    </div>
+  )
+}
+
+Pagination.propTypes = {
+  page: PropTypes.number,
+  lastPage: PropTypes.number,
+  handleClickPage: PropTypes.func
+};
+
+Pagination.defaultProps = {
+  handleClickPage: () => {},
+}
+
+export default memo(Pagination)
--- src/store/catalog/index.js
+++ src/store/catalog/index.js
@@ -10,17 +10,34 @@ class Catalog extends StoreModule {
 
   initState() {
     return {
-      list: []
+      list: [],
+      page: 1
     }
   }
 
-  async load() {
-    const response = await fetch('/api/v1/articles');
+  async load(page = this.getState().page) {
+    const pageList = (page - 1) * 10
+    const response = await fetch(`/api/v1/articles?limit=10&skip=${pageList}&fields=items(_id, title, price),count`);
     const json = await response.json();
     this.setState({
        ...this.getState(),
-       list: json.result.items
+       list: json.result.items,
+       page,
+       lastPage: Math.ceil(json.result.count / 10),
     }, 'Загружены товары из АПИ');
+  }
+  // Выбор страницы списка
+  handleClickPage(numberPage) {
+    if (numberPage === this.page) {
+      return 
+    } else {
+      this.setState({
+        ...this.getState(),
+        page: numberPage
+      })
+      this.load(this.page)
+    } 
+    return
   }
 }
 
