9d2ee2b9e9a134f04faff53260d991ec01e2975a
Andrey <arkhonfills@list.ru>
2023-05-30 17:28:51 +0300

    get item after f5

--- src/store/basket/index.js
+++ src/store/basket/index.js
@@ -31,7 +31,8 @@ class Basket extends StoreModule {
     if (!exist) {
       // Поиск товара в каталоге, чтобы его добавить в корзину.
       // @todo В реальном приложении будет запрос к АПИ вместо поиска по состоянию.
-      const item = this.store.getState().catalog.list.find(item => item._id === _id);
+      let item = this.store.getState().product.item;
+      // const item = this.store.getState().catalog.list.find(item => item._id === _id);
       list.push({...item, amount: 1}); // list уже новый, в него можно пушить.
       // Добавляем к сумме.
       sum += item.price;
