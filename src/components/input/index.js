e88db2798840e58e12f77b25bd20e9dc27a0e998
Star Noob <van.kol@mail.ru>
2023-06-01 17:49:27 +0300

    fixed: search text input width

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,7 +1,7 @@
-import {memo, useCallback, useLayoutEffect, useState} from 'react';
-import PropTypes from "prop-types";
-import {cn as bem} from '@bem-react/classname';
+import { cn as bem } from '@bem-react/classname';
 import debounce from 'lodash.debounce';
+import PropTypes from "prop-types";
+import { memo, useCallback, useLayoutEffect, useState } from 'react';
 
 import './style.css';
 
