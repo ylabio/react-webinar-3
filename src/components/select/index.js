9bd842f46c56a93e442b3d68eac7de658f583383
Nata <nata.m.nata@inbox.ru>
2023-06-07 12:41:22 +0300

    fix

--- src/components/select/index.js
+++ src/components/select/index.js
@@ -1,33 +1,39 @@
-import React, {memo} from "react";
-import PropTypes from 'prop-types';
-import './style.css';
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+import './style.css'
 
 function Select(props) {
   const onSelect = (e) => {
-      props.onChange(e.target.value);
-  };
+    props.onChange(e.target.value)
+  }
   return (
     <select className="Select" value={props.value} onChange={onSelect}>
       {props.options.map((item, index) => (
-        <option key={index} value={item.value} >{"-".repeat(item.indent) + item.title}</option>
+        <option key={index} value={item.value}>
+          {item.value === props.value
+            ? item.title
+            : '-'.repeat(item.indent) + item.title}
+        </option>
       ))}
     </select>
   )
 }
 
 Select.propTypes = {
-  options: PropTypes.arrayOf(PropTypes.shape({
-    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    title: PropTypes.string
-  })).isRequired,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      title: PropTypes.string,
+    })
+  ).isRequired,
   value: PropTypes.any,
   onChange: PropTypes.func,
-  resetPage: PropTypes.func
-};
+  resetPage: PropTypes.func,
+}
 
 Select.defaultProps = {
   onChange: () => {},
-  resetPage: () => {}
+  resetPage: () => {},
 }
 
-export default memo(Select);
+export default memo(Select)
