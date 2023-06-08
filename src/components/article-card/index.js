0ee3860702efb74df7e95cacbbb3e2447c34bf64
Star Noob <van.kol@mail.ru>
2023-06-08 20:08:23 +0300

    added: missed translations to ArticleCard

--- src/components/article-card/index.js
+++ src/components/article-card/index.js
@@ -1,6 +1,6 @@
-import {memo} from "react";
+import { cn as bem } from '@bem-react/classname';
 import PropTypes from 'prop-types';
-import {cn as bem} from '@bem-react/classname';
+import { memo } from "react";
 import numberFormat from "../../utils/number-format";
 import './style.css';
 
@@ -10,19 +10,19 @@ function ArticleCard({article, onAdd, t}) {
     <div className={cn()}>
       <div className={cn('description')}>{article.description}</div>
       <div className={cn('prop')}>
-        <div className={cn('label')}>Страна производитель:</div>
+        <div className={cn('label')}>{t('article.country')}:</div>
         <div className={cn('value')}>{article.madeIn?.title} ({article.madeIn?.code})</div>
       </div>
       <div className={cn('prop')}>
-        <div className={cn('label')}>Категория:</div>
+        <div className={cn('label')}>{t('article.category')}:</div>
         <div className={cn('value')}>{article.category?.title}</div>
       </div>
       <div className={cn('prop')}>
-        <div className={cn('label')}>Год выпуска:</div>
+        <div className={cn('label')}>{t('article.year')}:</div>
         <div className={cn('value')}>{article.edition}</div>
       </div>
       <div className={cn('prop', {size: 'big'})}>
-        <div className={cn('label')}>Цена:</div>
+        <div className={cn('label')}>{t('article.price')}:</div>
         <div className={cn('value')}>{numberFormat(article.price)} ₽</div>
       </div>
       <button onClick={() => onAdd(article._id)}>{t('article.add')}</button>
