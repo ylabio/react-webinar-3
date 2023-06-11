1ce61a4c4981104065163888381f784a2abdd939
cool29 <cool29@yandex.ru>
2023-06-11 23:38:24 +0500

    добавил прототип сервиса i18n

--- src/hooks/use-translate.js
+++ src/hooks/use-translate.js
@@ -1,22 +1,40 @@
-import {useCallback, useContext} from "react";
+import {useCallback, useContext, useEffect, useMemo, useState} from "react";
 // import useStore from "../store/use-store";
 // import useSelector from "../store/use-selector";
 // import translate from "../i18n/translate";
-import {I18nContext} from "../i18n/context";
+import useServices from "./use-services";
 
 /**
  * Хук возвращает функцию для локализации текстов, код языка и функцию его смены
  */
 export default function useTranslate() {
-  // const store = useStore();
-  // // Текущая локаль
-  // const lang = useSelector(state => state.locale.lang);
-  // // Функция для смены локали
-  // const setLang = useCallback(lang => store.actions.locale.setLang(lang), []);
-  // // Функция для локализации текстов
-  // const t = useCallback((text, number) => translate(lang, text, number), [lang]);
-  //
-  // return {lang, setLang, t};
 
-  return useContext(I18nContext);
+  const [rerenderKey, setRerenderKey] = useState(0);
+
+  const services = useServices();
+
+  const rerenderCb = useCallback(() => {
+    setRerenderKey(prev => prev + 1)
+  }, [])
+
+  useEffect(() => {
+    const id = services.i18n.addSubscriber(rerenderCb);
+
+    return () => {
+      services.i18n.deleteSubscriber(id);
+    }
+  }, [])
+
+  return useMemo(() => ({
+    lang: services.i18n.lang,
+
+    setLang(lang) {
+      services.i18n.lang = lang;
+
+    },
+
+    t: services.i18n.translate
+
+  }), [rerenderKey]);
 }
+
