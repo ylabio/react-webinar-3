9447d9aa6e28d1f91e665e5b790b9c949681b202
pego-rus <pego_rus@mail.ru>
2024-02-21 13:20:29 +0300

    homework_1

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {createElement} from './utils.js';
 import './styles.css';
 
 /**
@@ -26,7 +25,12 @@ function App({store}) {
               <div className={'Item' + (item.selected ? ' Item_selected' : '')}
                    onClick={() => store.selectItem(item.code)}>
                 <div className='Item-code'>{item.code}</div>
-                <div className='Item-title'>{item.title}</div>
+                <div className='Item-title'>
+                  {item.selectionQuantity ? `${item.title} | Выделяли ${item.selectionQuantity} раз${
+                                              (item.selectionQuantity < 10 || item.selectionQuantity > 20) 
+                                              && ([2,3,4].findIndex(el => el === item.selectionQuantity % 10) !== -1) ? 'а' : ''}`
+                                          : item.title}
+                </div>
                 <div className='Item-actions'>
                   <button onClick={() => store.deleteItem(item.code)}>
                     Удалить
