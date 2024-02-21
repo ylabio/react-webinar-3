dbb7aad3ee7f42b41809bfc7030c7f460cf11bf0
TriqqqS <333qqqsss@gmail.com>
2024-02-21 12:46:59 +0100

    lecture-1 commit

--- src/store.js
+++ src/store.js
@@ -16,8 +16,8 @@ class Store {
     this.listeners.push(listener);
     // Возвращается функция для удаления добавленного слушателя
     return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    }
+      this.listeners = this.listeners.filter((item) => item !== listener);
+    };
   }
 
   /**
@@ -44,9 +44,10 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
-    })
-  };
+      codeCounter: this.state.codeCounter + 1,
+      list: [...this.state.list, { code: this.state.codeCounter + 1, title: "Новая запись" }],
+    });
+  }
 
   /**
    * Удаление записи по коду
@@ -55,9 +56,9 @@ class Store {
   deleteItem(code) {
     this.setState({
       ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
+      list: this.state.list.filter((item) => item.code !== code),
+    });
+  }
 
   /**
    * Выделение записи по коду
@@ -66,13 +67,20 @@ class Store {
   selectItem(code) {
     this.setState({
       ...this.state,
-      list: this.state.list.map(item => {
+      list: this.state.list.map((item) => {
         if (item.code === code) {
-          item.selected = !item.selected;
+          if (item.selected) {
+            item.selected = false;
+          } else {
+            item.selected = true;
+            item.counter = (item.counter ?? 0) + 1;
+          }
+        } else {
+          item.selected = false;
         }
         return item;
-      })
-    })
+      }),
+    });
   }
 }
 
