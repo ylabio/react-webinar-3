ddb0d3aa1b2e8c583f4180715474169cfded2782
Zakir Bikbov <zakir.bikbov@gmail.com>
2024-02-21 20:57:10 +0500

    Выполнил первое задание

--- src/store.js
+++ src/store.js
@@ -44,7 +44,7 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
+      list: [...this.state.list, { code: this.state.list.length + 1, title: 'Новая запись' }]
     })
   };
 
@@ -69,6 +69,8 @@ class Store {
       list: this.state.list.map(item => {
         if (item.code === code) {
           item.selected = !item.selected;
+        } else {
+          item.selected = false;
         }
         return item;
       })
