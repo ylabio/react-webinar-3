d084e46dc7891dcf5c76f7e3dfe493db2548b5db
aleksander1802 <aleksander-18-02@yandex.ru>
2024-02-21 13:59:36 +0300

    feat: improved the selection of records/the creation of a new record

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createElement} from './utils.js';
+import { createElement } from './utils.js';
 import './styles.css';
 
 /**
@@ -7,38 +7,61 @@ import './styles.css';
  * @param store {Store} Состояние приложения
  * @returns {React.ReactElement}
  */
-function App({store}) {
+function App({ store }) {
+	const list = store.getState().list;
 
-  const list = store.getState().list;
+	return (
+		<div className="App">
+			<div className="App-head">
+				<h1>Приложение на чистом JS</h1>
+			</div>
+			<div className="App-controls">
+				<button onClick={() => store.addItem()}>Добавить</button>
+			</div>
+			<div className="App-center">
+				<div className="List">
+					{list.map((item) => (
+						<ListItem
+							key={item.code}
+							item={item}
+							store={store}
+						/>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+}
 
-  return (
-    <div className='App'>
-      <div className='App-head'>
-        <h1>Приложение на чистом JS</h1>
-      </div>
-      <div className='App-controls'>
-        <button onClick={() => store.addItem()}>Добавить</button>
-      </div>
-      <div className='App-center'>
-        <div className='List'>{
-          list.map(item =>
-            <div key={item.code} className='List-item'>
-              <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                   onClick={() => store.selectItem(item.code)}>
-                <div className='Item-code'>{item.code}</div>
-                <div className='Item-title'>{item.title}</div>
-                <div className='Item-actions'>
-                  <button onClick={() => store.deleteItem(item.code)}>
-                    Удалить
-                  </button>
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+function ListItem({ item, store }) {
+	const handleToggleSelection = () => {
+		store.selectItem(item.code);
+		store.incrementSelections(item.code);
+	};
+
+	return (
+		<div className="List-item">
+			<div
+				className={'Item' + (item.selected ? ' Item_selected' : '')}
+				onClick={handleToggleSelection}
+			>
+				<div className="Item-code">{item.code}</div>
+				<div className="Item-title">{item.title}</div>
+				<div className="Item-actions">
+					<button
+						onClick={(event) => store.deleteItem(item.code, event)}
+					>
+						Удалить
+					</button>
+				</div>
+			</div>
+			{item.selections > 0 && (
+				<div className="Item-selections">
+					Выделяли {item.selections} раз
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
--- src/store.js
+++ src/store.js
@@ -2,78 +2,123 @@
  * Хранилище состояния приложения
  */
 class Store {
-  constructor(initState = {}) {
-    this.state = initState;
-    this.listeners = []; // Слушатели изменений состояния
-  }
+	constructor(initState = {}) {
+		this.state = initState;
+		this.listeners = []; // Слушатели изменений состояния
 
-  /**
-   * Подписка слушателя на изменения состояния
-   * @param listener {Function}
-   * @returns {Function} Функция отписки
-   */
-  subscribe(listener) {
-    this.listeners.push(listener);
-    // Возвращается функция для удаления добавленного слушателя
-    return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    }
-  }
+		// Добавляем счетчик выделений для каждого элемента списка
+		this.state.list.forEach((item) => {
+			item.selections = 0;
+		});
 
-  /**
-   * Выбор состояния
-   * @returns {Object}
-   */
-  getState() {
-    return this.state;
-  }
+		this.lastCode =
+			initState.list.length > 0
+				? Math.max(...initState.list.map((item) => item.code))
+				: 0; // Последний использованный код
+	}
 
-  /**
-   * Установка состояния
-   * @param newState {Object}
-   */
-  setState(newState) {
-    this.state = newState;
-    // Вызываем всех слушателей
-    for (const listener of this.listeners) listener();
-  }
+	/**
+	 * Подписка слушателя на изменения состояния
+	 * @param listener {Function}
+	 * @returns {Function} Функция отписки
+	 */
+	subscribe(listener) {
+		this.listeners.push(listener);
+		// Возвращается функция для удаления добавленного слушателя
+		return () => {
+			this.listeners = this.listeners.filter((item) => item !== listener);
+		};
+	}
 
-  /**
-   * Добавление новой записи
-   */
-  addItem() {
-    this.setState({
-      ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
-    })
-  };
+	/**
+	 * Выбор состояния
+	 * @returns {Object}
+	 */
+	getState() {
+		return this.state;
+	}
 
-  /**
-   * Удаление записи по коду
-   * @param code
-   */
-  deleteItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
+	/**
+	 * Установка состояния
+	 * @param newState {Object}
+	 */
+	setState(newState) {
+		this.state = newState;
+		// Вызываем всех слушателей
+		for (const listener of this.listeners) listener();
+	}
 
-  /**
-   * Выделение записи по коду
-   * @param code
-   */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
-          item.selected = !item.selected;
-        }
-        return item;
-      })
-    })
-  }
+	/**
+	 * Добавление новой записи
+	 */
+	addItem() {
+		const newCode = ++this.lastCode; // Увеличиваем  значение на 1, а затем присваивать полученное значение переменной
+		const newItem = { code: newCode, title: 'Новая запись', selections: 0 }; // Создаем новую запись и устанавливаем selections в 0
+		this.setState({
+			...this.state,
+			list: [...this.state.list, newItem], // Добавляем новый объект в массив, учитывая предыдущие
+		});
+	}
+
+	/**
+	 * Удаление записи по коду
+	 * @param code
+	 */
+	deleteItem(code, event) {
+		event.stopPropagation(); // Предотвращаем всплытие события
+		const newList = this.state.list.filter((item) => item.code !== code); // Немного модифицируем, перенесём фильтрацию в переменную
+		this.setState({
+			...this.state,
+			list: newList,
+		});
+	}
+
+	/**
+	 * Увеличение счетчика выделений для элемента по его коду
+	 * @param code {number} Код элемента
+	 */
+	incrementSelections(code) {
+		this.setState({
+			...this.state,
+			list: this.state.list.map((item) => {
+				if (item.code === code) {
+					item.selections += 1;
+				}
+				return item;
+			}),
+		});
+	}
+
+	/**
+	 * Сброс счетчика выделений для всех элементов списка
+	 */
+	resetSelections() {
+		this.setState({
+			...this.state,
+			list: this.state.list.map((item) => {
+				item.selections = 0;
+				return item;
+			}),
+		});
+	}
+
+	/**
+	 * Выделение записи по коду
+	 * @param code
+	 */
+	selectItem(code) {
+		this.setState({
+			...this.state,
+			list: this.state.list.map((item) => {
+				if (item.code === code) {
+					item.selected = !item.selected;
+				} else if (item.selected) {
+					item.selected = false;
+				}
+				return item;
+			}),
+		});
+	}
 }
 
 export default Store;
