74325c1a5e3da3dcfea709ad23659c76ba552c05
Alexander Revenko <revenko.a@bk.ru>
2024-02-21 16:43:49 +0700

    Задания 1 и 2

--- src/store.js
+++ src/store.js
@@ -5,6 +5,13 @@ class Store {
   constructor(initState = {}) {
     this.state = initState;
     this.listeners = []; // Слушатели изменений состояния
+
+    // Добавить счётчик выделений элемента с нулём по умолчанию 
+    this.state.list.forEach((item) => item.selectCount = 0)
+    
+    // Генерация уникального кода для новых записей
+    // В последствии он будет увеличиваться на 1
+    this.initalCode = this.generateInitialCode();
   }
 
   /**
@@ -44,7 +51,7 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
+      list: [...this.state.list, {code: this.generateNewUniqueCode(), title: 'Новая запись', selectCount: 0},]
     })
   };
 
@@ -68,11 +75,46 @@ class Store {
       ...this.state,
       list: this.state.list.map(item => {
         if (item.code === code) {
-          item.selected = !item.selected;
+
+          // Если элемент не выделен -> выделить и увеличить значение счётчика 
+          if (!item.selected) {
+            item.selected = true;
+            item.selectCount++
+          } 
+          
+          // Если элемент выделен -> обнули выделение
+          else {
+            item.selected = false;
+          }
+        } 
+        
+        // Если элемент не подходит по коду -> обнули выделение 
+        else {
+          item.selected = false;
         }
+
         return item;
       })
     })
+  }
+
+  // Проанализирует коды начальных значений и вернёт код с самым большим значением 
+  // на основе него буду генерироваться новые коды для новых записей 
+  generateInitialCode() {
+    let initalCode = 0;
+
+    this.state.list.map((item) => {
+      if (item.code > initalCode) {
+        initalCode = item.code;
+      }
+    })
+
+    return initalCode;
+  }
+  
+  // Генерирует новый уникальный код путём инкремента прошлого кода на 1
+  generateNewUniqueCode() {
+    return this.initalCode += 1;
   }
 }
 
