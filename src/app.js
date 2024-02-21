90d8af9d148457ffebddeb8989cefc8be374e25a
Ruslan-Baldeznik <100410676+Ruslan-Baldeznik@users.noreply.github.com>
2024-02-21 14:25:11 +0400

    exercise_3 completed

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ function App({store}) {
       <div className='App-center'>
         <div className='List'>{
           list.map(item =>
-            <ListItem item={item} store={store}/>
+            <ListItem key={item.code} item={item} store={store}/>
           )}
         </div>
       </div>
