b4920327bcc7cce0f14881a72e03c0a89431d95f
19Vladimir85 <s.vladimiran@yandex.ru>
2024-02-22 22:43:01 +0300

    solution first home work

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ class Store {
   constructor(initState = {}) {
     this.state = initState;
     this.listeners = []; // Слушатели изменений состояния
+    this.uniCode = this.state.list.length + 1;
   }
 
   /**
@@ -16,8 +17,8 @@ class Store {
     this.listeners.push(listener);
     // Возвращается функция для удаления добавленного слушателя
     return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    }
+      this.listeners = this.listeners.filter((item) => item !== listener);
+    };
   }
 
   /**
@@ -44,9 +45,12 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
-    })
-  };
+      list: [
+        ...this.state.list,
+        { code: this.uniCode++, title: "Новая запись", сount: 0 },
+      ],
+    });
+  }
 
   /**
    * Удаление записи по коду
@@ -55,9 +59,9 @@ class Store {
   deleteItem(code) {
     this.setState({
       ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
+      list: this.state.list.filter((item) => item.code !== code),
+    });
+  }
 
   /**
    * Выделение записи по коду
@@ -66,13 +70,19 @@ class Store {
   selectItem(code) {
     this.setState({
       ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
+      list: this.state.list.map((item) => {
+        if (item.code !== code || item.selected) {
+          item.selected = false;
+        } else if (!item.count) {
           item.selected = !item.selected;
+          item.count = 1;
+        } else {
+          item.selected = !item.selected;
+          item.count++;
         }
         return item;
-      })
-    })
+      }),
+    });
   }
 }
 
