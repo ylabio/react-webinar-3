6a66dc0be21615845a7c22ea5d7193624ce826ef
DaniilKun <96586538+DaniilKun@users.noreply.github.com>
2024-02-22 18:20:19 +0400

    доделал все 3 задания

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {createElement} from './utils.js';
 import './styles.css';
 
+
 /**
  * Приложение
  * @param store {Store} Состояние приложения
  * @returns {React.ReactElement}
  */
-function App({store}) {
-
+function App({ store }) {
   const list = store.getState().list;
 
   return (
@@ -20,21 +20,23 @@ function App({store}) {
         <button onClick={() => store.addItem()}>Добавить</button>
       </div>
       <div className='App-center'>
-        <div className='List'>{
-          list.map(item =>
+        <div className='List'>
+          {list.map(item => (
             <div key={item.code} className='List-item'>
-              <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                   onClick={() => store.selectItem(item.code)}>
+              <div
+                className={'Item' + (item.selected ? ' Item_selected' : '')}
+                onClick={() => store.selectItem(item.code)}
+              >
                 <div className='Item-code'>{item.code}</div>
-                <div className='Item-title'>{item.title}</div>
+                <div className='Item-title'>
+                  {item.title}  {item.selectedCount > 0 && `| Выделяли ${item.selectedCount} раз`}
+                </div>
                 <div className='Item-actions'>
-                  <button onClick={() => store.deleteItem(item.code)}>
-                    Удалить
-                  </button>
+                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
                 </div>
               </div>
             </div>
-          )}
+          ))}
         </div>
       </div>
     </div>
