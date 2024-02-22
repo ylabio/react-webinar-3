2a8494676eca861cd7177efef09da63a77900853
Artur Ladin <artur.avitolog@yandex.ru>
2024-02-22 12:32:25 +0500

    complited tasks

--- src/store.js
+++ src/store.js
@@ -3,7 +3,11 @@
  */
 class Store {
   constructor(initState = {}) {
-    this.state = initState;
+    this.state = {
+      ...initState,
+      list: initState.list.map((item) => ({ ...item, counter: 0 })), // добавил счетчик в состояние (задание 3)
+    };
+    this.lastCode = this.state.list.length + 1; // сохраним последнее значение code (задание 2)
     this.listeners = []; // Слушатели изменений состояния
   }
 
@@ -16,8 +20,8 @@ class Store {
     this.listeners.push(listener);
     // Возвращается функция для удаления добавленного слушателя
     return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    }
+      this.listeners = this.listeners.filter((item) => item !== listener);
+    };
   }
 
   /**
@@ -44,9 +48,12 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
-    })
-  };
+      list: [
+        ...this.state.list,
+        { code: this.lastCode++, title: "Новая запись", counter: 0 }, // получается уникальный код для новой записи (задание 2)
+      ],
+    });
+  }
 
   /**
    * Удаление записи по коду
@@ -55,9 +62,9 @@ class Store {
   deleteItem(code) {
     this.setState({
       ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
+      list: this.state.list.filter((item) => item.code !== code),
+    });
+  }
 
   /**
    * Выделение записи по коду
@@ -66,13 +73,18 @@ class Store {
   selectItem(code) {
     this.setState({
       ...this.state,
-      list: this.state.list.map(item => {
+      list: this.state.list.map((item) => {
         if (item.code === code) {
           item.selected = !item.selected;
+          if (item.selected) {
+            item.counter++; // При выделении записи увеличиваем счетчик (задание 3)
+          }
+        } else {
+          item.selected = false; // убрал выделение со всех записей, кроме выбранной (задание 1)
         }
         return item;
-      })
-    })
+      }),
+    });
   }
 }
 
