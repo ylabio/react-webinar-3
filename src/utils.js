751d5d55fb89056caabed48751ae3bebe09ba4c5
Илона Курситыс <ilonakursitys@Ilonas-MacBook-Air.local>
2024-02-22 23:16:14 +0700

    homework 1

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const propNames = new Set(['id', 'className', 'textContent', 'onclick']);
+const propNames = new Set(["id", "className", "textContent", "onclick"]);
 
 /**
  * Создание элемента со свойствами и вложенными элементами
@@ -25,4 +25,9 @@ export function createElement(name, props = {}, ...children) {
   }
 
   return element;
+}
+
+export function createCode(inputCode) {
+  const uniqueCode = Math.random(inputCode);
+  return uniqueCode;
 }
