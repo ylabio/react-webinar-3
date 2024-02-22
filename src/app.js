1471b5f63e2d304ce929ae4d183a0c9374fa7430
DmitryLasuta <lasutadima01@gmail.com>
2024-02-22 20:45:19 +0300

    chore: fixed caption of selected elements

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ function App({ store }) {
               >
                 <div className="Item-code">{item.code}</div>
                 <div className="Item-title">
-                  {item.title} {item.selectedCount ? `(${item.selectedCount})` : ''}
+                  {item.title} {item.selectedCount ? `Выделяли ${item.selectedCount} раз` : ''}
                 </div>
                 <div className="Item-actions">
                   <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
