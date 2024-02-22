425604905a3cf2116949d9f5c9d94a580c6e9fbc
Denis Semyannikov <Denny32191@gmail.com>
2024-02-22 10:10:33 +0200

    nw

--- src/store.js
+++ src/store.js
@@ -1,6 +1,12 @@
+import { generateUniqueId } from "./utils";
+
+
+const uniqueId = generateUniqueId();
+
 /**
  * Хранилище состояния приложения
  */
+
 class Store {
   constructor(initState = {}) {
     this.state = initState;
@@ -16,8 +22,8 @@ class Store {
     this.listeners.push(listener);
     // Возвращается функция для удаления добавленного слушателя
     return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    }
+      this.listeners = this.listeners.filter((item) => item !== listener);
+    };
   }
 
   /**
@@ -41,12 +47,16 @@ class Store {
   /**
    * Добавление новой записи
    */
+ 
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись',count: 0}]
-    })
-  };
+      list: [
+        ...this.state.list,
+        { code: uniqueId(), title: "Новая запись", count: 0 },
+      ],
+    });
+  }
 
   /**
    * Удаление записи по коду
@@ -55,9 +65,9 @@ class Store {
   deleteItem(code) {
     this.setState({
       ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
+      list: this.state.list.filter((item) => item.code !== code),
+    });
+  }
 
   /**
    * Выделение записи по коду
@@ -66,16 +76,16 @@ class Store {
   selectItem(code) {
     this.setState({
       ...this.state,
-      list: this.state.list.map(item => {
+      list: this.state.list.map((item) => {
         if (item.code === code) {
           item.selected = !item.selected;
-           item.count = item.count + 1;
-          } else {
-            item.selected = false; // Сброс выделения у других записей
+          item.count = item.count + 1;
+        } else {
+          item.selected = false; // Сброс выделения у других записей
         }
         return item;
-      })
-    })
+      }),
+    });
   }
 }
 
