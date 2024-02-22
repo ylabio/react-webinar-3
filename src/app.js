720666ce57024901040ce6c88d80de46f72ae8c2
Regina Askerova <saparova.regina@yandex.ru>
2024-02-22 17:16:41 +0300

    lecture_1

--- src/app.js
+++ src/app.js
@@ -1,42 +1,127 @@
-import React from 'react';
-import {createElement} from './utils.js';
-import './styles.css';
+import React from "react";
+
+import { useState, useEffect } from "react";
+import Store from "./store.js";
+
+import "./styles.css";
 
 /**
  * Приложение
  * @param store {Store} Состояние приложения
  * @returns {React.ReactElement}
  */
-function App({store}) {
+function App() {
+  const [select, setSelected] = useState([
+    { code: 1, title: "Название элемента" },
+    { code: 2, title: "Некий объект" },
+    { code: 3, title: "Заголовок" },
+    { code: 4, title: "Очень длинное название элемента из семи слов" },
+    { code: 5, title: "Запись" },
+    { code: 6, title: "Шестая запись" },
+    { code: 7, title: "Седьмая запись" },
+  ]);
 
-  const list = store.getState().list;
+  const usedArr = select.map((item) => item.code);
+
+  const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const newCode = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (usedArr.includes(newCode)) {
+      newCode = Math.floor(Math.random() * (max - min + 1)) + min;
+    } else {
+      return newCode;
+    }
+  };
+
+  const addItem = () => {
+    const codeUpd = getRandomInt(select.length, select.length * 10);
+    setSelected([...select, { code: codeUpd, title: "Новая запись" }]);
+  };
+
+  const allValues = (obj) => {
+    const newObj = {};
+    newObj.code = obj.code;
+    newObj.title = obj.title;
+    newObj.selected = false;
+    return newObj;
+  };
+
+  const showSelect = () => {
+    if (select[0].selected === undefined) {
+      const newArr = select.map((item) => allValues(item));
+      setSelected(newArr);
+    }
+  };
+
+  const findByCode = (code) => {
+    const newSelect = select;
+    newSelect.map((item) => (item.selected = false));
+    newSelect.find((item) => item.code === code).selected = true;
+    setSelected([...select]);
+  };
+
+  useEffect(() => showSelect(), []);
 
   return (
-    <div className='App'>
-      <div className='App-head'>
+    <div className="App">
+      <div className="App-head">
         <h1>Приложение на чистом JS</h1>
       </div>
-      <div className='App-controls'>
-        <button onClick={() => store.addItem()}>Добавить</button>
+      <div className="App-controls">
+        <button onClick={addItem}>Добавить</button>
       </div>
-      <div className='App-center'>
-        <div className='List'>{
-          list.map(item =>
-            <div key={item.code} className='List-item'>
-              <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                   onClick={() => store.selectItem(item.code)}>
-                <div className='Item-code'>{item.code}</div>
-                <div className='Item-title'>{item.title}</div>
-                <div className='Item-actions'>
-                  <button onClick={() => store.deleteItem(item.code)}>
-                    Удалить
-                  </button>
-                </div>
-              </div>
-            </div>
-          )}
+      <div className="App-center">
+        <div className="List">
+          {select.map((item) => (
+            <Item
+              key={item.code}
+              select={select}
+              findByCode={findByCode}
+              selected={item.selected}
+              showSelect={showSelect}
+              setSelected={setSelected}
+              code={item.code}
+              title={item.title}
+            />
+          ))}
         </div>
       </div>
+    </div>
+  );
+}
+
+function Item({ code, title, selected, findByCode, setSelected, select }) {
+  const [count, setCount] = useState(0);
+  return (
+    <div key={code} className="List-item">
+      <div
+        onClick={() => {
+          findByCode(code);
+          setCount(count + 1);
+        }}
+        className={!selected ? "Item" : "Item Item_selected"}
+      >
+        <div className="Item-code">{code}</div>
+        <div className="Item-title">{title}</div>
+        {count > 0 ? <p> Выделяли {count} раз</p> : " "}
+        <DeleteButton setSelected={setSelected} code={code} select={select} />
+      </div>
+    </div>
+  );
+}
+
+function DeleteButton({ setSelected, code, select }) {
+  return (
+    <div className="Item-actions">
+      <button
+        onClick={(event) => {
+          setSelected([...select.filter((item) => item.code !== code)]);
+          event.stopPropagation();
+        }}
+      >
+        Удалить
+      </button>
     </div>
   );
 }
