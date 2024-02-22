0ddfa75ed37a7f21e25b75aa9cc489d7a950b666
Mila Gvozdeva <103929930+KaSofi@users.noreply.github.com>
2024-02-22 12:03:09 +0300

    Update store.js
    Обновление кода

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { codeId } from './utils';
+
 /**
  * Хранилище состояния приложения
  */
@@ -44,7 +46,7 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
+      list: [...this.state.list, {code: codeId(), numberOfSelects: 0, title: 'Новая запись'}]
     })
   };
 
@@ -69,11 +71,17 @@ class Store {
       list: this.state.list.map(item => {
         if (item.code === code) {
           item.selected = !item.selected;
+          item.selected ? item.numberOfSelects += 1 : item.numberOfSelects;
+        } else {
+          item.selected = false;
         }
         return item;
       })
     })
   }
+}
+
+export default Store;
 }
 
 export default Store;
