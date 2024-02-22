530a7978a653479de3b238894e6da765ac4f6122
Nikonova Larisa <97353756+Larrrisa@users.noreply.github.com>
2024-02-22 20:53:50 +0400

    store changed

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { forwardRef } from "react";
+
 /**
  * Хранилище состояния приложения
  */
@@ -16,8 +18,8 @@ class Store {
     this.listeners.push(listener);
     // Возвращается функция для удаления добавленного слушателя
     return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    }
+      this.listeners = this.listeners.filter((item) => item !== listener);
+    };
   }
 
   /**
@@ -42,11 +44,27 @@ class Store {
    * Добавление новой записи
    */
   addItem() {
+    const generateCode = () => {
+      let code = Math.floor(Math.random() * 100);
+      while (this.state.list.some((item) => item.code === code)) {
+        code = Math.floor(Math.random() * 100);
+      }
+      return code;
+    };
+    let generatedCode = generateCode();
+
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
-    })
-  };
+      list: [
+        ...this.state.list,
+        {
+          code: generatedCode,
+          title: "Новая запись",
+          selectCount: 0,
+        },
+      ],
+    });
+  }
 
   /**
    * Удаление записи по коду
@@ -55,9 +73,9 @@ class Store {
   deleteItem(code) {
     this.setState({
       ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
+      list: this.state.list.filter((item) => item.code !== code),
+    });
+  }
 
   /**
    * Выделение записи по коду
@@ -66,13 +84,17 @@ class Store {
   selectItem(code) {
     this.setState({
       ...this.state,
-      list: this.state.list.map(item => {
+      list: this.state.list.map((item) => {
         if (item.code === code) {
           item.selected = !item.selected;
+          item.selectCount = item.selectCount + 1;
+          // item.title = `${item.title} | Выделяли ${item.selectCount} раз`;
+        } else {
+          item.selected = false;
         }
         return item;
-      })
-    })
+      }),
+    });
   }
 }
 
