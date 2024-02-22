463a919c7a11d49fd0a8d542183e257ca84e7cf6
Roman <romanahramenya@gmail.com>
2024-02-22 08:51:27 +0300

    finish

--- src/app.js
+++ src/app.js
@@ -1,40 +1,51 @@
-import React from 'react';
-import {createElement} from './utils.js';
-import './styles.css';
+import React, { useEffect } from "react";
+import { createElement } from "./utils.js";
+import "./styles.css";
 
 /**
  * Приложение
  * @param store {Store} Состояние приложения
  * @returns {React.ReactElement}
  */
-function App({store}) {
-
+function App({ store }) {
   const list = store.getState().list;
+  useEffect(() => {
+    store.updateSessionTotalItems();
+    store.setTotalSelectedItems();
+  }, []);
 
   return (
-    <div className='App'>
-      <div className='App-head'>
+    <div className="App">
+      <div className="App-head">
         <h1>Приложение на чистом JS</h1>
       </div>
-      <div className='App-controls'>
+      <div className="App-controls">
         <button onClick={() => store.addItem()}>Добавить</button>
       </div>
-      <div className='App-center'>
-        <div className='List'>{
-          list.map(item =>
-            <div key={item.code} className='List-item'>
-              <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                   onClick={() => store.selectItem(item.code)}>
-                <div className='Item-code'>{item.code}</div>
-                <div className='Item-title'>{item.title}</div>
-                <div className='Item-actions'>
+      <div className="App-center">
+        <div className="List">
+          {list.map((item) => (
+            <div key={item.code} className="List-item">
+              <div
+                className={"Item" + (item.selected ? " Item_selected" : "")}
+                onClick={() => store.selectItem(item.code)}
+              >
+                <div className="Item-code">{item.code}</div>
+                <div className="Item-title">
+                  {item.title}{" "}
+                  {item.totalSelected
+                    ? `| Выделяли ${item.totalSelected} раз`
+                    : ""}
+                </div>
+
+                <div className="Item-actions">
                   <button onClick={() => store.deleteItem(item.code)}>
                     Удалить
                   </button>
                 </div>
               </div>
             </div>
-          )}
+          ))}
         </div>
       </div>
     </div>
