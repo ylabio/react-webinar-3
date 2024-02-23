b75274a2e61b0a1d40d39d1ca87487dccde916d5
Илья Кочергин <ilia@list.ru>
2024-02-23 08:59:11 +0300

    fix: show zero counters

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ function App({store}) {
               <div className={'Item' + (item.selected ? ' Item_selected' : '')}
                    onClick={() => store.selectItem(item.code)}>
                 <div className='Item-code'>{item.code}</div>
-                <div className='Item-title'>{item.title + (item.selections > 0 ? ` | Выделяли ${item.selections} раз` : '')}</div>
+                <div className='Item-title'>{item.title + ' | ' + (item.selections > 0 ? `Выделяли ${item.selections} раз` : 'Не выделяли')}</div>
                 <div className='Item-actions'>
                   <button onClick={event => { event.stopPropagation(); store.deleteItem(item.code); return false; }}>
                     Удалить
