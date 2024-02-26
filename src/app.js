51f87d58d50921044f32bd14db1521aa973db61c
Vladimir_ubuntu <komlevladimir@gmail.com>
2024-02-26 16:57:28 +0300

    fix: окончания слова раз, выделение при удалении

--- src/app.js
+++ src/app.js
@@ -8,62 +8,48 @@ import './styles.css';
  * @returns {React.ReactElement}
  */
 function App({ store }) {
-	const list = store.getState().list;
-	// Функция для подсчета выделений элемента +1 каждый раз при выделении элемента
+  const list = store.getState().list;
 
-	// const countSelections = (code) => {
-	// 	const selectedItems = list.filter(
-	// 		(item) => item.code === code && item.selected
-	// 	);
-	// 	return selectedItems.length;
-	// };
-
-	return (
-		<div className="App">
-			<div className="App-head">
-				<h1>Приложение на чистом JS</h1>
-			</div>
-			<div className="App-controls">
-				<button onClick={() => store.addItem()}>Добавить</button>
-			</div>
-			<div className="App-center">
-				<div className="List">
-					{list.map((item) => (
-						<div key={item.code} className="List-item">
-							<div
-								className={
-									'Item' +
-									(item.selected ? ' Item_selected' : '')
-								}
-								onClick={() => store.selectItem(item.code)}
-							>
-								<div className="Item-code">{item.code}</div>
-								<div className="Item-title">
-									{item.title}{' '}
-									{store.getSelectionCount(item.code) > 0 && (
-										<span>
-											| Выделяли{' '}
-											{store.getSelectionCount(item.code)}{' '}
-											раз
-										</span>
-									)}
-								</div>
-								<div className="Item-actions">
-									<button
-										onClick={() =>
-											store.deleteItem(item.code)
-										}
-									>
-										Удалить
-									</button>
-								</div>
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='App'>
+      <div className='App-head'>
+        <h1>Приложение на чистом JS</h1>
+      </div>
+      <div className='App-controls'>
+        <button onClick={() => store.addItem()}>Добавить</button>
+      </div>
+      <div className='App-center'>
+        <div className='List'>
+          {list.map((item) => (
+            <div key={item.code} className='List-item'>
+              <div
+                className={'Item' + (item.selected ? ' Item_selected' : '')}
+                onClick={() => store.selectItem(item.code)}
+              >
+                <div className='Item-code'>{item.code}</div>
+                <div className='Item-title'>
+                  {item.title}{' '}
+                  {store.getSelectionCount(item.code) > 0 && (
+                    <span>
+                      | Выделяли
+                      {store.getPluralizedForm(
+                        store.getSelectionCount(item.code)
+                      )}
+                    </span>
+                  )}
+                </div>
+                <div className='Item-actions'>
+                  <button onClick={() => store.deleteItem(item.code)}>
+                    Удалить
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/store.js
+++ src/store.js
@@ -2,115 +2,133 @@
  * Хранилище состояния приложения
  */
 class Store {
-	constructor(initState = {}) {
-		this.state = initState;
-		this.listeners = []; // Слушатели изменений состояния
-		this.deletedCodes = []; // Массив удаленных кодов
-		this.selectionCounts = {}; // Счетчик выделенных элементов
-	}
+  constructor(initState = {}) {
+    this.state = initState;
+    this.listeners = []; // Слушатели изменений состояния
+    this.deletedCodes = []; // Массив удаленных кодов
+    this.selectionCounts = {}; // Счетчик выделенных элементов
+    this.selectedItems = new Set(); // Множество выделенных элементов
+  }
 
-	/**
-	 * Подписка слушателя на изменения состояния
-	 * @param listener {Function}
-	 * @returns {Function} Функция отписки
-	 */
-	subscribe(listener) {
-		this.listeners.push(listener);
-		// Возвращается функция для удаления добавленного слушателя
-		return () => {
-			this.listeners = this.listeners.filter((item) => item !== listener);
-		};
-	}
+  /**
+   * Подписка слушателя на изменения состояния
+   * @param listener {Function}
+   * @returns {Function} Функция отписки
+   */
+  subscribe(listener) {
+    this.listeners.push(listener);
+    // Возвращается функция для удаления добавленного слушателя
+    return () => {
+      this.listeners = this.listeners.filter((item) => item !== listener);
+    };
+  }
 
-	/**
-	 * Выбор состояния
-	 * @returns {Object}
-	 */
-	getState() {
-		return this.state;
-	}
+  /**
+   * Выбор состояния
+   * @returns {Object}
+   */
+  getState() {
+    return this.state;
+  }
 
-	/**
-	 * Установка состояния
-	 * @param newState {Object}
-	 */
-	setState(newState) {
-		this.state = newState;
-		// Вызываем всех слушателей
-		for (const listener of this.listeners) listener();
-	}
+  /**
+   * Установка состояния
+   * @param newState {Object}
+   */
+  setState(newState) {
+    this.state = newState;
+    // Вызываем всех слушателей
+    for (const listener of this.listeners) listener();
+  }
 
-	/**
-	 * Генератор уникального кода
-	 */
-	generateNewCode() {
-		const itemCount = this.state.list.length + this.deletedCodes.length;
-		return itemCount + 1;
-	}
+  /**
+   * Генератор уникального кода
+   */
+  generateNewCode() {
+    const itemCount = this.state.list.length + this.deletedCodes.length;
+    return itemCount + 1;
+  }
 
-	/**
-	 * Добавление новой записи с новым уникальным кодом
-	 */
-	addItem() {
-		let newCode = this.generateNewCode();
+  /**
+   * Добавление новой записи с новым уникальным кодом
+   */
+  addItem() {
+    let newCode = this.generateNewCode();
 
-		this.setState({
-			...this.state,
-			list: [
-				...this.state.list,
-				{
-					code: newCode,
-					title: 'Новая запись',
-				},
-			],
-		});
-	}
+    this.setState({
+      ...this.state,
+      list: [
+        ...this.state.list,
+        {
+          code: newCode,
+          title: 'Новая запись',
+        },
+      ],
+    });
+  }
 
-	/**
-	 * Удаление записи по коду
-	 * @param code
-	 */
-	deleteItem(code) {
-		// Добавляем удаленный код в массив удаленных кодов
-		this.deletedCodes.push(code);
+  /**
+   * Удаление записи по коду
+   * @param code
+   */
+  deleteItem(code) {
+    this.deletedCodes.push(code);
 
-		this.setState({
-			...this.state,
-			list: this.state.list.filter((item) => item.code !== code),
-		});
-	}
+    this.state.list = this.state.list.filter((item) => item.code !== code);
 
-	/**
-	 * Выделение записи по коду
-	 * @param code
-	 */
+    if (this.selectedItems.has(code)) {
+      this.selectedItems.delete(code);
+    }
 
-	selectItem(code) {
-		this.setState({
-			...this.state,
-			list: this.state.list.map((item) => {
-				if (item.code === code) {
-					item.selected = !item.selected;
+    this.setState({...this.state});
+  }
 
-					// Увеличиваем количество выделений при выделении элемента
-					if (item.selected) {
-						this.selectionCounts[code] =
-							(this.selectionCounts[code] || 0) + 1;
-					}
-				} else {
-					item.selected = false;
-				}
-				return item;
-			}),
-		});
-	}
+  /**
+   * Выделение записи по коду
+   * @param code
+   */
 
-	/**
-	 * Получение количества выделений для заданной записи
-	 */
-	getSelectionCount(code) {
-		return this.selectionCounts[code] || 0;
-	}
+  selectItem(code) {
+    const item = this.state.list.find((item) => item.code === code);
+    if (!item) return;
+
+    item.selected = !item.selected;
+
+    if (item.selected) {
+      for (const otherItem of this.state.list) {
+        if (otherItem.code !== code && otherItem.selected) {
+          otherItem.selected = false;
+          this.selectedItems.delete(otherItem.code);
+        }
+      }
+      this.selectedItems.add(code);
+      this.selectionCounts[code] = (this.selectionCounts[code] || 0) + 1;
+    } else {
+      this.selectedItems.delete(code);
+    }
+
+    this.setState({...this.state});
+  }
+
+  /**
+   * Получение количества выделений для заданной записи
+   */
+  getSelectionCount(code) {
+    return this.selectionCounts[code] || 0;
+  }
+
+  getPluralizedForm(count) {
+    if (count % 10 === 1 && count % 100 !== 11) {
+      return ` ${count} раз`;
+    } else if (
+      [2, 3, 4].includes(count % 10) &&
+      ![12, 13, 14].includes(count % 100)
+    ) {
+      return ` ${count} раза`;
+    } else {
+      return ` ${count} раз`;
+    }
+  }
 }
 
 export default Store;
