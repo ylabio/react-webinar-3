f83476c2177e9dc28412f0856796d054493df5b2
Zakir Bikbov <zakir.bikbov@gmail.com>
2024-02-27 04:39:39 +0500

    Вернул для списка изначальные идентификаторы

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import Store from './store.js';
 
 const store = new Store({
   list: [
-    { code: 300, title: 'Название элемента' },
-    { code: 159, title: 'Некий объект' },
-    { code: 407, title: 'Заголовок' },
-    { code: 812, title: 'Очень длинное название элемента из семи слов' },
-    { code: 546, title: 'Запись' },
-    { code: 678, title: 'Шестая запись' },
-    { code: 235, title: 'Седьмая запись' },
+    { code: 1, title: 'Название элемента' },
+    { code: 2, title: 'Некий объект' },
+    { code: 3, title: 'Заголовок' },
+    { code: 4, title: 'Очень длинное название элемента из семи слов' },
+    { code: 5, title: 'Запись' },
+    { code: 6, title: 'Шестая запись' },
+    { code: 7, title: 'Седьмая запись' },
   ]
 });
 
