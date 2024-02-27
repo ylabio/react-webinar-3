2c919502c626d0bdac33865edac0b52eb82ec9e8
batalovmv <batalov94@gmail.com>
2024-02-27 18:35:00 +0600

    исправил регистр буквы Р, исправил структуру в соответствии с макетом

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createElement} from './utils.js';
+import { createElement } from './utils.js';
 import './styles.css';
 
 /**
@@ -7,57 +7,54 @@ import './styles.css';
  * @param store {Store} Состояние приложения
  * @returns {React.ReactElement}
  */
-function App({store}) {
+function App({ store }) {
 
     function getPluralForm(n) {
         let remainder10 = n % 10;
         let remainder100 = n % 100;
 
         if (remainder10 === 1 && remainder100 !== 11) {
-            return 'Раз';
+            return 'раз';
         } else if (remainder10 >= 2 && remainder10 <= 4 && (remainder100 < 10 || remainder100 >= 20)) {
-            return 'Раза';
+            return 'раза';
         } else {
-            return 'Раз';
+            return 'раз';
         }
     }
 
-  const list = store.getState().list;
+    const list = store.getState().list;
 
-  return (
-    <div className='App'>
-      <div className='App-head'>
-        <h1>Приложение на чистом JS</h1>
-      </div>
-      <div className='App-controls'>
-        <button onClick={() => store.addItem()}>Добавить</button>
-      </div>
-      <div className='App-center'>
-        <div className='List'>{
-          list.map(item =>
-            <div key={item.code} className='List-item'>
-              <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                   onClick={() => store.selectItem(item.code)}>
-                <div className='Item-code'>{item.code}</div>
-                <div className='Item-title'>{item.title}</div>
-
-               {item.count > 0 && <div className='Item-title'>| Выделяли {item.count} {getPluralForm(item.count)}</div>}
-
-                <div className='Item-actions'>
-                          <button onClick={(e) => {
-                              e.stopPropagation();
-                              store.deleteItem(item.code);
-                          }}>
-                    Удалить
-                  </button>
-                </div>
-              </div>
+    return (
+        <div className='App'>
+            <div className='App-head'>
+                <h1>Приложение на чистом JS</h1>
             </div>
-          )}
+            <div className='App-controls'>
+                <button onClick={() => store.addItem()}>Добавить</button>
+            </div>
+            <div className='App-center'>
+                <div className='List'>{
+                    list.map(item =>
+                        <div key={item.code} className='List-item'>
+                            <div className={'Item' + (item.selected ? ' Item_selected' : '')}
+                                onClick={() => store.selectItem(item.code)}>
+                                <div className='Item-code'>{item.code}</div>
+                                <div className='Item-title'>{item.title} {item.count > 0 && ` | Выделяли ${item.count} ${getPluralForm(item.count)}`} </div>
+                                <div className='Item-actions'>
+                                    <button onClick={(e) => {
+                                        e.stopPropagation();
+                                        store.deleteItem(item.code);
+                                    }}>
+                                        Удалить
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
