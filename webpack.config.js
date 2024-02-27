e986c4a369518d26305cdc24349fc84ab85a6435
Egor Krasilnikov <krasilnikov19@gmail.com>
2024-02-27 11:17:31 +0300

    returned dist to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ let config = {
 if (process.env.NODE_ENV === 'development') {
   config.devtool = 'inline-source-map';
   config.devServer = {
-    static: path.join(__dirname, 'public'),
+    static: path.join(__dirname, 'dist'),
     port: 8010,
     historyApiFallback: true,
   };
