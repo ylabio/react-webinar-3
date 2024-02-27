b3bc3105fd2e41e0eaa2e1a963188c17e48ca9ce
ktydg <annatars@ktydg>
2024-02-27 22:20:02 +0300

    Styles went to hell again

--- src/app.js
+++ src/app.js
@@ -1,28 +1,29 @@
-import React from "react";
-import { createElement } from "./utils.js";
-import "./styles.css";
-import Item from "./components/Item.jsx";
+import React from 'react';
+import {createElement} from './utils.js';
+import './styles.css';
+import Item from './components/Item.jsx';
 
 /**
  * Приложение
  * @param store {Store} Состояние приложения
  * @returns {React.ReactElement}
  */
-function App({ store }) {
+function App({store}) {
+
   const list = store.getState().list;
 
   return (
-    <div className="App">
-      <div className="App-head">
+    <div className='App'>
+      <div className='App-head'>
         <h1>Приложение на чистом JS</h1>
       </div>
-      <div className="App-controls">
+      <div className='App-controls'>
         <button onClick={() => store.addItem()}>Добавить</button>
       </div>
-      <div className="App-center">
-        <div className="List">
+      <div className='App-center'>
+        <div className='List'>
           {list.map((item) => (
-            <Item
+            <Item 
               key={item.code}
               onSelectItem={store.selectItem.bind(store)}
               onDeleteItem={store.deleteItem.bind(store)}
