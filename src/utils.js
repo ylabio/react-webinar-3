a237100ae8e8069f1ed917a564714f0a21095b4a
Oikapi <seryy.gordeev.78@mail.ru>
2024-02-27 21:02:13 +0700

    Fix plural-form

--- src/utils.js
+++ src/utils.js
@@ -28,11 +28,12 @@ export function createElement(name, props = {}, ...children) {
 }
 
 export function getPluralForm(count) {
+
   if (count === 1) {
-      return count + " раз";
-  } else if (count >= 2 && count <= 4) {
-      return count + " раза";
-  } else {
-      return count + " раз";
-  }
+    return count + " раз";
+} else if (count % 10 >= 2 && count % 10 <= 4 && (count % 100 < 10 || count % 100 >= 20)) {
+    return count + " раза";
+} else {
+    return count + " раз";
+}
 }
