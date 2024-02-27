a35fc134dce1d573aa3c5cee9136fa8035449482
Mila Gvozdeva <103929930+KaSofi@users.noreply.github.com>
2024-02-27 13:20:01 +0300

    Update utils.js
    plurals added

--- src/utils.js
+++ src/utils.js
@@ -34,3 +34,16 @@ export const codeId = (() => {
       return id ++;
   };
 })();
+
+
+// plurals для слова раз/раза
+export function morph(numberOfSelects) {
+  if (numberOfSelects % 10 === 1 && numberOfSelects % 100 !== 11) {
+    return 'раз'
+  } else if (numberOfSelects % 10 >= 2 && numberOfSelects % 10 <= 4 && (numberOfSelects % 100 < 10 || numberOfSelects % 100 >= 20)) {
+    return 'раза'
+  } else {
+    return 'раз'
+  }
+}
+console.log(morph(1055))
