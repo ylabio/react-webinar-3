eebdf7bace720ec17e2267fe732d4abdc61e9e66
CrossCanon <145831866+CrossCanon@users.noreply.github.com>
2024-02-28 15:50:23 +0400

    Добавил склонения для количества

--- src/store.js
+++ src/store.js
@@ -89,6 +89,20 @@ class Store {
       })
     })
   }
+  
+  countForm(count) {
+    if (count % 10 === 1 && count % 100 !== 11) {
+      return ` ${count} раз`;
+    } else if (
+      [2, 3, 4].includes(count % 10) &&
+      ![12, 13, 14].includes(count % 100)
+    ) {
+      return ` ${count} раза`;
+    } else {
+      return ` ${count} раз`;
+    }
+  }
+  
 }
 
 export default Store;
