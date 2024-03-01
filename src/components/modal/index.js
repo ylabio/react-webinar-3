97120215da7dd18c671999aa0a28e7c8de129db4
batalovmv <batalov94@gmail.com>
2024-03-02 02:21:14 +0600

    добавил стандартное закрытие на модалку

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -4,11 +4,15 @@ import Item from "../item";
 import './style.css';
 import  Controls  from "../controls";
 
-function Modal({ isOpen, children }) {
+function Modal({ isOpen, children, onClose }) {
     if (!isOpen) return null;
-
+    const handleBackdropClick = (event) => {
+        if (event.target === event.currentTarget) {
+            onClose(); 
+        }
+    };
     return (
-        <div className="modal">
+        <div className="modal" onClick={handleBackdropClick}>
             <div className="modal-content">
                 {children}
             </div>
