c903dfbc966130259c0d3695ce41eec29f85f25a
Илья Кочергин <ilia@list.ru>
2024-03-01 19:26:31 +0300

    remove: items selection

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -5,16 +5,7 @@ import './style.css';
 
 function Item(props) {
 
-  // Счётчик выделений
-  const [count, setCount] = useState(0);
-
   const callbacks = {
-    onClick: () => {
-      props.onSelect(props.item.code);
-      if (!props.item.selected) {
-        setCount(count + 1);
-      }
-    },
     onDelete: (e) => {
       e.stopPropagation();
       props.onDelete(props.item.code);
@@ -23,15 +14,11 @@ function Item(props) {
   }
 
   return (
-    <div className={'Item' + (props.item.selected ? ' Item_selected' : '')}
+    <div className={'Item'}
          onClick={callbacks.onClick}>
       <div className='Item-code'>{props.item.code}</div>
       <div className='Item-title'>
-        {props.item.title} {count ? ` | Выделяли ${count} ${plural(count, {
-        one: 'раз',
-        few: 'раза',
-        many: 'раз'
-      })}` : ''}
+        {props.item.title}
       </div>
       <div className='Item-actions'>
         <button onClick={callbacks.onDelete}>
@@ -45,18 +32,13 @@ function Item(props) {
 Item.propTypes = {
   item: PropTypes.shape({
     code: PropTypes.number,
-    title: PropTypes.string,
-    selected: PropTypes.bool,
-    count: PropTypes.number
+    title: PropTypes.string
   }).isRequired,
-  onDelete: PropTypes.func,
-  onSelect: PropTypes.func
+  onDelete: PropTypes.func
 };
 
 Item.defaultProps = {
   onDelete: () => {
-  },
-  onSelect: () => {
   },
 }
 
