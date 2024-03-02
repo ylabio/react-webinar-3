3a533af8db61b87f87cfef238cc914131f758f5a
dableeet <krasilnikov19@gmail.com>
2024-03-02 22:53:15 +0300

    start

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,21 +1,37 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import './style.css';
+import PropTypes from "prop-types";
+import { cn as bem } from "@bem-react/classname";
+import "./style.css";
+import { plural } from "../../utils";
 
-function Controls({onAdd}) {
+function Controls({ onClick, itemsCount, price }) {
+  const cn = bem("Controls");
+
   return (
-    <div className='Controls'>
-      <button onClick={() => onAdd()}>Добавить</button>
+    <div className={cn()}>
+      <p className={cn("cart-info")}>
+        В корзине:
+        <span className={cn("cart-info", "status")}>
+          {!itemsCount
+            ? "Пусто"
+            : `${itemsCount} ${plural(itemsCount, {
+                one: "товар",
+                few: "товара",
+                many: "товаров",
+              })} / ${price} ₽`}
+        </span>
+      </p>
+      <button onClick={onClick}>Перейти</button>
     </div>
-  )
+  );
 }
 
 Controls.propTypes = {
-  onAdd: PropTypes.func
+  onClick: PropTypes.func,
 };
 
 Controls.defaultProps = {
-  onAdd: () => {}
-}
+  onClick: () => {},
+};
 
 export default React.memo(Controls);
