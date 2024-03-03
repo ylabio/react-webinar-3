e4ddb649fa7ee60fea911a8d30e0cd6ca76f346d
Alexander Revenko <revenko.a@bk.ru>
2024-03-03 22:53:42 +0700

    Изменил компонент Head

--- src/components/head/index.js
+++ src/components/head/index.js
@@ -1,17 +1,18 @@
-import React from "react";
 import PropTypes from "prop-types";
-import './style.css';
+import React from "react";
+import "./style.css";
 
-function Head({title}) {
+function Head({ title, children }) {
   return (
-    <div className='Head'>
+    <div className="Head">
       <h1>{title}</h1>
+      <div className="Head-content">{children}</div>
     </div>
-  )
+  );
 }
 
-Head.propTypes = {
-  title: PropTypes.node,
+Head.propsType = {
+  title: PropTypes.node.isRequired,
 };
 
 export default React.memo(Head);
