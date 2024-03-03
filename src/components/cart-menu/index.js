429a9007631de0575a0e2c9e069ca3b9dc8bd4c8
Alexander Revenko <revenko.a@bk.ru>
2024-03-03 23:02:45 +0700

    Сделал компонент CartMenu

---
+++ src/components/cart-menu/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./style.css";
+
+function CartMenu({ cartList, onCartOpen }) {
+  const count = cartList.length;
+
+  const sum = cartList.reduce((acc, product) => {
+    return (acc += (product.price * product.count));
+  }, 0);
+
+  const value = cartList.length ? `${count} товара / ${sum}` : 'пусто'
+
+  return (
+    <div className="CartMenu">
+      <span className="CartMenu-text">В корзине:</span>
+      <span className="CartMenu-value">{value}</span>
+      <button className="CartMenu-btn" onClick={onCartOpen}>Перейти</button>
+    </div>
+  );
+}
+
+CartMenu.propTypes = {
+  cartList: PropTypes.arrayOf(PropTypes.shape({
+    price: PropTypes.number.isRequired,
+    count: PropTypes.number.isRequired,
+  })).isRequired,
+  onCartOpen: PropTypes.func.isRequired
+}
+
+
+export default CartMenu;
