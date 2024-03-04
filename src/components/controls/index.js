4c48079fa9507c6e743923e441853e333aaefdbe
Danil Dyubin <danildyubin@MacBook-Air-Danil.local>
2024-03-04 10:51:35 +0300

    Not finished! Need more time

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,21 +1,33 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import './style.css';
+import PropTypes from "prop-types";
+import "./style.css";
 
-function Controls({onAdd}) {
+function Controls({ onAdd }) {
   return (
-    <div className='Controls'>
-      <button onClick={() => onAdd()}>Добавить</button>
+    <div className="Controls">
+      {/* <button onClick={() => onAdd()}>Добавить</button> */}
+      <div className="Controls-cart">В корзине:</div>
+      <div className="Controls-count">
+        {/* {count
+          ? ` | Выделяли ${count} ${plural(count, {
+              one: "товар",
+              few: "товара",
+              many: "товаров",
+            })}`
+          : ""} */}
+        2 товара / 223 ₽
+      </div>
+      <button className="Controls-button">Перейти</button>
     </div>
-  )
+  );
 }
 
 Controls.propTypes = {
-  onAdd: PropTypes.func
+  onAdd: PropTypes.func,
 };
 
 Controls.defaultProps = {
-  onAdd: () => {}
-}
+  onAdd: () => {},
+};
 
 export default React.memo(Controls);
---
+++ src/components/modal/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+const ModalWindow = () => {
+  return (
+    <div className="modal">
+      <div className="content"></div>
+    </div>
+  );
+};
