f7ff94f219bfc8a28235363533ab5a0bb6dff3ec
Vladimir_ubuntu <komlevladimir@gmail.com>
2024-03-04 12:35:30 +0300

    add: price, cart

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -5,40 +5,35 @@ import './style.css';
 
 function Item(props) {
 
-  // Счётчик выделений
-  const [count, setCount] = useState(0);
-
   const callbacks = {
-    onClick: () => {
-      props.onSelect(props.item.code);
-      if (!props.item.selected) {
-        setCount(count + 1);
-      }
-    },
-    onDelete: (e) => {
+    // onClick: () => {
+    //   props.onSelect(props.item.code);
+    //   if (!props.item.selected) {
+    //     setCount(count + 1);
+    //   }
+    // },
+    // onDelete: (e) => {
+    //   e.stopPropagation();
+    //   props.onDelete(props.item.code);
+    // },
+    onAddToCart: (e) => {
       e.stopPropagation();
-      props.onDelete(props.item.code);
-
+      props.onAddToCart(props.item);
     }
   }
 
   return (
-    <div className={'Item' + (props.item.selected ? ' Item_selected' : '')}
-         onClick={callbacks.onClick}>
+    <div className='Item'>
       <div className='Item-code'>{props.item.code}</div>
       <div className='Item-title'>
-        {props.item.title} {count ? ` | Выделяли ${count} ${plural(count, {
-        one: 'раз',
-        few: 'раза',
-        many: 'раз'
-      })}` : ''}
+        {props.item.title}
       </div>
+          <div className='Item-price'>{props.item.price} ₽</div>
       <div className='Item-actions'>
-        <button onClick={callbacks.onDelete}>
-          Удалить
-        </button>
+        <button onAddToCart={callbacks.onAddToCart}>Добавить</button>
       </div>
     </div>
+
   );
 }
 
@@ -46,17 +41,21 @@ Item.propTypes = {
   item: PropTypes.shape({
     code: PropTypes.number,
     title: PropTypes.string,
-    selected: PropTypes.bool,
+    // selected: PropTypes.bool,
+    price: PropTypes.number,
     count: PropTypes.number
   }).isRequired,
-  onDelete: PropTypes.func,
-  onSelect: PropTypes.func
+  onAddToCart: PropTypes.func
+  // onDelete: PropTypes.func,
+  // onSelect: PropTypes.func
 };
 
 Item.defaultProps = {
-  onDelete: () => {
-  },
-  onSelect: () => {
+  // onDelete: () => {
+  // },
+  // onSelect: () => {
+  // },
+  onAddToCart: () => {
   },
 }
 
