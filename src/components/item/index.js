f1226a0f9191d229fea3b7df6dcb70c52172a466
Junior Programmer <progphyton@gmail.com>
2024-03-04 12:18:23 +0300

    refactor: item component

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,41 +1,31 @@
-import React, {useState} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import {plural} from "../../utils";
 import './style.css';
 
+/* COMPONENTS TODO:
+component item:
+ - component button */
+
 function Item(props) {
 
-  // Счётчик выделений
-  const [count, setCount] = useState(0);
-
-  const callbacks = {
-    onClick: () => {
-      props.onSelect(props.item.code);
-      if (!props.item.selected) {
-        setCount(count + 1);
-      }
-    },
+/*   const callbacks = {
     onDelete: (e) => {
       e.stopPropagation();
       props.onDelete(props.item.code);
 
     }
-  }
+  } */
 
   return (
-    <div className={'Item' + (props.item.selected ? ' Item_selected' : '')}
-         onClick={callbacks.onClick}>
+    <div className='Item'>
       <div className='Item-code'>{props.item.code}</div>
-      <div className='Item-title'>
-        {props.item.title} {count ? ` | Выделяли ${count} ${plural(count, {
-        one: 'раз',
-        few: 'раза',
-        many: 'раз'
-      })}` : ''}
-      </div>
+      <div className='Item-title'>{props.item.title}</div>
+      <div className='Item-price'>{`${props.item.price} ₽`}</div>
       <div className='Item-actions'>
-        <button onClick={callbacks.onDelete}>
-          Удалить
+        {/* callbacks.onDelete */}
+        <button className='Item-button' onClick={() => null}>
+          Добавить
         </button>
       </div>
     </div>
@@ -46,18 +36,15 @@ Item.propTypes = {
   item: PropTypes.shape({
     code: PropTypes.number,
     title: PropTypes.string,
-    selected: PropTypes.bool,
-    count: PropTypes.number
+    price: PropTypes.number
   }).isRequired,
-  onDelete: PropTypes.func,
-  onSelect: PropTypes.func
 };
 
-Item.defaultProps = {
+/* Item.defaultProps = {
   onDelete: () => {
   },
   onSelect: () => {
   },
-}
+} */
 
 export default React.memo(Item);
