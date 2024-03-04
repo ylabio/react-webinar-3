be54fe9efc42cf056661e4bb06c8e312fa71ec84
Junior Programmer <progphyton@gmail.com>
2024-03-04 21:33:22 +0300

    refactor: head component to reusable

--- src/components/head/index.js
+++ src/components/head/index.js
@@ -2,16 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import './style.css';
 
-function Head({title}) {
+function Head({tag = 'h1', title, children}) {
   return (
     <div className='Head'>
-      <h1>{title}</h1>
+      {tag === 'h1' ? <h1>{title}</h1> : <h2>{title}</h2>}
+      <div className="Head-content">
+        {children ? children : null}
+      </div>
     </div>
   )
 }
 
 Head.propTypes = {
+  tag: PropTypes.string,
   title: PropTypes.node,
+  children: PropTypes.node,
 };
 
 export default React.memo(Head);
