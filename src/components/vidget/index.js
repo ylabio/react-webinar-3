c688f5f2027027ee887d8d435038a74489386e2b
Junior Programmer <progphyton@gmail.com>
2024-03-05 10:30:50 +0300

    feat: add vidget component

---
+++ src/components/vidget/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+import './style.css';
+
+function Vidget({cart}) {
+  return (
+    <div className='Vidget'>
+      <div className='Vidget-title'>В корзине:
+        <strong className='Vidget-inform'>
+          {cart ? 'что-то' : 'пусто'}
+        </strong>
+      </div>
+    </div>
+  )
+}
+
+// Typechecking with PropTypes:
+Vidget.propTypes = {
+  cart: PropTypes.shape({
+    goods: PropTypes.number,
+    costs: PropTypes.number
+  }),
+};
+
+export default React.memo(Vidget);
