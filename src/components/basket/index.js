71e3e4d4c17b39c79c131fd1c345b7ce5aab4a81
Илья Кочергин <ilia@list.ru>
2024-03-05 19:03:38 +0300

    feat: modal with embedded head

--- src/components/basket/index.js
+++ src/components/basket/index.js
@@ -10,20 +10,16 @@ import './style.css';
  */
 function Basket(props) {
   return (
-    <Modal>
-      <Head title='Корзина'>
-        <button className='action' onClick={props.onHideBasket}>
-          Закрыть
-        </button>
-      </Head>
-      <div className='Basket-body'>{
-        props.basket.length > 0 
-          ? <List list={props.basket}
-                  options={{showCount: true, showTotals: true, isAppendable: false, isDeletable: true}}
-                  onDeleteItem={props.onDeleteItem}/>
-          : <div className='Basket-empty'>В корзине пусто</div>
-        }
-      </div>
+    <Modal
+      title='Корзина'
+      onHide={props.onHideBasket}
+    >
+      {props.basket.length > 0 
+        ? <List list={props.basket}
+                options={{showCount: true, showTotals: true, isAppendable: false, isDeletable: true}}
+                onDeleteItem={props.onDeleteItem}/>
+        : <div className='Basket-empty'>В корзине пусто</div>
+      }
     </Modal>
   )
 }
