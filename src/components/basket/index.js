1edb0a09ad5df248ab7fc5bf3950eef0f3e38669
Vampir-kolik <157666599+Vampir-kolik@users.noreply.github.com>
2024-03-05 21:51:30 +0300

    куликов николай дз_2 (исправления) №5

--- src/components/basket/index.js
+++ src/components/basket/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-//import Modal from "react-overlays/Modal";
 import PropTypes from 'prop-types';
 import "./style.css";
 import Head from "../head";
@@ -9,8 +8,7 @@ import BottomItem from "../bottom-item";
 
 function ModalApp({showModal,handleClose,list,listBasket,onFunc,action,onAmountPrice}) {
 
-  //const renderBackdrop = (props) => <div className="container" {...props} />;
-  const Modal = ({ show }) => {
+ const Modal = ({ show }) => {
     return (
       <>
        <div
@@ -38,29 +36,6 @@ function ModalApp({showModal,handleClose,list,listBasket,onFunc,action,onAmountP
       </>
     );
   };
-
-  /*const element = <Modal
-  //className={classNameModal}
-  //show={showModal}
-  //onHide={handleClose}
-  //renderBackdrop={renderBackdrop}
->
-<PageLayout page={action}>
-  <div className='LayoutTop'>
-  <Head title='Корзина' orange={true}/>
-  <div className='Close'>
-  <button className='Button-shop' onClick={handleClose}>
-    Закрыть
-  </button>
-  </div>
-  </div>
-  <List list={list}
-        listBasket={listBasket}
-        onFunc={onFunc}
-        action={action}/>
-  {<BottomItem onAmountPrice={onAmountPrice}/>}
-</PageLayout>
-</Modal>*/
 
   return (
     <Modal show={showModal} />
