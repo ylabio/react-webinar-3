33be2d21ea8a61756294168bbccaa1b17af9a7cc
Junior Programmer <progphyton@gmail.com>
2024-03-06 10:00:50 +0300

    refactor: vidget as reusable component

--- src/components/vidget/index.js
+++ src/components/vidget/index.js
@@ -3,14 +3,22 @@ import PropTypes from "prop-types";
 import { plural, format } from '../../utils';
 import './style.css';
 
-function Vidget({cart}) {
+function Vidget({cart, title, full = true}) {
+
   const plForm = plural(cart.goods, { one: 'товар', few: 'товара', many: 'товаров' });
   const numForm = format(cart.costs);
+
   return (
     <div className='Vidget'>
-      <div className='Vidget-title'>В корзине:
+      <div className='Vidget-title'>
+          {title}
         <strong className='Vidget-inform'>
-          {Boolean(cart.goods) ? `${cart.goods} ${plForm} / ${numForm} ₽` : 'пусто'}
+          {full && <span className='Vidget-goods'>
+            {Boolean(cart.goods) ? `${cart.goods} ${plForm} / ${numForm} ₽` : 'пусто'}
+          </span>}
+          {!full && <span className='Vidget-costs'>
+            {Boolean(cart.goods) ? `${numForm} ₽` : '0 ₽'}
+          </span>}
         </strong>
       </div>
     </div>
@@ -23,6 +31,14 @@ Vidget.propTypes = {
     goods: PropTypes.number,
     costs: PropTypes.number
   }).isRequired,
+  title: PropTypes.string,
+  full: PropTypes.bool,
 };
+
+// Default values for properties:
+Vidget.defaultProps = {
+  title: "В корзине:",
+  full: true,
+}
 
 export default React.memo(Vidget);
