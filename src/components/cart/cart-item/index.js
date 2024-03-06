57792b7a9625a076955574657964eca8b83ab726
VladimirrAndreevich <110813805+VladimirrAndreevich@users.noreply.github.com>
2024-03-07 02:50:23 +0400

    style(format): convert tabs to spaces

--- src/components/cart/cart-item/index.js
+++ src/components/cart/cart-item/index.js
@@ -1,35 +1,44 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import "./style.css";
 import { formatPrice } from "../../../utils";
 
-const CartItem = ({id, itemData, onDelete}) => {
-	const priceContent = itemData.totalPrice === 0 ? "бесплатно" : `${formatPrice(itemData.totalPrice)} ₽`;
+const CartItem = ({ id, itemData, onDelete }) => {
+  const priceContent =
+    itemData.totalPrice === 0
+      ? "бесплатно"
+      : `${formatPrice(itemData.totalPrice)} ₽`;
 
-	return (
-		<li className="CartItem">
-			<span className="CartItem-number">{id}</span>
-			<p className="CartItem-title">{itemData.title}</p>
-			<span className="CartItem-price">{priceContent}</span>
-			<span className="CartItem-count">{itemData.quantity} шт</span>
+  return (
+    <li className="CartItem">
+      <span className="CartItem-number">{id}</span>
+      <p className="CartItem-title">{itemData.title}</p>
+      <span className="CartItem-price">{priceContent}</span>
+      <span className="CartItem-count">{itemData.quantity} шт</span>
 
-			<button onClick={() => { onDelete() }}>Удалить</button>
-		</li>
-	);
-}
+      <button
+        onClick={() => {
+          onDelete();
+        }}
+      >
+        Удалить
+      </button>
+    </li>
+  );
+};
 
 CartItem.propTypes = {
-	id: PropTypes.number,
-	itemData: PropTypes.shape({
+  id: PropTypes.number,
+  itemData: PropTypes.shape({
     price: PropTypes.number,
-		title: PropTypes.string,
-		quantity: PropTypes.number
+    title: PropTypes.string,
+    quantity: PropTypes.number,
   }).isRequired,
-	onDelete: PropTypes.func
+  onDelete: PropTypes.func,
 };
 
 CartItem.defaultProps = {
-  onDelete: () => {}
-}
+  onDelete: () => {},
+};
 
 export default React.memo(CartItem);
--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -1,43 +1,52 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import "./style.css";
 import CartItem from "./cart-item";
 import { formatPrice } from "../../utils";
 
 function Cart({ list, onDeleteCartItem, totalCartPrice }) {
-	if(list.length === 0) {
-		return <h3 className="Cart-warning">Корзинка пуста!</h3>;
-	}
+  if (list.length === 0) {
+    return <h3 className="Cart-warning">Корзинка пуста!</h3>;
+  }
 
-	const totalCartPriceContent = `${formatPrice(totalCartPrice)} ₽`;
+  const totalCartPriceContent = `${formatPrice(totalCartPrice)} ₽`;
 
-	return (
-		<>
-			<ul className="Cart-list">
-				{list.map((item, index) => 
-					<CartItem key={index} id={ index + 1 } itemData={item} onDelete={() => { onDeleteCartItem(item.code) }}/>
-					)}
-			</ul>
+  return (
+    <>
+      <ul className="Cart-list">
+        {list.map((item, index) => (
+          <CartItem
+            key={index}
+            id={index + 1}
+            itemData={item}
+            onDelete={() => {
+              onDeleteCartItem(item.code);
+            }}
+          />
+        ))}
+      </ul>
 
-			<p className="Cart-total">
-				Итого <span>{totalCartPriceContent}</span>
-			</p>
-		</>
-	);
+      <p className="Cart-total">
+        Итого <span>{totalCartPriceContent}</span>
+      </p>
+    </>
+  );
 }
 
 Cart.propTypes = {
   onDeleteCartItem: PropTypes.func,
-	list: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string,
-		price: PropTypes.number
-  })),
-	totalCartPrice: PropTypes.number
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      price: PropTypes.number,
+    })
+  ),
+  totalCartPrice: PropTypes.number,
 };
 
 Cart.defaultProps = {
   onDeleteCartItem: () => {},
-	totalCartPrice: 0
-}
+  totalCartPrice: 0,
+};
 
 export default React.memo(Cart);
