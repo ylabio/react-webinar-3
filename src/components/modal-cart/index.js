d26f2e78692d84908df426437418a6618499ee96
TolkinSL <tolkinsl@yandex.ru>
2024-03-07 12:52:18 +0300

    LES-02 фик скролл в Корзине на items

--- src/components/modal-cart/index.js
+++ src/components/modal-cart/index.js
@@ -1,30 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {cn as bem} from "@bem-react/classname";
+import { cn as bem } from "@bem-react/classname";
 import "./style.css";
 import List from "../list";
-import {localeNum} from "../../utils";
+import { localeNum } from "../../utils";
 
-function ModalCart({store, onClick}) {
+function ModalCart({ store, onClick }) {
 
   const cn = bem("ModalCart");
   const storeList = store.getState().storeList;
 
   return (
-    <div className={cn()}>
-      <div className={cn("space")}></div>
-      <List list={storeList}
-            onClick={onClick} btnName={"Удалить"} cart={true} />
+    <>
+      <div className={cn()}>
+        <div className={cn("space")}></div>
+        <List list={storeList}
+              onClick={onClick} btnName={"Удалить"} cart={true} />
+      </div>
       <div className={cn("header")}>
         <p className={cn("bold_span")}>Итого</p>
         <p className={cn("bold_span", "price")}>{`${localeNum(store.getCartPrice())} ₽`}</p>
       </div>
-    </div>
+    </>
   );
 }
 
 ModalCart.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node
 };
 
 export default ModalCart;
