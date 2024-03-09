61dc9a96cdc1b4b75b231ddf59cec70d8f6071a6
Zakir Bikbov <zakir.bikbov@gmail.com>
2024-03-09 19:19:47 +0500

    Вынес pagination из list-а в main, переработал логику, избавился от хука useState

--- src/app/main/index.js
+++ src/app/main/index.js
@@ -6,6 +6,7 @@ import BasketTool from "../../components/basket-tool";
 import List from "../../components/list";
 import useStore from "../../store/use-store";
 import useSelector from "../../store/use-selector";
+import Pagination from '../../components/pagination';
 
 function Main() {
 
@@ -18,6 +19,7 @@ function Main() {
   const select = useSelector(state => ({
     list: state.catalog.list,
     count: state.catalog.count,
+    currentPage: state.catalog.currentPage,
     amount: state.basket.amount,
     sum: state.basket.sum
   }));
@@ -27,6 +29,8 @@ function Main() {
     addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store]),
     // Открытие модалки корзины
     openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store]),
+    // Переключение страницы
+    changePage: useCallback(page => store.actions.catalog.load(page), [store])
   }
 
   const renders = {
@@ -40,7 +44,12 @@ function Main() {
       <Head title='Магазин' />
       <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount}
         sum={select.sum} />
-      <List list={select.list} count={select.count} renderItem={renders.item} />
+      <List list={select.list} renderItem={renders.item} />
+      <Pagination
+        count={select.count}
+        currentPage={select.currentPage}
+        onPageChange={callbacks.changePage}
+      />
     </PageLayout>
 
   );
--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,34 +1,16 @@
-import { memo, useState } from "react";
+import { memo } from "react";
 import PropTypes from 'prop-types';
-import Pagination from "../pagination";
 import './style.css';
-import useStore from "../../store/use-store";
 
-function List({ list, count, renderItem }) {
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const store = useStore();
-
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-    store.actions.catalog.load(page);
-  };
-
+function List({ list, renderItem }) {
   return (
-    <>
-      <div className='List'>{
-        list.map(item =>
-          <div key={item._id} className='List-item'>
-            {renderItem(item)}
-          </div>
-        )}
-      </div>
-      <Pagination
-        count={count}
-        currentPage={currentPage}
-        onPageChange={handlePageChange}
-      />
-    </>
+    <div className='List'>{
+      list.map(item =>
+        <div key={item._id} className='List-item'>
+          {renderItem(item)}
+        </div>
+      )}
+    </div>
   )
 }
 
