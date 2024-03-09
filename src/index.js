b33fbafaf9d007d189bd7d38b0f0ebbae92e0ef5
aleksander1802 <aleksander-18-02@yandex.ru>
2024-03-09 04:04:49 +0300

    fix: fixed path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './app';
 import LanguageProvider from "./language-provider"; 
 import Store from "./store";
 import { StoreContext } from "./store/context";
