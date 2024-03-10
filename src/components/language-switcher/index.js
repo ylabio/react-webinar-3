36adb3a61e4f60b4b0121e3070600764b20e5902
SpiritWin <misha14ark@mail.ru>
2024-03-11 00:15:15 +0300

    added style lang btm

--- src/components/language-switcher/index.js
+++ src/components/language-switcher/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useLanguage } from '../../store/language-context';
+import './style.css';
+import {cn as bem} from "@bem-react/classname";
 
 const LanguageSwitcher = () => {
     const { switchLanguage } = useLanguage();
 
+    const cn = bem('LanguageSwitcher');
+
     return (
-        <div>
+        <div className={cn()}>
             <button onClick={switchLanguage}>Switch Language</button>
         </div>
     );
