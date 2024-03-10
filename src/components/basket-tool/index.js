373cef5bb80f0db55aae07ba1a7a98985a279d56
SpiritWin <misha14ark@mail.ru>
2024-03-10 13:57:28 +0300

    add link from home page

--- src/components/basket-tool/index.js
+++ src/components/basket-tool/index.js
@@ -3,23 +3,27 @@ import PropTypes from 'prop-types';
 import {cn as bem} from '@bem-react/classname';
 import {numberFormat, plural} from "../../utils";
 import './style.css';
+import { Link } from "react-router-dom";
 
 function BasketTool({sum, amount, onOpen}) {
   const cn = bem('BasketTool');
   return (
     <div className={cn()}>
-      <span className={cn('label')}>В корзине:</span>
-      <span className={cn('total')}>
-        {amount
-          ? `${amount} ${plural(amount, {
-            one: 'товар',
-            few: 'товара',
-            many: 'товаров'
-          })} / ${numberFormat(sum)} ₽`
-          : `пусто`
-        }
-      </span>
-      <button onClick={onOpen}>Перейти</button>
+      <Link className={cn('link')} to="/">Главная</Link>
+      <div className={cn('action')}>
+        <span className={cn('label')}>В корзине:</span>
+        <span className={cn('total')}>
+          {amount
+            ? `${amount} ${plural(amount, {
+              one: 'товар',
+              few: 'товара',
+              many: 'товаров'
+            })} / ${numberFormat(sum)} ₽`
+            : `пусто`
+          }
+        </span>
+        <button onClick={onOpen}>Перейти</button>
+      </div>
     </div>
   );
 }
