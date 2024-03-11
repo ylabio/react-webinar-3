7b0354ac79e739e1a2785cd510a618d32c65ae1a
Junior Programmer <progphyton@gmail.com>
2024-03-11 12:40:25 +0300

    feat: add the paging to the main

--- src/app/main/index.js
+++ src/app/main/index.js
@@ -6,27 +6,37 @@ import BasketTool from "../../components/basket-tool";
 import List from "../../components/list";
 import useStore from "../../store/use-store";
 import useSelector from "../../store/use-selector";
+import Paging from "../../components/paging";
 
 function Main() {
 
   const store = useStore();
 
-  useEffect(() => {
-    store.actions.catalog.load();
-  }, []);
-
   const select = useSelector(state => ({
+    skip: state.paging.skip,
+    limit: state.paging.limit,
+    page: state.paging.page,
+    avail: state.catalog.avail,
     list: state.catalog.list,
     amount: state.basket.amount,
     sum: state.basket.sum
   }));
+
+  useEffect(() => {
+    store.actions.catalog.avail();
+    store.actions.catalog.load(select.skip, select.limit);
+  }, [select.skip, select.limit]);
 
   const callbacks = {
     // Добавление в корзину
     addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store]),
     // Открытие модалки корзины
     openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store]),
-  }
+    // change page
+    changePage: useCallback((page) => store.actions.paging.gotoPage(page), [store]),
+    // setup limit
+    setupLimit: useCallback((limit) => store.actions.paging.setLimit(limit), [store]),
+  };
 
   const renders = {
     item: useCallback((item) => {
@@ -40,6 +50,12 @@ function Main() {
       <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount}
                   sum={select.sum}/>
       <List list={select.list} renderItem={renders.item}/>
+      <Paging changePage={callbacks.changePage}
+              setupLimit={callbacks.setupLimit}
+              dataPaging={{page: select.page,
+                          limit: select.limit,
+                          avail: select.avail}}
+            />
     </PageLayout>
 
   );
