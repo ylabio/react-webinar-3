9cca7a39a3d1be870bd43e409a0178049f85fdc1
Alina Potapchuk <alinapotapchuk7@gmail.com>
2024-03-11 15:16:42 +0300

    all done

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -4,28 +4,69 @@ import './style.css';
 import useSelector from "../../store/use-selector";
 
 function Pagination({ total, limit, onPageChange, currentPage}) {
-  const countPages = Math.ceil(total/limit);
-  const pages = [];
-  //const pages = total.slice(0, 10);
-  for (let i = 1; i <= countPages; i++) {
-    pages.push(i);
+  const totalPages = Math.ceil(total/limit);
+
+  const onPageClick = (item) => {
+    onPageChange(item);
   };
 
-  const onPageClick = (event) => {
-    event.stopPropagation();
-    onPageChange(event.target.innerHTML);
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+
+    // Если активная страница <= 2, отображаем 1,2,3,...последняя
+    if (currentPage <= 2) {
+      for (let i = 1; i <= Math.min(3, totalPages); i++) {
+        pageNumbers.push(renderPageButton(i));
+      }
+
+      if (totalPages > 2) {
+        pageNumbers.push(<span key="ellipsis1">...</span>);
+        pageNumbers.push(renderPageButton(totalPages));
+      }
+      // Если активная страница === 3, отображаем 1,2,3,4...последняя
+    } else if (currentPage === 3) {
+      for (let i = 1; i <= Math.min(4, totalPages); i++) {
+        pageNumbers.push(renderPageButton(i));
+      }
+
+      if (totalPages > 4) {
+        pageNumbers.push(<span key="ellipsis1">...</span>);
+        pageNumbers.push(renderPageButton(totalPages));
+      }
+    } else {
+      // Если активная страница !== 3, отображаем 1,...страница перед, активная, страница после,...последняя
+      pageNumbers.push(renderPageButton(1));
+      pageNumbers.push(<span key="ellipsis2">...</span>);
+
+      const start = Math.max(1, currentPage - 1);
+      const end = Math.min(currentPage + 1, totalPages);
+
+      for (let i = start; i <= end; i++) {
+        pageNumbers.push(renderPageButton(i));
+      }
+
+      if (end < totalPages) {
+        pageNumbers.push(<span key="ellipsis3">...</span>);
+        pageNumbers.push(renderPageButton(totalPages));
+      }
+    }
+
+    return pageNumbers;
   };
+
+  const renderPageButton = (pageNumber) => (
+    <div
+      key={pageNumber}
+      onClick={() => onPageClick(pageNumber)}
+      className={`Pagination-item ${currentPage === pageNumber ? 'Pagination-item-active' : ''}`}
+    >
+      {pageNumber}
+    </div>
+  );
 
   return (
     <div className='Pagination'>
-      {
-      pages.map(item =>
-        <div onClick={onPageClick}
-          // key={item} className='Pagination-item'>
-          key={item} className={`Pagination-item ${currentPage === item ? 'Pagination-item-active' : ''}`}>
-          {item}
-        </div>
-      )}
+      {renderPageNumbers()}
     </div>
   )
 }
