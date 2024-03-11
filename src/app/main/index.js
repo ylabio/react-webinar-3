c798b91fbab8089d6adef8e921a8fa742fdbc4b7
batalovmv <batalov94@gmail.com>
2024-03-11 14:48:31 +0600

    добавил базовую логику пагинации

--- src/app/main/index.js
+++ src/app/main/index.js
@@ -6,17 +6,21 @@ import BasketTool from "../../components/basket-tool";
 import List from "../../components/list";
 import useStore from "../../store/use-store";
 import useSelector from "../../store/use-selector";
+import { useState } from 'react';
 
 function Main() {
 
   const store = useStore();
+    const [limit, setLimit] = useState(2); 
+    const [skip, setSkip] = useState(0); 
 
-  useEffect(() => {
-    store.actions.catalog.load();
-  }, []);
+    useEffect(() => {
+        store.actions.catalog.load({ limit, skip });
+    }, [limit, skip, store.actions.catalog]);
 
   const select = useSelector(state => ({
     list: state.catalog.list,
+    isLastPage: state.catalog.isLastPage,
     amount: state.basket.amount,
     sum: state.basket.sum
   }));
@@ -26,6 +30,12 @@ function Main() {
     addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store]),
     // Открытие модалки корзины
     openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store]),
+      nextPage: useCallback(() => {
+          setSkip(skip + limit); 
+      }, [skip, limit]),
+      prevPage: useCallback(() => {
+          setSkip(Math.max(0, skip - limit));
+      }, [skip, limit])
   }
 
   const renders = {
@@ -40,6 +50,11 @@ function Main() {
       <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount}
                   sum={select.sum}/>
       <List list={select.list} renderItem={renders.item}/>
+          
+          <div>
+              {skip > 0 && <button onClick={callbacks.prevPage}>Предыдущая страница</button>}
+              {!select.isLastPage && <button onClick={callbacks.nextPage}>Следующая страница</button>}
+          </div>
     </PageLayout>
 
   );
--- src/store/catalog/index.js
+++ src/store/catalog/index.js
@@ -14,14 +14,18 @@ class Catalog extends StoreModule {
     }
   }
 
-  async load() {
-    const response = await fetch('/api/v1/articles');
-    const json = await response.json();
-    this.setState({
-      ...this.getState(),
-      list: json.result.items
-    }, 'Загружены товары из АПИ');
-  }
+    async load({ limit = 10, skip = 0 } = {}) {
+        const url = `/api/v1/articles?limit=${limit}&skip=${skip}`;
+        const response = await fetch(url);
+        const json = await response.json();
+        const items = json.result.items;
+        const isLastPage = items.length < limit;
+        this.setState({
+            ...this.getState(),
+            list: items,
+            isLastPage: isLastPage,
+        }, 'Загружены товары из АПИ с пагинацией');
+    }
 }
 
 export default Catalog;
