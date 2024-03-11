6ea222e42e792603b8b3d34e6d66c0d63a9d538e
ste1v0 <tyotarjous@yandex.ru>
2024-03-11 22:50:09 +0300

    Create separate comp for item details

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,8 @@
-import {useCallback, useContext, useEffect, useState} from 'react';
 import Main from "./main";
 import Basket from "./basket";
-import useStore from "../store/use-store";
 import useSelector from "../store/use-selector";
 import { Routes, Route } from 'react-router-dom';
-import PageLayout from '../components/page-layout';
-import Head from '../components/head';
-import BasketTool from '../components/basket-tool';
-import { useLocation } from 'react-router-dom';
-import Item from '../components/item';
+import ItemDetails from '../components/item-details';
 
 /**
  * Приложение
@@ -16,38 +10,7 @@ import Item from '../components/item';
  */
 function App() {
 
-  const store = useStore();
-
-  const location = useLocation()
-
-  const [currentItem, setCurrentItem] = useState({})
-
-  const select = useSelector(state => ({
-    list: state.catalog.list,
-    amount: state.basket.amount,
-    sum: state.basket.sum
-  }));
-
-  const callbacks = {
-    // Добавление в корзину
-    addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store]),
-    // Открытие модалки корзины
-    openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store]),
-  }
-
   const activeModal = useSelector(state => state.modals.name);
-
-  useEffect(() => {
-    const pathname = location.pathname;
-    const state = location.state
-    console.log(location.state)  
-    if (pathname.includes('item')) {
-      store.actions.modals.close('basket')
-      fetch(`/api/v1/articles/${state}?fields=*,madeIn(title,code),category(title)`)
-        .then(res => res.json())
-        .then(data => setCurrentItem(data.result))
-    }
-  }, [location]);
 
   return (
     <>
@@ -59,14 +22,7 @@ function App() {
         </>
         }/>
         <Route path="item/:id" 
-          element={
-          <PageLayout>
-            <Head title={currentItem.title}/>
-            <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount} sum={select.sum}/>
-            <Item item={currentItem} onAdd={callbacks.addToBasket}/>
-            {activeModal === 'basket' && <Basket/>}
-          </PageLayout>
-          }
+          element={<ItemDetails activeModal={activeModal}/>}
           />
       </Routes>
     </>
