d222e0bd1920a5321b73298fda8f669c4a95d0b3
ТАЗДМБ ТАЗДМБ <fhdbudhh@gmail.com>
2024-03-12 10:24:15 +0300

    feat: added home button to basket-tool

--- src/components/product-data/index.js
+++ src/components/product-data/index.js
@@ -12,10 +12,10 @@ export function ProductData(props) {
 
   return (
     <div className={cn()}>
-      <p>{props.item.description}</p>
-      <p>Страна производитель: <span className={cn('text', {highlighted: true})}>{`${props.item.madeIn.title} (${props.item.madeIn.code})`}</span></p>
-      <p>Категория: <span className={cn('text', {highlighted: true})}>{`${props.item.category.title}`}</span></p>
-      <p>Год выпуска: <span className={cn('text', {highlighted: true})}>{`${props.item.edition}`}</span></p>
+      <p className={cn('text')}>{props.item.description}</p>
+      <p className={cn('text')}>Страна производитель: <span className={cn('text', {highlighted: true})}>{`${props.item.madeIn.title} (${props.item.madeIn.code})`}</span></p>
+      <p className={cn('text')}>Категория: <span className={cn('text', {highlighted: true})}>{`${props.item.category.title}`}</span></p>
+      <p className={cn('text')}>Год выпуска: <span className={cn('text', {highlighted: true})}>{`${props.item.edition}`}</span></p>
       <p className={cn('price')}>Цена: {props.item.price}</p>
       <button onClick={callbacks.onAdd}>Добавить</button>
     </div>
