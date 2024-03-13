b37b2e4dc5c76f5d0be9cad40f032433e84000f0
Junior Programmer <progphyton@gmail.com>
2024-03-13 15:13:26 +0300

    feat: add routing to app

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,8 @@
 import {useCallback, useContext, useEffect, useState} from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./main";
 import Basket from "./basket";
+import Goods from "./goods"
 import useStore from "../store/use-store";
 import useSelector from "../store/use-selector";
 
@@ -13,10 +15,15 @@ function App() {
   const activeModal = useSelector(state => state.modals.name);
 
   return (
-    <>
-      <Main/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/goods">
+          <Route path="/goods/:id" element={<Goods/>}/>
+        </Route>
+      </Routes>
       {activeModal === 'basket' && <Basket/>}
-    </>
+    </BrowserRouter>
   );
 }
 
