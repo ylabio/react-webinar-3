9a57ef9d9e7a70e1a00d7293a67902ee9c5ee329
Junior Programmer <progphyton@gmail.com>
2024-03-13 18:58:51 +0300

    feat: add component layout

--- src/components/details/index.js
+++ src/components/details/index.js
@@ -1,22 +1,44 @@
 import {memo} from "react";
 import PropTypes from "prop-types";
+import {cn as bem} from '@bem-react/classname';
 import './style.css';
 
 function Details({detail, onAdd}) {
+
+  const cn = bem('Details');
   console.log(detail);
+
   return (
-    <div className='Details'>
-      <p>Свойства товара</p>
-      <button onClick={() => onAdd()}>Добавить</button>
+    <div className={cn()}>
+      <p className={cn('description')}>{detail.description}</p>
+      <p className={cn('madein')}>Страна производитель:
+        <span className={cn('madein', {weight: 'bold'})}>
+          {detail.madeIn?.title} ({detail.madeIn?.code})
+        </span>
+      </p>
+      <p className={cn('category')}>Категория:
+        <span className={cn('category', {weight: 'bold'})}>
+          {detail.category?.title}
+        </span>
+      </p>
+      <p className={cn('edition')}>Год выпуска:
+        <span className={cn('edition', {weight: 'bold'})}>
+          {detail.edition}
+        </span>
+      </p>
+      <p className={cn('price')}>Цена: {detail.price} ₽</p>
+      <button onClick={() => onAdd(detail._id)}>Добавить</button>
     </div>
   )
 }
 
 Details.propTypes = {
+  detail: PropTypes.objectOf(PropTypes.any),
   onAdd: PropTypes.func.isRequired,
 };
 
 Details.defaultProps = {
+  detail: {},
   onAdd: () => {},
 }
 
