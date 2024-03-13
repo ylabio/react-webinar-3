695722361e4f954b8918d8ac5df7e9fae66b614c
Junior Programmer <progphyton@gmail.com>
2024-03-13 15:24:20 +0300

    refactor: the components with the routing

--- src/components/item-basket/index.js
+++ src/components/item-basket/index.js
@@ -1,4 +1,5 @@
 import {memo, useCallback} from 'react';
+import { Link } from "react-router-dom";
 import propTypes from 'prop-types';
 import {numberFormat} from "../../utils";
 import {cn as bem} from "@bem-react/classname";
@@ -10,13 +11,18 @@ function ItemBasket(props) {
   const cn = bem('ItemBasket');
 
   const callbacks = {
-    onRemove: (e) => props.onRemove(props.item._id)
+    onRemove: (e) => props.onRemove(props.item._id),
+    onClose: () => props.onClose(),
   };
 
   return (
     <div className={cn()}>
       <div className={cn('code')}>{props.item._key}</div>
-      <div className={cn('title')}>{props.item.title}</div>
+      <div className={cn('title')}>
+        <Link to={`/goods/${props.item._key}`} onClick={callbacks.onClose}>
+          {props.item.title}
+        </Link>
+      </div>
       <div className={cn('right')}>
         <div className={cn('cell')}>{numberFormat(props.item.price)} ₽</div>
         <div className={cn('cell')}>{numberFormat(props.item.amount || 0)} шт</div>
@@ -36,10 +42,12 @@ ItemBasket.propTypes = {
     amount: PropTypes.number
   }).isRequired,
   onRemove: propTypes.func,
+  onClose: PropTypes.func,
 }
 
 ItemBasket.defaultProps = {
   onRemove: () => {},
+  onClose: () => {},
 }
 
 export default memo(ItemBasket);
