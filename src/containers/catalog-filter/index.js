5cf24812404440cf4360b59339c6208b686991d3
Vladimir Shestakov <boolive@yandex.ru>
2024-03-14 20:21:49 +0300

    fix input

--- src/containers/catalog-filter/index.js
+++ src/containers/catalog-filter/index.js
@@ -57,7 +57,7 @@ function CatalogFilter() {
       <Select options={options.categories} value={select.category} onChange={callbacks.onCategory}/>
       <Select options={options.sort} value={select.sort} onChange={callbacks.onSort}/>
       <Input value={select.query} onChange={callbacks.onSearch} placeholder={'Поиск'}
-             delay={1000}/>
+             delay={1000} theme={'big'}/>
       <button onClick={callbacks.onReset}>{t('filter.reset')}</button>
     </SideLayout>
   )
