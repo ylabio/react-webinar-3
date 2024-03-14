178ac0d726f8a75efab60cb5c6f25382837a8345
ТАЗДМБ ТАЗДМБ <fhdbudhh@gmail.com>
2024-03-14 17:06:59 +0300

    feat: changed createBrowserTouter to <BrowserRouter/> and rebased routing into app component

--- src/app/index.js
+++ src/app/index.js
@@ -1,21 +1,26 @@
-import {useCallback, useContext, useEffect, useState} from 'react';
+import { useCallback, useContext, useEffect, useState } from "react";
 import Main from "./main";
 import Basket from "./basket";
 import useStore from "../store/use-store";
 import useSelector from "../store/use-selector";
+import { Routes, Route } from "react-router-dom";
+import { ProductPage } from "./product-page";
 
 /**
  * Приложение
  * @returns {React.ReactElement}
  */
 function App() {
-
-  const activeModal = useSelector(state => state.modals.name);
+  const activeModal = useSelector((state) => state.modals.name);
 
   return (
     <>
-      <Main/>
-      {activeModal === 'basket' && <Basket/>}
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/:current' element={<Main />} />
+        <Route path='/products/:productId' element={<ProductPage />} />
+      </Routes>
+      {activeModal === "basket" && <Basket />}
     </>
   );
 }
