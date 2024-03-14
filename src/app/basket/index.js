f7ed3689981a7d871373528a306ed21c20f7ee65
vlvch <107064521+vlvch@users.noreply.github.com>
2024-03-14 15:16:46 +0300

    Update index.js

--- src/app/basket/index.js
+++ src/app/basket/index.js
@@ -5,7 +5,7 @@ import ModalLayout from "../../components/modal-layout";
 import BasketTotal from "../../components/basket-total";
 import useStore from "../../store/use-store";
 import useSelector from "../../store/use-selector";
-import { locale } from '../../locale';
+import {locale} from '../../locale';
 
 function Basket({lang}) {
 
