00dfe9e60e3d91b015ec859bac7cd2bba1809cd1
Zakir Bikbov <zakir.bikbov@gmail.com>
2024-03-16 16:11:08 +0500

    Выполнил первое задание - реализовал фильтр товаров по категории

--- src/store/catalog/index.js
+++ src/store/catalog/index.js
@@ -15,6 +15,7 @@ class CatalogState extends StoreModule {
       params: {
         page: 1,
         limit: 10,
+        category: 'all',
         sort: 'order',
         query: ''
       },
@@ -34,9 +35,10 @@ class CatalogState extends StoreModule {
     let validParams = {};
     if (urlParams.has('page')) validParams.page = Number(urlParams.get('page')) || 1;
     if (urlParams.has('limit')) validParams.limit = Math.min(Number(urlParams.get('limit')) || 10, 50);
+    if (urlParams.has('category')) validParams.category = urlParams.get('category');
     if (urlParams.has('sort')) validParams.sort = urlParams.get('sort');
     if (urlParams.has('query')) validParams.query = urlParams.get('query');
-    await this.setParams({...this.initState().params, ...validParams, ...newParams}, true);
+    await this.setParams({ ...this.initState().params, ...validParams, ...newParams }, true);
   }
 
   /**
@@ -46,7 +48,7 @@ class CatalogState extends StoreModule {
    */
   async resetParams(newParams = {}) {
     // Итоговые параметры из начальных, из URL и из переданных явно
-    const params = {...this.initState().params, ...newParams};
+    const params = { ...this.initState().params, ...newParams };
     // Установка параметров и загрузка данных
     await this.setParams(params);
   }
@@ -58,7 +60,7 @@ class CatalogState extends StoreModule {
    * @returns {Promise<void>}
    */
   async setParams(newParams = {}, replaceHistory = false) {
-    const params = {...this.getState().params, ...newParams};
+    const params = { ...this.getState().params, ...newParams };
 
     // Установка новых параметров и признака загрузки
     this.setState({
@@ -83,6 +85,10 @@ class CatalogState extends StoreModule {
       sort: params.sort,
       'search[query]': params.query
     };
+
+    if (params.category !== 'all') {
+      apiParams['search[category]'] = params.category;
+    }
 
     const response = await fetch(`/api/v1/articles?${new URLSearchParams(apiParams)}`);
     const json = await response.json();
