99857c49d805db62688d0a698a672b6382134b88
Kubasultanov2005 <kubasultanov2005@gmail.com>
2024-03-17 19:06:53 +0300

    correction

--- src/store/catalog/index.js
+++ src/store/catalog/index.js
@@ -91,6 +91,7 @@ class CatalogState extends StoreModule {
     if (urlParams.has('limit')) validParams.limit = Math.min(Number(urlParams.get('limit')) || 10, 50);
     if (urlParams.has('sort')) validParams.sort = urlParams.get('sort');
     if (urlParams.has('query')) validParams.query = urlParams.get('query');
+    if (urlParams.has('category')) validParams.category = urlParams.get('category');
     await this.setParams({...this.initState().params, ...validParams, ...newParams}, true);
   }
 
