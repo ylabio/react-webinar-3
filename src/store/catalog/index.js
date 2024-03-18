8f119f4e6d6e4c2038d4bf1ab69c2f6221ceac07
TommyTheMob <zv3rbrus@gmail.com>
2024-03-18 14:36:24 +0300

    fix bug with sort

--- src/store/catalog/index.js
+++ src/store/catalog/index.js
@@ -37,7 +37,7 @@ class CatalogState extends StoreModule {
     if (urlParams.has('limit')) validParams.limit = Math.min(Number(urlParams.get('limit')) || 10, 50);
     if (urlParams.has('sort')) validParams.sort = urlParams.get('sort');
     if (urlParams.has('query')) validParams.query = urlParams.get('query');
-    if (urlParams.has('category')) validParams.query = urlParams.get('category');
+    if (urlParams.has('category')) validParams.category = urlParams.get('category');
     await this.setParams({...this.initState().params, ...validParams, ...newParams}, true);
   }
 
