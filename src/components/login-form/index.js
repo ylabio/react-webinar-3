569aaf430de6b14fea72ee13636457e1e9e18251
TommyTheMob <zv3rbrus@gmail.com>
2024-03-18 14:31:37 +0300

    add blur for login inputs

--- src/components/login-form/index.js
+++ src/components/login-form/index.js
@@ -7,19 +7,22 @@ const LoginForm = ({ onClick, error, t }) => {
   const [login, setLogin] = useState('')
   const [pass, setPass] = useState('')
 
+  const [loginBlur, setLoginBlur] = useState(false)
+  const [passBlur, setPassBlur] = useState(false)
+
   return (
     <SideLayout side='start' padding='medium'>
       <div className='LoginForm'>
         <span className='LoginForm-header'>{t('user.login')}</span>
         <div className='LoginForm-login'>
           <label htmlFor="user-login">{t('user.loginInput')}</label>
-          <input id='user-login' type="text" value={login} onChange={(e) => setLogin(e.target.value)}/>
+          <input id='user-login' type="text" value={login} onBlur={() => setLoginBlur(true)} onChange={(e) => setLogin(e.target.value)}/>
         </div>
         <div className='LoginForm-pass'>
           <label htmlFor="user-pass">{t('user.passInput')}</label>
-          <input id='user-pass' type="password" value={pass} onChange={(e) => setPass(e.target.value)}/>
+          <input id='user-pass' type="password" onBlur={() => setPassBlur(true)} value={pass} onChange={(e) => setPass(e.target.value)}/>
         </div>
-        {error &&
+        {error && (loginBlur || passBlur) &&
           <div className='LoginForm-error'>{error}</div>
         }
         <button className='LoginForm-btn' onClick={() => onClick(login, pass)}>{t('user.enter')}</button>
