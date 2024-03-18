35ec17c2f8c39dca78ea14cb3a8f63e65764d2fb
dableeet <krasilnikov19@gmail.com>
2024-03-18 12:04:20 +0300

    start

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,5 @@
-import {useCallback, useContext, useEffect, useState} from 'react';
-import {Routes, Route} from 'react-router-dom';
+import { useCallback, useContext, useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import useSelector from "../hooks/use-selector";
 import Main from "./main";
 import Basket from "./basket";
@@ -10,17 +10,18 @@ import Article from "./article";
  * Маршрутизация по страницам и модалкам
  */
 function App() {
+  const activeModal = useSelector((state) => state.modals.name);
 
-  const activeModal = useSelector(state => state.modals.name);
+  console.log("start");
 
   return (
     <>
       <Routes>
-        <Route path={''} element={<Main/>}/>
-        <Route path={'/articles/:id'} element={<Article/>}/>
+        <Route path={""} element={<Main />} />
+        <Route path={"/articles/:id"} element={<Article />} />
       </Routes>
 
-      {activeModal === 'basket' && <Basket/>}
+      {activeModal === "basket" && <Basket />}
     </>
   );
 }
