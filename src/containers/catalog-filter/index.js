da445471eddf14ac5ad931180856592fbf74e9c7
Илья Кочергин <ilia@list.ru>
2024-03-19 17:42:33 +0300

    style: search field customization

--- src/containers/catalog-filter/index.js
+++ src/containers/catalog-filter/index.js
@@ -57,7 +57,7 @@ function CatalogFilter() {
       <Select options={categories} value={select.category} onChange={callbacks.onSetCategory}/>
       <Select options={options.sort} value={select.sort} onChange={callbacks.onSort}/>
       <Input value={select.query} onChange={callbacks.onSearch} placeholder={'Поиск'}
-             delay={1000}/>
+             delay={1000} theme='big'/>
       <button onClick={callbacks.onReset}>{t('filter.reset')}</button>
     </SideLayout>
   )
