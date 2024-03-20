80b6a992caabb505f5782d81234c53838729ff2f
TommyTheMob <zv3rbrus@gmail.com>
2024-03-20 13:35:50 +0300

    fix: apply theme for search input

--- src/containers/catalog-filter/index.js
+++ src/containers/catalog-filter/index.js
@@ -46,7 +46,7 @@ function CatalogFilter() {
     <SideLayout padding='medium'>
       <Select options={select.categories} value={select.category} onChange={callbacks.onFilter} />
       <Select options={options.sort} value={select.sort} onChange={callbacks.onSort}/>
-      <Input value={select.query} onChange={callbacks.onSearch} placeholder={'Поиск'}
+      <Input theme={'big'} value={select.query} onChange={callbacks.onSearch} placeholder={'Поиск'}
              delay={1000}/>
       <button onClick={callbacks.onReset}>{t('filter.reset')}</button>
     </SideLayout>
