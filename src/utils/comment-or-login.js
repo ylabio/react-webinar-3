6d83f045be6a1ef7bf91d03911458c22f472addf
batalovmv <batalov94@gmail.com>
2024-03-28 01:44:06 +0600

    исправил редирект

--- src/utils/comment-or-login.js
+++ src/utils/comment-or-login.js
@@ -8,11 +8,14 @@ function IsLogin({ Component, componentProps, baseIndent = 0, level = 0, onCance
     const loginLink = "/login"; 
     const location = useLocation();
     const paddingLeft = baseIndent * level + 40;
+    
     const name = useSelector(state => state.session.user.profile?.name);
     if (!name) {
         return (
             <p className="link" style={{ paddingLeft }}>
-                <Link to={{ pathname: loginLink, state: { back: location.pathname } }}> 
+                <Link
+                    to={loginLink} state={{ back: location.pathname }}
+                >
                     {t('comments.loginToComment')}
                 </Link>, {t('comments.toComment')}
                 {onCancel && <button className="Comment-decly-button" type="button" onClick={onCancel}>{t('comments.cancel')}</button>}
