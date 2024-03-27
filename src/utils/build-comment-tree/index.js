191fd42bce6a35ee5aca1f45f7c49738ccaacd14
batalovmv <batalov94@gmail.com>
2024-03-27 21:15:53 +0600

    изменил порядок коммнтерияв от старых к новым

--- src/utils/build-comment-tree/index.js
+++ src/utils/build-comment-tree/index.js
@@ -5,12 +5,16 @@
  * @returns {Array} Корневые узлы
  */
 function buildCommentTree(comments) {
-    const rootComments = comments.filter(c => c.parent?._type === 'article');
+    const rootComments = comments
+        .filter(c => c.parent?._type === 'article')
+        .sort((a, b) => new Date(a.dateCreate) - new Date(b.dateCreate)); 
+
     const commentMap = {};
     comments.forEach(comment => {
         comment.replies = [];
         commentMap[comment._id] = comment;
     });
+
     comments.forEach(comment => {
         if (comment.parent && comment.parent._type === 'comment') {
             const parentComment = commentMap[comment.parent._id];
@@ -20,7 +24,12 @@ function buildCommentTree(comments) {
         }
     });
 
+    Object.values(commentMap).forEach(comment => {
+        comment.replies.sort((a, b) => new Date(a.dateCreate) - new Date(b.dateCreate));
+    });
+
     return rootComments;
 }
+
 export default buildCommentTree;
 
