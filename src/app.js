477055cf90c08f69a74690aebe6d5b88e91e2f35
Alex Tyuzhin <alexey.tyuzhin@gmail.com>
2025-03-19 17:55:39 +0200

    HW completed

--- src/app.js
+++ src/app.js
@@ -24,12 +24,22 @@ function App({ store }) {
             <div key={item.code} className="List-item">
               <div
                 className={'Item' + (item.selected ? ' Item_selected' : '')}
-                onClick={() => store.selectItem(item.code)}
+                onClick={e => store.selectItem(item.code, e)}
               >
                 <div className="Item-code">{item.code}</div>
-                <div className="Item-title">{item.title}</div>
+                <div className="Item-title">
+                  {item.title}
+                  {item.count > 0 && <span> (Выделяли {item.count} раз)</span>}
+                </div>
                 <div className="Item-actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+                  <button
+                    onClick={e => {
+                      store.deleteItem(item.code);
+                      e.stopPropagation(); //выделение не убирается
+                    }}
+                  >
+                    Удалить
+                  </button>
                 </div>
               </div>
             </div>
--- src/store.js
+++ src/store.js
@@ -35,16 +35,24 @@ class Store {
   setState(newState) {
     this.state = newState;
     // Вызываем всех слушателей
-    for (const listener of this.listeners) listener();
+    // for (const listener of this.listeners) listener();
+    this.listeners.forEach(listener => listener());
   }
 
   /**
    * Добавление новой записи
    */
   addItem() {
+    const newItem = {
+      code: Math.floor(Math.random() * 1000), //уникальный код
+      title: 'Новая запись',
+      selected: false,
+      count: 0,
+    };
+
     this.setState({
       ...this.state,
-      list: [...this.state.list, { code: this.state.list.length + 1, title: 'Новая запись' }],
+      list: [...this.state.list, newItem],
     });
   }
 
@@ -63,14 +71,23 @@ class Store {
    * Выделение записи по коду
    * @param code
    */
-  selectItem(code) {
+  selectItem(code, e) {
     this.setState({
       ...this.state,
       list: this.state.list.map(item => {
         if (item.code === code) {
-          item.selected = !item.selected;
+          const isSelected = !item.selected;
+
+          // возвращение нового объект
+          return {
+            ...item,
+            selected: isSelected,
+            count: isSelected ? (item.count || 0) + 1 : item.count,
+          };
         }
-        return item;
+
+        // если элемент не выбран, то обновляем его в зависимости от ctrl/metaKey
+        return e.ctrlKey || e.metaKey ? item : { ...item, selected: false };
       }),
     });
   }
