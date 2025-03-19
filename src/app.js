0debdc135b36147f187b7202d683851ae2bd639d
Denis <den141delta@gmail.com>
2025-03-19 21:47:21 +0800

    Сделана 1 лекция - 1, 2, 3 задачи + изменена верстка

--- src/app.js
+++ src/app.js
@@ -8,36 +8,53 @@ import './styles.css';
  * @returns {React.ReactElement}
  */
 function App({ store }) {
-  const list = store.getState().list;
+    const list = store.getState().list;
 
-  return (
-    <div className="App">
-      <div className="App-head">
-        <h1>Приложение на чистом JS</h1>
-      </div>
-      <div className="App-controls">
-        <button onClick={() => store.addItem()}>Добавить</button>
-      </div>
-      <div className="App-center">
-        <div className="List">
-          {list.map(item => (
-            <div key={item.code} className="List-item">
-              <div
-                className={'Item' + (item.selected ? ' Item_selected' : '')}
-                onClick={() => store.selectItem(item.code)}
-              >
-                <div className="Item-code">{item.code}</div>
-                <div className="Item-title">{item.title}</div>
-                <div className="Item-actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
-                </div>
-              </div>
+    return (
+        <div className="App">
+            <div className="App-head">
+                <h1>Приложение на чистом JS</h1>
             </div>
-          ))}
+            <div className="App-controls">
+                <button onClick={() => store.addItem()}>Добавить</button>
+            </div>
+            <div className="App-center">
+                <div className="List">
+                    {list.map(item => (
+                        <div key={item.code} className="List-item">
+                            <div
+                                className={'Item' + (item.selected ? ' Item_selected' : '')}
+                                onClick={(event) => {
+                                    store.selectItem(item.code, event.ctrlKey || event.metaKey);
+                                }}
+                            >
+                                <div className="Item-code">{item.code}</div>
+                                <div className="Item-title">
+                                    {item.title}
+                                    <span className="Item-title_count-highlighted">
+                                        {item.countH
+                                            ? ` | Выделяли ${item.countH} раз`
+                                            : ''
+                                        }
+                                    </span>
+                                </div>
+                                <div className="Item-actions">
+                                    <button
+                                        onClick={event => {
+                                            event.stopPropagation();
+                                            store.deleteItem(item.code);
+                                        }}
+                                    >
+                                        Удалить
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
