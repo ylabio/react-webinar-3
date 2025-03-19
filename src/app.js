10b45f8091dbfa19543a5b1defeba3702c6e1bb2
Iakov Zelenkov <iakovzelenkov@gmail.com>
2025-03-19 17:24:28 +0300

    feat: all tasks completed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { createElement } from './utils.js';
 import './styles.css';
 
 /**
@@ -15,21 +14,40 @@ function App({ store }) {
       <div className="App-head">
         <h1>Приложение на чистом JS</h1>
       </div>
-      <div className="App-controls">
-        <button onClick={() => store.addItem()}>Добавить</button>
-      </div>
       <div className="App-center">
+        <div className="App-controls">
+          <button className="App-button button" onClick={() => store.addItem()}>
+            Добавить
+          </button>
+        </div>
+
         <div className="List">
           {list.map(item => (
             <div key={item.code} className="List-item">
               <div
                 className={'Item' + (item.selected ? ' Item_selected' : '')}
-                onClick={() => store.selectItem(item.code)}
+                onClick={event => store.selectItem(item.code, event.ctrlKey || event.metaKey)}
               >
                 <div className="Item-code">{item.code}</div>
-                <div className="Item-title">{item.title}</div>
+                <div className="Item-title-container">
+                  <div className="Item-title">{item.title}</div>
+                  {item.selectionCount > 0 && (
+                    <>
+                      <span className="Item-separator">|</span>
+                      <div className="Item-selection-count">Выделяли {item.selectionCount} раз</div>
+                    </>
+                  )}
+                </div>
                 <div className="Item-actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+                  <button
+                    className="Item-button button"
+                    onClick={event => {
+                      store.deleteItem(item.code);
+                      event.stopPropagation();
+                    }}
+                  >
+                    Удалить
+                  </button>
                 </div>
               </div>
             </div>
