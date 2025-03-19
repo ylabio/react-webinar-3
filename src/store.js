5a8f971774c782189c7b4c9598dc127e997b0707
Konstantin <kostya.svetashov.01@gmail.com>
2025-03-19 19:07:03 +0400

    feat: add unique list code for new items

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@
  */
 class Store {
   constructor(initState = {}) {
-    this.state = initState;
+    this.state = { ...initState, lastCode: initState.list.length };
     this.listeners = []; // Слушатели изменений состояния
   }
 
@@ -44,7 +44,8 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, { code: this.state.list.length + 1, title: 'Новая запись' }],
+      list: [...this.state.list, { code: this.state.lastCode + 1, title: 'Новая запись' }],
+      lastCode: this.state.lastCode + 1,
     });
   }
 
