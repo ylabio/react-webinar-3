48c47b5f55e51559130490ac6a94086eaaa6a38d
SempaiDarcy <mr_darcy_1@mail.ru>
2025-03-20 00:22:18 +0400

    feat: updated layout and styles to match Figma design

--- src/app.js
+++ src/app.js
@@ -15,7 +15,9 @@ function App({ store }) {
         <h1>Приложение на чистом JS</h1>
       </div>
       <div className="App-controls">
-        <button onClick={() => store.addItem()}>Добавить</button>
+        <button className="button add" onClick={() => store.addItem()}>
+          Добавить
+        </button>
       </div>
       <div className="App-center">
         <div className="List">
@@ -23,22 +25,31 @@ function App({ store }) {
             <div key={item.code} className="List-item">
               <div
                 className={'Item' + (item.selected ? ' Item_selected' : '')}
+                style={{
+                  background: item.selected
+                    ? `rgba(107, 74, 203, ${Math.min(0.0314 + item.selectedCount * 0.0007, 0.1)})`
+                    : 'transparent',
+                }}
                 onClick={event => store.selectItem(item.code, event.ctrlKey || event.metaKey)}
               >
                 <div className="Item-code">{item.code}</div>
-                <div className="Item-title">{item.title}</div>
-                {item.selectedCount > 0 && (
-                  <div className="Item-selected-count">
-                    Выделяли {item.selectedCount}{' '}
-                    {item.selectedCount % 10 >= 2 &&
-                    item.selectedCount % 10 <= 4 &&
-                    !(item.selectedCount % 100 >= 12 && item.selectedCount % 100 <= 14)
-                      ? 'раза'
-                      : 'раз'}
-                  </div>
-                )}
+                <div className="Item-title">
+                  {item.title}
+                  {item.selectedCount > 0 && (
+                    <div className="Item-selected-count">
+                      | Выделяли {item.selectedCount}{' '}
+                      {item.selectedCount % 10 >= 2 &&
+                      item.selectedCount % 10 <= 4 &&
+                      !(item.selectedCount % 100 >= 12 && item.selectedCount % 100 <= 14)
+                        ? 'раза'
+                        : 'раз'}
+                    </div>
+                  )}
+                </div>
                 <div className="Item-actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+                  <button className="button delete" onClick={() => store.deleteItem(item.code)}>
+                    Удалить
+                  </button>
                 </div>
               </div>
             </div>
