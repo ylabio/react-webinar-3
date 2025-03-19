04ecac7910c95e63552f879fb3b8116ee074d3cb
SempaiDarcy <mr_darcy_1@mail.ru>
2025-03-19 18:14:16 +0400

    feat: implemented unique sequential code generation for records

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ class Store {
   constructor(initState = {}) {
     this.state = initState;
     this.listeners = []; // Слушатели изменений состояния
+    this.lastCode = initState.list.reduce((max, item) => Math.max(max, item.code), 0); // Запоминаем последний использованный код
   }
 
   /**
@@ -37,14 +38,14 @@ class Store {
     // Вызываем всех слушателей
     for (const listener of this.listeners) listener();
   }
-
   /**
-   * Добавление новой записи
+   * Добавление новой записи с уникальным кодом
    */
   addItem() {
+    this.lastCode += 1; // Увеличиваем глобальный счетчик
     this.setState({
       ...this.state,
-      list: [...this.state.list, { code: this.state.list.length + 1, title: 'Новая запись' }],
+      list: [...this.state.list, { code: this.lastCode, title: 'Новая запись' }],
     });
   }
 
