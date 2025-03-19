c892538613d3e6ead3a748c0ee52821d5df11a67
Kochkina Ekaterina <gudrum1983@gmail.com>
2025-03-19 19:44:22 +0100

    Реализовать верстку по макету

--- src/app.js
+++ src/app.js
@@ -13,25 +13,24 @@ function App({ store }) {
   return (
     <div className="App">
       <div className="App-head">
-        <h1>Приложение на чистом JS</h1>
+        <h1 className="App-title Well">Приложение на чистом JS</h1>
       </div>
-      <div className="App-controls">
-        <button onClick={() => store.addItem()}>Добавить</button>
+      <div className="App-controls Well">
+        <button onClick={() => store.addItem()} className="Button Btn-text">Добавить</button>
       </div>
-      <div className="App-center">
+      <div className="Well">
         <div className="List">
           {list.map(item => (
-            <div key={item.code} className="List-item">
               <div
-                className={'Item' + (item.selected ? ' Item_selected' : '')}
+                key={item.code}
+                className={'List-item Item' + (item.selected ? ' Item_selected' : '')}
                 onClick={() => store.selectItem(item.code)}
               >
                 <div className="Item-code">{item.code}</div>
-                <div className="Item-title">{item.title}</div>
+                <div className="Item-title">{item.title}{(item.code === 1 || item.code === 3) && <span className="Item-span"> | Выделяли 7 раз</span>}</div>
                 <div className="Item-actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+                  <button onClick={() => store.deleteItem(item.code)} className="Button Button_accent Btn-text">Удалить</button>
                 </div>
-              </div>
             </div>
           ))}
         </div>
