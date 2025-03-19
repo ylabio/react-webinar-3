91a8e9b831bbed4fa8162a6f8207a9c88e6dc159
Denis Kalkopf <ddenproton@protonmail.com>
2025-03-19 13:26:18 +0500

    refactor: add function getCountWordForm

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createElement } from './utils.js';
+import { createElement, getCountWordForm } from './utils.js';
 import './styles.css';
 
 /**
@@ -34,7 +34,12 @@ function App({ store }) {
                 <div className="Item-code">{item.code}</div>
                 <div className="Item-title">
                   {item.title}
-                  {item.selectedCount > 0 && <span> | Выделяли {item.selectedCount} раз</span>}
+                  {item.selectedCount > 0 && (
+                    <span>
+                      {' '}
+                      | Выделяли {item.selectedCount} {getCountWordForm(item.selectedCount)}
+                    </span>
+                  )}
                 </div>
 
                 <div className="Item-actions">
--- src/utils.js
+++ src/utils.js
@@ -26,3 +26,16 @@ export function createElement(name, props = {}, ...children) {
 
   return element;
 }
+
+export function getCountWordForm(count) {
+  const lastDigit = count % 10;
+  const lastTwoDigits = count % 100;
+
+  if (lastDigit === 1 && lastTwoDigits !== 11) {
+    return 'раз';
+  } else if ([2, 3, 4].includes(lastDigit) && ![12, 13, 14].includes(lastTwoDigits)) {
+    return 'раза';
+  } else {
+    return 'раз';
+  }
+}
