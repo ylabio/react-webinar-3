1cdd7518449703b2c01adb9dcc14d7f5cf6b9b76
Ilya Pridanov <ik.front2558988@gmail.com>
2025-03-20 20:33:31 +0300

    add: disign

--- src/app.js
+++ src/app.js
@@ -13,32 +13,36 @@ function App({ store }) {
   return (
     <div className="App">
       <div className="App-head">
-        <h1>Приложение на чистом JS</h1>
+        <div className="App-container">
+          <h1>Приложение на чистом JS</h1>
+        </div>
       </div>
-      <div className="App-controls">
-        <button onClick={() => store.addItem()}>Добавить</button>
-      </div>
-      <div className="App-center">
-        <div className="List">
-          {list.map(item => (
-            <div key={item.code} className="List-item">
-              <div
-                className={'Item' + (item.selected ? ' Item_selected' : '')}
-                onClick={(evt) => store.selectItem(item.code, evt)}
-              >
-                <div className="Item-code">{item.code}</div>
-                <div className="Item-title">
-                  {item.title}
-                  {item.sumSelection > 0 && (
-                    <span> | Выделяли {item.sumSelection} раз</span>
-                  )}
-                </div>
-                <div className="Item-actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+      <div className="App-container">
+        <div className="App-controls">
+          <button className="Button" onClick={() => store.addItem()}>Добавить</button>
+        </div>
+        <div className="App-center">
+          <div className="List">
+            {list.map(item => (
+              <div key={item.code} className="List-item">
+                <div
+                  className={'Item' + (item.selected ? ' Item_selected' : '')}
+                  onClick={(evt) => store.selectItem(item.code, evt)}
+                >
+                  <div className="Item-code">{item.code}</div>
+                  <div className="Item-title">
+                    {item.title}
+                    {item.sumSelection > 0 && (
+                      <span> | Выделяли {item.sumSelection} раз</span>
+                    )}
+                  </div>
+                  <div className="Item-actions">
+                    <button className="Button" onClick={() => store.deleteItem(item.code)}>Удалить</button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
