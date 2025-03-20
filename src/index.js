0999efb83bb914413f17b29407dc9b8ad3698d5e
maximqa20 <maks.karp96@yandex.ru>
2025-03-21 00:07:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import Store from './store.js';
 
 const store = new Store({
   list: [
-    { code: 1, title: 'Название элемента' },
-    { code: 2, title: 'Некий объект' },
-    { code: 3, title: 'Заголовок' },
-    { code: 4, title: 'Очень длинное название элемента из семи слов' },
-    { code: 5, title: 'Запись' },
-    { code: 6, title: 'Шестая запись' },
-    { code: 7, title: 'Седьмая запись' },
+    { code: 1, title: 'Название элемента', clicks: 0, selected: false },
+    { code: 2, title: 'Некий объект', clicks: 0, selected: false },
+    { code: 3, title: 'Заголовок', clicks: 0, selected: false },
+    { code: 4, title: 'Очень длинное название элемента из семи слов', clicks: 0, selected: false },
+    { code: 5, title: 'Запись', clicks: 0, selected: false },
+    { code: 6, title: 'Шестая запись', clicks: 0, selected: false },
+    { code: 7, title: 'Седьмая запись', clicks: 0, selected: false },
   ],
 });
 
