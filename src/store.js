8f6c3bb0ba68e1f41e28748995773c1815d8f962
Truffie <trufanov.vl2013@gmail.com>
2025-03-20 19:08:26 +0300

    fix: add new item counter

--- src/store.js
+++ src/store.js
@@ -45,7 +45,7 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, { code: ++this.lastCode, title: 'Новая запись' }],
+      list: [...this.state.list, { code: ++this.lastCode, title: 'Новая запись', counter: 0 }],
     });
   }
 
