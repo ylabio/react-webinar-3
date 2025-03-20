5d808c28cd3eb6cb251e6110b5b5c26d9161f078
Anton-Osip <osipchika@gmail.com>
2025-03-20 14:02:37 +0300

    feat: completed task2

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ class Store {
   constructor(initState = {}) {
     this.state = initState;
     this.listeners = []; // Слушатели изменений состояния
+    this.maxCode = Math.max(0, ...initState.list.map(item => item.code));
   }
 
   /**
@@ -44,7 +45,7 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, { code: this.state.list.length + 1, title: 'Новая запись' }],
+      list: [...this.state.list, { code: ++this.maxCode, title: 'Новая запись' }],
     });
   }
 
