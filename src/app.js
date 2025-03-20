af2f96da13b2bfa69bdf81d32e757d102ef0cb51
aceberg <1502200+aceberg@users.noreply.github.com>
2025-03-20 13:39:53 +0700

    task 1

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { createElement } from './utils.js';
+import "@fontsource/golos-text/700.css";
+import "@fontsource/golos-text/400.css";
+import "@fontsource/montserrat-alternates/700.css";
 import './styles.css';
 
 /**
@@ -15,21 +18,30 @@ function App({ store }) {
       <div className="App-head">
         <h1>Приложение на чистом JS</h1>
       </div>
-      <div className="App-controls">
-        <button onClick={() => store.addItem()}>Добавить</button>
-      </div>
       <div className="App-center">
+        <div className="App-controls">
+          <button className="Btn-add" onClick={() => store.addItem()}>Добавить</button>
+        </div>
         <div className="List">
-          {list.map(item => (
-            <div key={item.code} className="List-item">
+          {list.map((item, index) => (
+            <div key={index} className={'List-item' + (index % 2 === 0 ? ' Item-striped' : '')}>
               <div
                 className={'Item' + (item.selected ? ' Item_selected' : '')}
-                onClick={() => store.selectItem(item.code)}
+                onClick={(e) => store.selectItem(item.code, e.ctrlKey || e.metaKey)}
               >
                 <div className="Item-code">{item.code}</div>
-                <div className="Item-title">{item.title}</div>
+                <div className="Item-text">
+                  <div className="Item-title">{item.title}</div>
+                  {item.countSelect > 0
+                    ? <div>| Выделили {item.countSelect} раз</div>
+                    : <></>
+                  }
+                </div>
                 <div className="Item-actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+                  <button className="Btn-del" onClick={(e) => {
+                    e.stopPropagation();
+                    store.deleteItem(item.code)}}
+                  >Удалить</button>
                 </div>
               </div>
             </div>
