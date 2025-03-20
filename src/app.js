6bd6f07769b812b0aeb4c4766447a19e58d84acc
black3snake <black3snake@mail.ru>
2025-03-20 15:18:06 +0800

    done task1

--- src/app.js
+++ src/app.js
@@ -13,27 +13,37 @@ function App({ store }) {
   return (
     <div className="App">
       <div className="App-head">
-        <h1>Приложение на чистом JS</h1>
+        <div className="container">
+          <h1>Приложение на чистом JS</h1>
+        </div>
       </div>
       <div className="App-controls">
-        <button onClick={() => store.addItem()}>Добавить</button>
+        <div className="container">
+          <button onClick={() => store.addItem()}>Добавить</button>
+        </div>
       </div>
       <div className="App-center">
-        <div className="List">
-          {list.map(item => (
-            <div key={item.code} className="List-item">
-              <div
-                className={'Item' + (item.selected ? ' Item_selected' : '')}
-                onClick={() => store.selectItem(item.code)}
-              >
-                <div className="Item-code">{item.code}</div>
-                <div className="Item-title">{item.title}</div>
-                <div className="Item-actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+        <div className="container">
+          <div className="List">
+            {list.map(item => (
+              <div key={item.code} className="List-item">
+                <div
+                  className={'Item' + (item.selected ? ' Item_selected' : '')}
+                  onClick={e => store.selectItem(item.code, e)}
+                >
+                  <div className="Item-code">{item.code}</div>
+                  <div className="Item-title">
+                    {item.title } <span>
+                    {(item.count > 0 ? ` | Выделяли ${item.count} раз` : '')}
+                  </span>
+                  </div>
+                  <div className="Item-actions">
+                    <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
