be800d28b09a72581b331ccb032d02778929784c
maximqa20 <maks.karp96@yandex.ru>
2025-03-21 00:11:08 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,26 +10,59 @@ import './styles.css';
 function App({ store }) {
   const list = store.getState().list;
 
+  // Событие для отслеживания нажатия клавиш
+  function handleKeyDown(event) {
+    if (event.key === 'Control') {
+      document.body.classList.add('ctrl-pressed');
+    }
+  }
+
+  function handleKeyUp(event) {
+    if (event.key === 'Control') {
+      document.body.classList.remove('ctrl-pressed');
+    }
+  }
+
+  React.useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+
   return (
     <div className="App">
       <div className="App-head">
         <h1>Приложение на чистом JS</h1>
       </div>
       <div className="App-controls">
-        <button onClick={() => store.addItem()}>Добавить</button>
+        <div className="App-actions">
+          <button className="Add-button" onClick={() => store.addItem()}>Добавить</button>
+        </div>
       </div>
       <div className="App-center">
         <div className="List">
-          {list.map(item => (
-            <div key={item.code} className="List-item">
+          {list.map((item, index) => (
+            <div key={item.code} className={`List-item ${index % 2 === 1 ? '' : 'Tr-color'}`} onClick={() => store.clickOnRow(item.code)}>
+
               <div
                 className={'Item' + (item.selected ? ' Item_selected' : '')}
-                onClick={() => store.selectItem(item.code)}
+                onClick={() => store.selectItem(item.code, document.body.classList.contains('ctrl-pressed'))}
               >
                 <div className="Item-code">{item.code}</div>
-                <div className="Item-title">{item.title}</div>
+                <div className="Item-title">{item.title}
+                  {item.clicks > 0 && ( // Условие для отображения строки с количеством кликов
+                    item.clicks >= 2 && item.clicks <= 4 ? (
+                      <span className="Item-click"> | Выделяли {item.clicks} раза</span>
+                    ) : (
+                      <span className="Item-click"> | Выделяли {item.clicks} раз</span>
+                    )
+                  )}
+                </div>
                 <div className="Item-actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+                  <button className="Delete-button" onClick={() => store.deleteItem(item.code)}>Удалить</button>
                 </div>
               </div>
             </div>
@@ -41,3 +74,4 @@ function App({ store }) {
 }
 
 export default App;
+
