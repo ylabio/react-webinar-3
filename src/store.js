d7b8218b2b3fe2c7a32bd12fd8efc781baa697f1
Denis <den141delta@gmail.com>
2025-03-20 14:31:13 +0800

    back tab 2 spaces

--- src/store.js
+++ src/store.js
@@ -2,84 +2,84 @@
  * Хранилище состояния приложения
  */
 class Store {
-    constructor(initState = {}) {
-        this.state = initState;
-        this.listeners = []; // Слушатели изменений состояния
-    }
+  constructor(initState = {}) {
+    this.state = initState;
+    this.listeners = []; // Слушатели изменений состояния
+  }
 
-    /**
-     * Подписка слушателя на изменения состояния
-     * @param listener {Function}
-     * @returns {Function} Функция отписки
-     */
-    subscribe(listener) {
-        this.listeners.push(listener);
-        // Возвращается функция для удаления добавленного слушателя
-        return () => {
-            this.listeners = this.listeners.filter(item => item !== listener);
-        };
-    }
+  /**
+   * Подписка слушателя на изменения состояния
+   * @param listener {Function}
+   * @returns {Function} Функция отписки
+   */
+  subscribe(listener) {
+    this.listeners.push(listener);
+    // Возвращается функция для удаления добавленного слушателя
+    return () => {
+      this.listeners = this.listeners.filter(item => item !== listener);
+    };
+  }
 
-    /**
-     * Выбор состояния
-     * @returns {Object}
-     */
-    getState() {
-        return this.state;
-    }
+  /**
+   * Выбор состояния
+   * @returns {Object}
+   */
+  getState() {
+    return this.state;
+  }
 
-    /**
-     * Установка состояния
-     * @param newState {Object}
-     */
-    setState(newState) {
-        this.state = newState;
-        // Вызываем всех слушателей
-        for (const listener of this.listeners) listener();
-    }
+  /**
+   * Установка состояния
+   * @param newState {Object}
+   */
+  setState(newState) {
+    this.state = newState;
+    // Вызываем всех слушателей
+    for (const listener of this.listeners) listener();
+  }
 
-    /**
-     * Добавление новой записи
-     */
-    addItem() {
-        this.setState({
-            ...this.state,
-            list: [...this.state.list, { code: this.state.lastInd + 1, title: 'Новая запись'}],
-            lastInd: this.state.lastInd + 1,
-        });
-    }
+  /**
+   * Добавление новой записи
+   */
+  addItem() {
+    this.setState({
+      ...this.state,
+      list: [...this.state.list, { code: this.state.lastInd + 1, title: 'Новая запись' }],
+      lastInd: this.state.lastInd + 1,
+    });
+  }
 
-    /**
-     * Удаление записи по коду
-     * @param code
-     */
-    deleteItem(code) {
-        this.setState({
-            ...this.state,
-            list: this.state.list.filter(item => item.code !== code),
-        });
-    }
+  /**
+   * Удаление записи по коду
+   * @param code
+   */
+  deleteItem(code) {
+    this.setState({
+      ...this.state,
+      list: this.state.list.filter(item => item.code !== code),
+    });
+  }
 
-    /**
-     * Выделение записи по коду
-     * @param code
-     */
-    selectItem(code, ctrlCmd = false) {
-        this.setState({
-            ...this.state,
-            list: this.state.list.map(item => {
-                if (item.code === code) {
-                    item.selected = !item.selected;
-                    if (item.selected) {
-                        !item.highlightCount ? item.highlightCount = 1 : item.highlightCount++
-                    }
-                } else if (!ctrlCmd) {
-                    item.selected = false;
-                }
-                return item;
-            }),
-        });
-    }
+  /**
+   * Выделение записи по коду
+   * @param code
+   */
+  selectItem(code, ctrlCmd = false) {
+    this.setState({
+      ...this.state,
+      list: this.state.list.map(item => {
+        if (item.code === code) {
+          item.selected = !item.selected;
+          if (item.selected) {
+            !item.highlightCount ? (item.highlightCount = 1) : item.highlightCount++;
+          }
+        } else if (!ctrlCmd) {
+          item.selected = false;
+        }
+        return item;
+      }),
+    });
+  }
 }
 
 export default Store;
