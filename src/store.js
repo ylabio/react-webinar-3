7fbcd7d859f0901817cba17a187ccbb32cd88dda
Mikhail Morozov <ya.moroz95@yandex.ru>
2025-03-21 10:48:15 +0300

    feat: Added the generation of a unique entry code for task 2

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ class Store {
   constructor(initState = {}) {
     this.state = initState;
     this.listeners = []; // Слушатели изменений состояния
+    this.entryCode = initState.list.length ?? 0; // Счетчик кодов для записей
   }
 
   /**
@@ -44,7 +45,7 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, { code: this.state.list.length + 1, title: 'Новая запись' }],
+      list: [...this.state.list, { code: ++this.entryCode, title: 'Новая запись' }],
     });
   }
 
