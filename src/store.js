a208b1da373bf3efd9c56a6d1a7693dfd6b26e65
Anna Khokhrina <valdes.anya@mail.ru>
2025-03-21 13:53:28 +0300

    Improved record selection
    Доработано выделение записей.
    По умолчанию должна выделяться только одна запись, поэтому при выделении
    одной записи, выделение с других записей должно сбрасываться.
    Клик по уже выделенной записи снимает её выделение.
    Клики, связанные с другими действиями (например, добавление или
    удаление), не должны влиять на текущее выделение. Если при клике на
    запись дополнительно удерживать клавишу Ctrl (или Cmd на macOS), то
    выделение других записей не должно сбрасываться, что должно позволять
    выделить множество записей.

--- src/store.js
+++ src/store.js
@@ -44,7 +44,7 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, { code: this.state.list.length + 1, title: 'Новая запись' }],
+      list: [...this.state.list, { code: this.state.list.length + 1, title: 'Новая запись', selected: false }],
     });
   }
 
@@ -63,12 +63,19 @@ class Store {
    * Выделение записи по коду
    * @param code
    */
-  selectItem(code) {
+  selectItem(code, event) {
+    const isCtrlPressed = event.ctrlKey || event.metaKey; //проверка нажатия ctrl и cmd
+
     this.setState({
       ...this.state,
       list: this.state.list.map(item => {
         if (item.code === code) {
-          item.selected = !item.selected;
+          return {...item, selected: !item.selected};
+          //item.selected = !item.selected;
+        }
+        else if (!isCtrlPressed) { //если не нажато, снимаем выделение с других записей
+          return {...item, selected: false};
+          //item.selected = false;
         }
         return item;
       }),
