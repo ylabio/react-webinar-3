3a97ddf13b01d0d8a200d895ed4ff974723eb177
ivan <pavlovaleksey159@gmail.com>
2025-03-21 21:46:49 +0300

    done?

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createElement } from './utils.js';
+import { createElement, countMessage } from './utils.js';
 import './styles.css';
 
 /**
@@ -13,10 +13,10 @@ function App({ store }) {
   return (
     <div className="App">
       <div className="App-head">
-        <h1>Приложение на чистом JS</h1>
+        <h1 className="header">Приложение на чистом JS</h1>
       </div>
       <div className="App-controls">
-        <button onClick={() => store.addItem()}>Добавить</button>
+        <button className="button button-add" onClick={() => store.addItem()}>Добавить</button>
       </div>
       <div className="App-center">
         <div className="List">
@@ -27,9 +27,17 @@ function App({ store }) {
                 onClick={() => store.selectItem(item.code)}
               >
                 <div className="Item-code">{item.code}</div>
-                <div className="Item-title">{item.title}</div>
+                <div className="Item-title">
+                  <span>{item.title}</span>
+                  {
+                    (item.selectCount > 0 ? countMessage('Выделяли', item.selectCount, 'раз', '', 'а', '') : '')
+                  }
+                </div>
                 <div className="Item-actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+                  <button className="button button-delete" onClick={(e) => {
+                    e.stopPropagation();
+                    store.deleteItem(item.code);
+                    }}>Удалить</button>
                 </div>
               </div>
             </div>
