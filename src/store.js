33e18bc7eed5383d1251041000b2bf6601611313
Dmitry <Dimatch86@mail.ru>
2025-03-21 14:54:23 +0300

    task 2

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
 /**
  * Хранилище состояния приложения
  */
+import { SequenceGenerator } from './sequence_generator';
+
 class Store {
   constructor(initState = {}) {
     this.state = initState;
     this.listeners = []; // Слушатели изменений состояния
+    this.generator = new SequenceGenerator(this.state.list.length + 1)
   }
 
   /**
@@ -44,7 +47,7 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, { code: this.state.list.length + 1, title: 'Новая запись' }],
+      list: [...this.state.list, { code: this.generator.generate(), title: 'Новая запись' }],
     });
   }
 
