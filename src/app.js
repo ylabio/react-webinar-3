e7458c7e3ee845146346810a7a7a9dc030e0196b
Elena Zimakova <ahes@mail.ru>
2025-03-21 21:05:54 +0300

    [feat] add styles

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ function App({ store }) {
               >
                 <div className="Item-code">{item.code}</div>
                 <div className="Item-title">{item.title}
-                  {item.selectCount > 0 ? <span>{`| Выделяли: ${item.selectCount} раз`}</span> : ''}</div>
+                  {item.selectCount > 0 ? <span>{` | Выделяли: ${item.selectCount} раз`}</span> : ''}</div>
                 <div className="Item-actions">
                   <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
                 </div>
