77f051f6db6da0b777307b7bf5521ffd27c5ac56
Denis Kazantsev <kaz13den@gmail.com>
2025-03-22 00:09:37 +0500

    well

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ function App({ store }) {
                 onClick={() => store.selectItem(item.code)}
               >
                 <div className="Item-code">{item.code}</div>
-                <div className="Item-title">{item.title}</div>
+                <div className="Item-title">{item.title}{item.count > 0 && ` | Выделяли ${item.count} раз`}</div>
                 <div className="Item-actions">
                   <button className="App-center-btn" onClick={() => store.deleteItem(item.code)}>Удалить</button>
                 </div>
