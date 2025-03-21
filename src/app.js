39d5565c350eb97e9860a5c6c0a21758583352f6
Alnikov Aleksandr <web3aleksandr@gmail.com>
2025-03-21 18:50:47 +0700

    выделение, уникальсчетчик

--- src/app.js
+++ src/app.js
@@ -10,32 +10,53 @@ import './styles.css';
 function App({ store }) {
   const list = store.getState().list;
 
+  const handleItemClick = (code, event) => {
+    const isMultiSelect = event.ctrlKey || event.metaKey;
+    store.selectItem(code, isMultiSelect);
+  };
+
+
+
   return (
     <div className="App">
-      <div className="App-head">
-        <h1>Приложение на чистом JS</h1>
-      </div>
-      <div className="App-controls">
-        <button onClick={() => store.addItem()}>Добавить</button>
-      </div>
-      <div className="App-center">
-        <div className="List">
-          {list.map(item => (
-            <div key={item.code} className="List-item">
-              <div
-                className={'Item' + (item.selected ? ' Item_selected' : '')}
-                onClick={() => store.selectItem(item.code)}
-              >
-                <div className="Item-code">{item.code}</div>
-                <div className="Item-title">{item.title}</div>
-                <div className="Item-actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+     <header className="App-head">
+       <div className='container'>
+         <h1 className='title'>Приложение на чистом JS</h1>
+       </div>
+      </header>
+    <main>
+      <div className='container'>
+        <div className="App-controls">
+          <button className='add' onClick={() => store.addItem()}>Добавить</button>
+        </div>
+        <div className="App-center">
+          <ul className="List">
+            {list.map(item => (
+              <li key={item.code} className="List-item">
+                <div
+                  className={'Item' + (item.selected ? ' Item_selected' : '')}
+                  onClick={(event) => handleItemClick(item.code, event)}
+                >
+                  <div className="Item-code">{item.code}</div>
+                  <div className="Item-title">{item.title}&nbsp;</div>
+                  {item.selectionCount > 0 && (
+                    <p className="Selection-count">|
+                       Выделяли {item.selectionCount} раз
+                    </p>
+                  )}
+                  <div className="Item-actions">
+                    <button className='remove' onClick={(e)  => {
+                      e.stopPropagation();
+                      store.deleteItem(item.code);
+                    }}>Удалить</button>
+                  </div>
                 </div>
-              </div>
-            </div>
-          ))}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
+    </main>
     </div>
   );
 }
