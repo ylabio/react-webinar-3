c81304e5d6e280cc0886ca15f6c921b3b857ff6b
Ilya <84274399+IlyaDeminMain@users.noreply.github.com>
2025-03-21 20:30:01 +0300

    Update app.js
    Добавлен обработчик кликов на записи с учетом Ctrl/Cmd для множественного выделения
    Реализованы методы `selectItem` и `toggleItemSelection` в хранилище для управления выделением
    Введено поле `selectionCount` для отслеживания количества выделений каждой записи
    Добавлена функция `renderTitleWithSelectionInfo` для вывода информации о выделении в названии записи

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createElement } from './utils.js';
 import './styles.css';
 
+
 /**
  * Приложение
  * @param store {Store} Состояние приложения
@@ -10,13 +11,21 @@ import './styles.css';
 function App({ store }) {
   const list = store.getState().list;
 
+  const handleItemClick = (event, itemCode) => {
+    if (event.ctrlKey || event.metaKey) {
+      store.toggleItemSelection(itemCode);
+    } else {
+      store.selectItem(itemCode);
+    }
+  };
+
   return (
     <div className="App">
       <div className="App-head">
         <h1>Приложение на чистом JS</h1>
       </div>
       <div className="App-controls">
-        <button onClick={() => store.addItem()}>Добавить</button>
+        <button className="App-control__btn" onClick={() => store.addItem()}>Добавить</button>
       </div>
       <div className="App-center">
         <div className="List">
@@ -24,12 +33,12 @@ function App({ store }) {
             <div key={item.code} className="List-item">
               <div
                 className={'Item' + (item.selected ? ' Item_selected' : '')}
-                onClick={() => store.selectItem(item.code)}
+                onClick={(event) => handleItemClick(event, item.code)}
               >
                 <div className="Item-code">{item.code}</div>
-                <div className="Item-title">{item.title}</div>
+                {renderTitleWithSelectionInfo(item)}
                 <div className="Item-actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+                  <button className="Item-actions__btn" onClick={() => store.deleteItem(item.code)}>Удалить</button>
                 </div>
               </div>
             </div>
@@ -39,5 +48,17 @@ function App({ store }) {
     </div>
   );
 }
+
+function renderTitleWithSelectionInfo(item) {
+  return (
+    <div className="Item-title">
+      {item.title}
+      {item.selectionCount > 0 && (
+        <span className="Item-info"> &nbsp;| Выделяли {item.selectionCount} раз</span>
+      )}
+    </div>
+  );
+}
+
 
 export default App;
