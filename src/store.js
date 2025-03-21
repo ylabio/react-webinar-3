767086b802f219c29f9e3b65b68509adc8b1c7ab
Фёдор Зуев <zuev.f.s@gmail.com>
2025-03-22 05:34:00 +1000

    ДЗ1

--- src/store.js
+++ src/store.js
@@ -3,7 +3,12 @@
  */
 class Store {
   constructor(initState = {}) {
-    this.state = initState;
+    this.state = {
+      ...initState,
+      selectedItems: new Set(), // Множество выделенных элементов
+      selectionCounts: {}, // Счётчики выделений для каждого элемента
+      maxCode: Math.max(...initState.list.map(item => item.code), 0), // Максимальный использованный код
+    };
     this.listeners = []; // Слушатели изменений состояния
   }
 
@@ -42,36 +47,65 @@ class Store {
    * Добавление новой записи
    */
   addItem() {
+    const newCode = this.state.maxCode + 1;
     this.setState({
       ...this.state,
-      list: [...this.state.list, { code: this.state.list.length + 1, title: 'Новая запись' }],
+      list: [...this.state.list, { code: newCode, title: 'Новая запись' }],
+      maxCode: newCode,
     });
   }
 
   /**
    * Удаление записи по коду
-   * @param code
+   * @param code {number}
    */
   deleteItem(code) {
+    const newSelectedItems = new Set(this.state.selectedItems);
+    newSelectedItems.delete(code);
+
     this.setState({
       ...this.state,
       list: this.state.list.filter(item => item.code !== code),
+      selectedItems: newSelectedItems,
     });
   }
 
   /**
    * Выделение записи по коду
-   * @param code
+   * @param code {number} Код записи
+   * @param isCtrlPressed {boolean} Нажата ли клавиша Ctrl
    */
-  selectItem(code) {
+  selectItem(code, isCtrlPressed) {
+    const newSelectedItems = new Set(this.state.selectedItems);
+
+    if (newSelectedItems.has(code)) {
+      // Если элемент уже выделен, снимаем выделение
+      newSelectedItems.delete(code);
+    } else {
+      // Если Ctrl не нажат, очищаем все выделения
+      if (!isCtrlPressed) {
+        newSelectedItems.clear();
+      }
+      newSelectedItems.add(code);
+
+      // Увеличиваем счётчик только при добавлении выделения
+      const newSelectionCounts = {
+        ...this.state.selectionCounts,
+        [code]: (this.state.selectionCounts[code] || 0) + 1,
+      };
+
+      this.setState({
+        ...this.state,
+        selectedItems: newSelectedItems,
+        selectionCounts: newSelectionCounts,
+      });
+      return;
+    }
+
+    // Если снимаем выделение, просто обновляем selectedItems
     this.setState({
       ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
-          item.selected = !item.selected;
-        }
-        return item;
-      }),
+      selectedItems: newSelectedItems,
     });
   }
 }
