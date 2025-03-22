17ea6bb55b6633e42b9a2f02709fc47491e56e16
Daniil Zhbir <54086833+Burnmyheart@users.noreply.github.com>
2025-03-23 02:17:56 +0300

    Fix last1

---
+++ .history/src/app_20250323021557.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { createElement } from './utils.js';
+import './styles.css';
+
+/**
+ * Приложение
+ * @param store {Store} Состояние приложения
+ * @returns {React.ReactElement}
+ */
+function App({ store }) {
+  const list = store.getState().list;
+
+  return (
+    <div className="App">
+      <div className="App-head">
+        <h1>Приложение на чистом JS</h1>
+      </div>
+      <div className="App-controls">
+        <button onClick={() => store.addItem()}>Добавить</button>
+      </div>
+      <div className="App-center">
+        <div className="List">
+          {list.map((item, index) => (
+            <div key={item.code} className="List-item">
+              <div
+                className={`Item ${item.selected ? 'Item_selected' : ''}`}
+                onClick={(e) => store.selectItem(item.code , e.ctrlKey || e.metaKey)}
+              >
+                <div className="Item-content">
+                  <div className="Item-number">
+                    {index + 1}. {/* Индекс элемента в списке (нумерация с 1) */}
+                  </div>
+                  <div className="Item-code">{item.code}</div>
+                  <div className="Item-title">{item.title}</div>
+                  {item.selectCount > 0 && (
+                    <div className="Item-count">| Выделяли {item.selectCount} раз</div>
+                  )}
+                </div>
+                <div className="Item-actions">
+                  <button onClick={(e) => { 
+                    e.stopPropagation();
+                    store.deleteItem(item.code);
+                  }}>Удалить</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
