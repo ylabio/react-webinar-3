33739aba9f1d59cfe019b066be383ad9276008a8
maxim necrasov <maddmax22@yandex.ru>
2025-03-22 15:50:49 +0300

    add styles

--- src/app.js
+++ src/app.js
@@ -16,7 +16,9 @@ function App({ store }) {
         <h1>Приложение на чистом JS</h1>
       </div>
       <div className="App-controls">
-        <button onClick={() => store.addItem()}>Добавить</button>
+        <button className="App-button Add-button" onClick={() => store.addItem()}>
+          Добавить
+        </button>
       </div>
       <div className="App-center">
         <div className="List">
@@ -30,11 +32,20 @@ function App({ store }) {
                   <div className="Item-code">{item.code}</div>
                   <div className="Item-title">{item.title}</div>
                   <div className="Item-counts">
-                    {Boolean(item.selectedTimes) && `| Выделяли ${item.selectedTimes} раз`}
+                    {Boolean(item.selectedTimes) && (
+                      <span className="Item-counts_count">
+                        {`| Выделяли ${item.selectedTimes} раз`}
+                      </span>
+                    )}
                   </div>
                 </div>
                 <div className="Item-actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+                  <button
+                    className="App-button Remove-button"
+                    onClick={() => store.deleteItem(item.code)}
+                  >
+                    Удалить
+                  </button>
                 </div>
               </div>
             </div>
