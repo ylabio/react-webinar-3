589a3713e03cca6c2471abc71129cc6aa9f483fe
TamiFam <ilya.innain@mail.ru>
2025-03-23 03:44:56 +0300

    Доделал дазайн

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ function App({ store }) {
         <h1>Приложение на чистом JS</h1>
       </div>
       <div className="App-controls">
-        <button onClick={() => store.addItem()}>Добавить</button>
+        <button className="Button-add" onClick={() => store.addItem()}>Добавить</button>
       </div>
       <div className="App-center">
         <div className="List">
@@ -41,7 +41,7 @@ function App({ store }) {
                   )}
                 </div>
                 <div className="Item-actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+                  <button className="Button-delete" onClick={() => store.deleteItem(item.code)}>Удалить</button>
                 </div>
               </div>
             </div>
