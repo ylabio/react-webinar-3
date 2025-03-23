1e46cecbfc2982eeca703786372ccd1a8725f5c4
ArtyomXXX <ambartsumyanartyomxxx@gmail.com>
2025-03-23 20:38:35 +0300

    Добавил обёртку wrapper

--- src/app.js
+++ src/app.js
@@ -13,32 +13,36 @@ function App({ store }) {
   return (
     <div className="App">
       <div className="App-head">
-        <h1>Приложение на чистом JS</h1>
+        <div className="wrapper">
+          <h1>Приложение на чистом JS</h1>
+        </div>
       </div>
-      <div className="App-controls">
-        <button onClick={(e) => store.addItem(e)}>Добавить</button>
-      </div>
-      <div className="App-center">
-        <div className="List">
-          {list.map(item => (
-            <div key={item.code} className="List-item">
-              <div
-                className={'Item' + (item.selected ? ' Item_selected' : '')}
-                onClick={(e) => store.selectItem(item.code, e)}
-              >
-                <div className="Item-code">{item.code}</div>
-                <div className="Item-title">
-                    {item.title}&nbsp;
-                    <span className="Item-count-select">
-                      {item.count !== 0 ? `| Выделяли ${item.count} раз` : ''}
-                    </span>
-                </div>
-                <div className="Item-actions">
-                  <button onClick={(e) => store.deleteItem(item.code, e)}>Удалить</button>
+      <div className="wrapper">
+        <div className="App-controls">
+          <button onClick={(e) => store.addItem(e)}>Добавить</button>
+        </div>
+        <div className="App-center ">
+          <div className="List">
+            {list.map(item => (
+              <div key={item.code} className="List-item">
+                <div
+                  className={'Item' + (item.selected ? ' Item_selected' : '')}
+                  onClick={(e) => store.selectItem(item.code, e)}
+                >
+                  <div className="Item-code">{item.code}</div>
+                  <div className="Item-title">
+                      {item.title}&nbsp;
+                      <span className="Item-count-select">
+                        {item.click !== 0 ? `| Выделяли ${store.pluralization(item.click)}` : ''}
+                      </span>
+                  </div>
+                  <div className="Item-actions">
+                    <button onClick={(e) => store.deleteItem(item.code, e)}>Удалить</button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
