364a8a9129a1d387bc08c056ecf960a4b8427c8f
ArtyomXXX <ambartsumyanartyomxxx@gmail.com>
2025-03-23 20:37:20 +0300

    В объектах списка list изменил свойство count на click

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import Store from './store.js';
 
 const store = new Store({
   list: [
-    { code: 1, title: 'Название элемента', count: 0 },
-    { code: 2, title: 'Некий объект', count: 0 },
-    { code: 3, title: 'Заголовок', count: 0 },
-    { code: 4, title: 'Очень длинное название элемента из семи слов', count: 0 },
-    { code: 5, title: 'Запись', count: 0 },
-    { code: 6, title: 'Шестая запись', count: 0 },
-    { code: 7, title: 'Седьмая запись', count: 0 },
+    { code: 1, title: 'Название элемента', click: 0 },
+    { code: 2, title: 'Некий объект', click: 0 },
+    { code: 3, title: 'Заголовок', click: 0 },
+    { code: 4, title: 'Очень длинное название элемента из семи слов', click: 0 },
+    { code: 5, title: 'Запись', click: 0 },
+    { code: 6, title: 'Шестая запись', click: 0 },
+    { code: 7, title: 'Седьмая запись', click: 0 },
   ],
 });
 
