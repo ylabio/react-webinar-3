cd2dd8a6ba4c0f48af878e8fc2eb78498b44f3e2
goharvard77 <goharvard77@gmail.com>
2025-03-27 20:58:49 +0300

    Fixed import

--- src/components/head/index.js
+++ src/components/head/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { cn as bem } from '@bem-react/classname';
 import './style.css';
-import Title from '../../title';
+import Title from '../title';
 
 function Head({ title }) {
   const cn = bem('Head');
