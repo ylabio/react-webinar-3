adb33a7637086aa4fc3743099e5cba4b8d931845
ArtyomXXX <ambartsumyanartyomxxx@gmail.com>
2025-03-27 23:29:17 +0300

    Исправил баг

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ function App({ store }) {
             {list.map(item => (
               <div key={item.code} className="List-item">
                 <div
-                  className={"Item" + (item.selected ? "Item_selected" : "")}
+                  className={"Item" + (item.selected ? " Item_selected" : "")}
                   onClick={(e) => store.selectItem(item.code, e)}
                 >
                   <div className="Item-code">{item.code}</div>
