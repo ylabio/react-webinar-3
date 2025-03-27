b94622f45053883c989560625786c87feef99461
Slumrag <mrgreenthecristal@yandex.ru>
2025-03-27 20:46:41 +0300

    chore: :wrench: configure svg loader

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,18 @@ let config = {
           { loader: 'css-loader', options: { url: true, import: true } },
         ],
       },
+      //.svg
+      {
+        test: /\.svg$/i,
+        type: 'asset',
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+        use: ['@svgr/webpack'],
+      },
     ],
   },
   plugins: [
