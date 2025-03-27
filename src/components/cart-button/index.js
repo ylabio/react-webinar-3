33fbb19c1d7c8cfc717bb275428be90b3579e10e
Slumrag <mrgreenthecristal@yandex.ru>
2025-03-27 21:27:04 +0300

    feat: :sparkles: add cart-button

---
+++ src/components/cart-button/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from '../button';
+import PropTypes from 'prop-types';
+import { formatCurrency, plural } from '../../utils';
+import './style.css';
+import Cart from '../../assets/cart.svg';
+
+function CartButton({ itemCount = 0, totalCost = 0, ...props }) {
+  const formattedCount =
+    itemCount + ' ' + plural(itemCount, { one: 'товар', few: 'товара', many: 'товаров' });
+  const formattedCost = formatCurrency(totalCost);
+
+  return (
+    <Button variant={'outline'} icon={<Cart fill={'var(--primary)'} />} {...props}>
+      {formattedCount} / {formattedCost}
+    </Button>
+  );
+}
+
+CartButton.propTypes = {
+  itemCount: PropTypes.number,
+  totalCost: PropTypes.number,
+};
+
+export default CartButton;
