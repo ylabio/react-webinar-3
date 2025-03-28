fac98905f6d84cdf15e6e4ed769982315bc6ddf4
Mikhail Morozov <ya.moroz95@yandex.ru>
2025-03-28 11:24:06 +0300

    feat: changed the basic layout of the basket button

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -2,20 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
 
-function Controls({ onAdd }) {
+function Controls({ onOpenBasket = () => {} }) {
   return (
     <div className="Controls">
-      <button onClick={() => onAdd()}>Добавить</button>
+      <button onClick={() => onOpenBasket()}>Пусто</button>
     </div>
   );
 }
 
 Controls.propTypes = {
-  onAdd: PropTypes.func,
-};
-
-Controls.defaultProps = {
-  onAdd: () => {},
+  onOpenBasket: PropTypes.func,
 };
 
 export default React.memo(Controls);
