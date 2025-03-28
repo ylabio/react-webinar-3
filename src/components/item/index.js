9efec13a6e1ef24ff19241691e2f38b0a320662a
Slumrag <mrgreenthecristal@yandex.ru>
2025-03-28 12:07:43 +0300

    refactor: :recycle: decuple item from store

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,62 +1,27 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { plural } from '../../utils';
+import { cn as bem } from '@bem-react/classname';
+import { formatCurrency } from '../../utils';
 import './style.css';
 
-function Item(props) {
-  // Счётчик выделений
-  const [count, setCount] = useState(0);
-
-  const callbacks = {
-    onClick: () => {
-      props.onSelect(props.item.code);
-      if (!props.item.selected) {
-        setCount(count + 1);
-      }
-    },
-    onDelete: e => {
-      e.stopPropagation();
-      props.onDelete(props.item.code);
-    },
-  };
+function Item({ title, price, action, ...props }) {
+  const cn = bem('Item');
 
   return (
-    <div
-      className={'Item' + (props.item.selected ? ' Item_selected' : '')}
-      onClick={callbacks.onClick}
-    >
-      <div className="Item-code">{props.item.code}</div>
-      <div className="Item-title">
-        <b>{props.item.title}</b>
-        {count
-          ? ` | Выделяли ${count} ${plural(count, {
-              one: 'раз',
-              few: 'раза',
-              many: 'раз',
-            })}`
-          : ''}
+    <div className={cn()} {...props}>
+      <div className={cn('Title')}>
+        <strong>{title}</strong>
+        <span>{formatCurrency(price)}</span>
       </div>
-      <div className="Item-actions">
-        <button onClick={callbacks.onDelete}>Удалить</button>
-      </div>
+      <div className={cn('Actions')}>{action}</div>
     </div>
   );
 }
 
 Item.propTypes = {
-  item: PropTypes.shape({
-    code: PropTypes.number,
-    title: PropTypes.string,
-    selected: PropTypes.bool,
-    count: PropTypes.number,
-  }).isRequired,
-  onDelete: PropTypes.func,
-  onSelect: PropTypes.func,
-};
-
-Item.defaultProps = {
-  onDelete: () => {},
-  onSelect: () => {},
+  title: PropTypes.string,
+  price: PropTypes.number,
+  action: PropTypes.element,
 };
 
 export default React.memo(Item);
