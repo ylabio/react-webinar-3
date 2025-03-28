d7ddcb9c6afcd938967c413e1c1df3769c47370d
Margo <m.yunanova@gmail.com>
2025-03-28 12:41:32 +0300

    feat: implement control button

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes, { number } from 'prop-types';
 import './style.css';
+import { formatNumber, plural } from '../../utils';
+import CartIcon from './images/cart.svg';
 
-function Controls({ onAdd }) {
+function Controls(props) {
+  const formatTotal = number =>
+    `${number} ${plural(number, { one: 'товар', few: 'товара', many: 'товаров', other: 'товаров' })}`;
+
   return (
     <div className="Controls">
-      <button onClick={() => onAdd()}>Добавить</button>
+      <button onClick={props.openModal}>
+        <CartIcon />
+        <div>
+          <b>
+            {!!props.totalItems ? 'Пусто' : formatTotal(props.totalItems)} /
+            {formatNumber({ number: props.totalPrice })}
+          </b>
+        </div>
+      </button>
     </div>
   );
 }
 
 Controls.propTypes = {
-  onAdd: PropTypes.func,
+  totalItems: number,
+  totalPrice: number,
+  openModal: PropTypes.func,
 };
 
 Controls.defaultProps = {
-  onAdd: () => {},
+  openModal: () => {},
 };
 
 export default React.memo(Controls);
