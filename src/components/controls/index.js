bc8328de5026561980706757c2e508763f2ff783
maximqa20 <maks.karp96@yandex.ru>
2025-03-28 20:47:17 +0300

    Update index.js

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -2,20 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
 
-function Controls({ onAdd }) {
+function Controls({ totalItems, totalPrice, onToggleCartModal }) {
+  let buttonText;
+  if (totalItems === 0) {
+    buttonText = 'В корзине пусто'
+  } else if (totalItems === 1) {
+    buttonText = buttonText = `${totalItems} товар / ${totalPrice.toFixed(2)} ₽`
+  } else if (totalItems > 0 && totalItems >= 2 && totalItems <= 4) {
+    buttonText = `${totalItems} товара / ${totalPrice.toFixed(2)} ₽`
+  } else buttonText = `${totalItems} товаров / ${totalPrice.toFixed(2)} ₽`
+    
   return (
     <div className="Controls">
-      <button onClick={() => onAdd()}>Добавить</button>
+      <button onClick={onToggleCartModal}>
+        {buttonText}
+      </button>
     </div>
   );
 }
 
 Controls.propTypes = {
-  onAdd: PropTypes.func,
+  totalItems: PropTypes.number.isRequired,
+  totalPrice: PropTypes.number.isRequired,
+  onToggleCartModal: PropTypes.func.isRequired,
 };
 
-Controls.defaultProps = {
-  onAdd: () => {},
-};
 
 export default React.memo(Controls);
