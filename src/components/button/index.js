d5e98994963d64fa38f59f98e3a68249a2c41149
Slumrag <mrgreenthecristal@yandex.ru>
2025-03-28 12:43:17 +0300

    feat: add color variant to button

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import { cn as bem } from '@bem-react/classname';
 import './style.css';
 
-function Button({ variant = 'solid', children, icon, ...props }) {
+function Button({ variant = 'solid', color = 'primary', children, icon, ...props }) {
   const cn = bem('Button');
   return (
-    <button className={cn({ variant: variant })} {...props}>
-      <span className={cn('Icon')}>{icon}</span>
+    <button className={cn({ variant, color })} {...props}>
+      {icon && <span className={cn('Icon')}>{icon}</span>}
       {children}
     </button>
   );
@@ -15,6 +15,7 @@ function Button({ variant = 'solid', children, icon, ...props }) {
 
 Button.propTypes = {
   variant: PropTypes.oneOf(['solid', 'outline']),
+  color: PropTypes.oneOf(['primary', 'delete']),
   icon: PropTypes.node,
 };
 
