76198e1d6a57a3ffdf680974955ad0f55e669ca4
edelsid <eradonna@yandex.ru>
2025-03-28 21:06:20 +0300

    image paths

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,11 @@ let config = {
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: (name) => {
+            const path = name.filename.split("/").slice(1, -1).join("/");
+            return `${path}/[name][ext]`;
+        }},
       },
     ],
   },
