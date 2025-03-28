d84efbe7aa8f2494aa8d06cf8d2ef3839f8834d1
Viktor-Kurgashov <E843xx9@gmail.com>
2025-03-28 21:09:00 +0400

    корневые компоненты

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import List from './components/list';
 import Controls from './components/controls';
 import Head from './components/head';
 import PageLayout from './components/page-layout';
+import Modal from './components/modal';
+import Cart from './components/cart';
 
 /**
  * Приложение
@@ -10,7 +12,9 @@ import PageLayout from './components/page-layout';
  * @returns {React.ReactElement}
  */
 function App({ store }) {
-  const list = store.getState().list;
+  const { list, cart, total } = store.getState();
+
+  const [modalOpened, setModalOpened] = useState(false);
 
   const callbacks = {
     onDeleteItem: useCallback(
@@ -27,20 +31,43 @@ function App({ store }) {
       [store],
     ),
 
-    onAddItem: useCallback(() => {
-      store.addItem();
-    }, [store]),
+    openModal: useCallback(
+      () => {
+        setModalOpened(true);
+      },
+      [modalOpened],
+    ),
+
+    closeModal: useCallback(
+      () => {
+        setModalOpened(false);
+      },
+      [modalOpened],
+    ),
   };
 
   return (
     <PageLayout>
-      <Head title="Приложение на React" />
-      <Controls onAdd={callbacks.onAddItem} />
+      <Head title="Магазин" />
+      <Controls
+        total={total}
+        onTotalClick={callbacks.openModal}
+      />
       <List
         list={list}
-        onDeleteItem={callbacks.onDeleteItem}
         onSelectItem={callbacks.onSelectItem}
       />
+      <Modal
+        opened={modalOpened}
+        close={callbacks.closeModal}
+      >
+        <Cart
+          title="Корзина"
+          cart={cart}
+          total={total}
+          onDeleteItem={callbacks.onDeleteItem}
+        />
+      </Modal>
     </PageLayout>
   );
 }
