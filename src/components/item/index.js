5cb41b3ff8b6b657244b4ac9fc6f34488cb5c825
Denis Kazantsev <kaz13den@gmail.com>
2025-03-29 16:17:20 +0500

    lecture-2

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -25,11 +25,12 @@ function Item(props) {
       className={'Item' + (props.item.selected ? ' Item_selected' : '')}
       onClick={callbacks.onClick}
     >
-      <div className="Item-code">{props.item.code}</div>
+      
       <div className="Item-title">
         <b>{props.item.title}</b>
         
       </div>
+      <div className="Item-price">{props.item.price} ₽</div>
       <div className="Item-actions">
         <button onClick={callbacks.onDelete}>Добавить</button>
       </div>
