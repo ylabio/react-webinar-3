e44de51ff8886b1a68e32ac5bafd7b23e15ebe79
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-03-31 16:09:44 +0300

    Modal component as a wrapper for all modals

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,51 +1,19 @@
-import closeForButton from '../../assets/icons/close-button.svg';
 import React from 'react';
 import './style.css';
 import PropTypes from 'prop-types';
+import Basket from '../basket';
 
-function Modal({
-  modalOpen,
-  setModalOpen = () => {},
-  cartItems,
-  onGetCartTotal = () => {},
-  onRemoveFromCart = () => {},
-}) {
+function Modal({ modalOpen, setModalOpen, cartItems, onRemoveFromCart, cartTotal }) {
   return (
     modalOpen && (
       <div className="Modal-overlay">
         <div className="Modal-container">
-          <div className="Modal-header">
-            <h2 className="Modal-title">Корзина</h2>
-            <button onClick={() => setModalOpen(false)} className="Modal-close">
-              <img src={closeForButton} alt="Кнопка закрыть" />
-            </button>
-          </div>
-          <ul className="Modal-list">
-            {cartItems.map(item => (
-              <li key={item.code} className="Modal-listItem">
-                <div className="Modal-item">
-                  <div className="Modal-itemTitle">
-                    <b>{item.title}</b>
-                  </div>
-                  <div className="Modal-actions">
-                    <div>
-                      <span>{item.quantity} шт</span>
-                      <div>
-                        <span>{(item.price * item.quantity).toLocaleString('ru-RU')} ₽</span>
-                        <button onClick={() => onRemoveFromCart(item.code)}>Удалить</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-          <div className="Modal-footer">
-            <div>
-              <span>Итого:</span>
-              <span>{onGetCartTotal()} ₽</span>
-            </div>
-          </div>
+          <Basket
+            setModalOpen={setModalOpen}
+            cartItems={cartItems}
+            cartTotal={cartTotal}
+            onRemoveFromCart={onRemoveFromCart}
+          />
         </div>
       </div>
     )
