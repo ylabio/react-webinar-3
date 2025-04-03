2ea06801bcd68aa55faf664523b225a004f74c1c
Iakov Zelenkov <iakovzelenkov@gmail.com>
2025-04-03 13:18:43 +0300

    fix: modal ref

--- src/components/modal-layout/index.js
+++ src/components/modal-layout/index.js
@@ -7,18 +7,25 @@ function ModalLayout({ title = 'Модалка', children, onClose = () => {} })
   const cn = bem('ModalLayout');
 
   // Корректировка центра, если модалка больше окна браузера.
-  const layout = useRef();
-  const frame = useRef();
+  const layout = useRef(null);
+  const frame = useRef(null);
+
   useEffect(() => {
     const resizeObserver = new ResizeObserver(() => {
       // Центрирование frame или его прижатие к краю, если размеры больше чем у layout
-      layout.current.style.alignItems =
-        layout.current.clientHeight < frame.current.clientHeight ? 'flex-start' : 'center';
-      layout.current.style.justifyContent =
-        layout.current.clientWidth < frame.current.clientWidth ? 'flex-start' : 'center';
+      if (layout.current && frame.current) {
+        layout.current.style.alignItems =
+          layout.current.clientHeight < frame.current.clientHeight ? 'flex-start' : 'center';
+        layout.current.style.justifyContent =
+          layout.current.clientWidth < frame.current.clientWidth ? 'flex-start' : 'center';
+      }
     });
+
     // Следим за изменениями размеров layout
-    resizeObserver.observe(layout.current);
+    if (layout.current) {
+      resizeObserver.observe(layout.current);
+    }
+
     return () => {
       resizeObserver.disconnect();
     };
