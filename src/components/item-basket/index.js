72fa47f873ae78358face6751adc769299ba762a
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2025-04-04 18:31:34 +0300

    продолжение решения 2-ой задачи

--- src/components/item-basket/index.js
+++ src/components/item-basket/index.js
@@ -5,18 +5,23 @@ import { cn as bem } from '@bem-react/classname';
 import PropTypes from 'prop-types';
 import Button from '../button';
 import './style.css';
+import { Link } from 'react-router-dom';
 
 function ItemBasket(props) {
   const cn = bem('ItemBasket');
 
   const callbacks = {
     onRemove: e => props.onRemove(props.item._id),
+    onLinkClick: e => {
+      props.onCloseModal(); // Закрываем модалку при клике
+    },
   };
 
   return (
     <div className={cn()}>
-      {/* <div className={cn('code')}>{props.item._id}</div> */}
-      <h4 className={cn('title')}>{props.item.title}</h4>
+      <Link to={`${props.item._id}`} className={cn('link')} onClick={callbacks.onLinkClick}>
+        <h4 className={cn('title')}>{props.item.title}</h4>
+      </Link>
       <div className={cn('right')}>
         <div className={cn('cell')}>{numberFormat(props.item.amount || 0)} шт</div>
         <div className={cn('cell')}>{numberFormat(props.item.price)} ₽</div>
