b6dc6ff3d2af9fb5bd0e7ad1ab0a73aeb8ba9453
VNagorniy <vnagorniyrnd@gmail.com>
2025-04-04 20:21:42 +0300

    fix: closing basket

--- src/components/item-basket/index.js
+++ src/components/item-basket/index.js
@@ -3,6 +3,7 @@ import propTypes from 'prop-types';
 import { useTranslation } from '../../translation/use-translation';
 import { numberFormat } from '../../utils';
 import { cn as bem } from '@bem-react/classname';
+import useStore from '../../store/use-store';
 import PropTypes from 'prop-types';
 import Button from '../button';
 import { NavLink } from 'react-router-dom';
@@ -11,15 +12,19 @@ import './style.css';
 function ItemBasket(props) {
   const cn = bem('ItemBasket');
   const { t } = useTranslation();
+  const store = useStore();
 
   const callbacks = {
     onRemove: () => {
       props.onRemove(props.item._id);
     },
+    onNavigate: e => {
+      store.actions.modals.close();
+    },
   };
 
   return (
-    <NavLink to={`/article/${props.item._id}`} className={cn()}>
+    <NavLink to={`/article/${props.item._id}`} className={cn()} onClick={callbacks.onNavigate}>
       <h4 className={cn('title')}>{props.item.title}</h4>
       <div className={cn('right')}>
         <div className={cn('cell')}>{numberFormat(props.item.amount || 0)} шт</div>
