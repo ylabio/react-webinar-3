d809830592ce45dd933c8e15f5355e5bdaca9e66
dinar1122 <gert.olh@mail.ru>
2025-04-06 13:28:05 +0300

    fix

--- src/store/basket/index.js
+++ src/store/basket/index.js
@@ -13,77 +13,7 @@ class Basket extends StoreModule {
    * Добавление товара в корзину
    * @param _id Код товара
    */
-  /* addToBasket(_id) {
-    let sum = 0;
-    // Ищем товар в корзине, чтобы увеличить его количество
-    let exist = false;
-    const list = this.getState().list.map(item => {
-      let result = item;
-      if (item._id === _id) {
-        exist = true; // Запомним, что был найден в корзине
-        result = { ...item, amount: item.amount + 1 };
-      }
-      sum += result.price * result.amount;
-      return result;
-    });
-
-    if (!exist) {
-      // Поиск товара в каталоге, чтобы его добавить в корзину.
-      // @todo В реальном приложении будет запрос к АПИ вместо поиска по состоянию.
-      const item = this.store.getState().catalog.list.find(item => item._id === _id);
-      list.push({ ...item, amount: 1 }); // list уже новый, в него можно пушить.
-      // Добавляем к сумме.
-      sum += item.price;
-    }
-
-    this.setState(
-      {
-        ...this.getState(),
-        list,
-        sum,
-        amount: list.length,
-      },
-      'Добавление в корзину',
-    );
-  }
-  addToBasketFromItemPage(_id) {
-    let sum = 0;
-    // Ищем товар в корзине, чтобы увеличить его количество
-    let exist = false;
-    const list = this.getState().list.map(item => {
-      let result = item;
-      if (item._id === _id) {
-        exist = true; // Запомним, что был найден в корзине
-        result = { ...item, amount: item.amount + 1 };
-      }
-      sum += result.price * result.amount;
-      return result;
-    });
-
-    if (!exist) {
-      // Поиск товара в каталоге, чтобы его добавить в корзину.
-      // @todo В реальном приложении будет запрос к АПИ вместо поиска по состоянию.
-      const item = this.store.getState().itemInfo.item;
-      if (item._id === _id) {
-        list.push({ ...item, amount: 1 });
-      } else {
-        throw new Error('ID не совпадает');
-      }
-      // list уже новый, в него можно пушить.
-      // Добавляем к сумме.
-      sum += item.price;
-    }
-
-    this.setState(
-      {
-        ...this.getState(),
-        list,
-        sum,
-        amount: list.length,
-      },
-      'Добавление в корзину',
-    );
-  } */
+  
 
   addOrUpdateItem(item) {
     const state = this.getState();
