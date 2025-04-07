eb5ea6d11aaf7ea04b278d8167f3dc98934d50a8
Anna Khokhrina <valdes.anya@mail.ru>
2025-04-07 14:24:44 +0300

    Added reset to the first page when pressing "Home"
    При нажатии на "Главная" происходит переход на первую страницу каталога

--- src/components/basket-tool/index.js
+++ src/components/basket-tool/index.js
@@ -6,17 +6,28 @@ import { numberFormat, plural } from '../../utils';
 import useTranslate from '../../hooks/useTranslate';
 import Cart from '../../assets/icon/cart.svg';
 import useSelector from '../../store/use-selector';
+import useStore from '../../store/use-store';
 import './style.css';
 
 function BasketTool(props) {
   const { onOpen = () => {}, sum = 0, amount = 0 } = props;
   const t = useTranslate();
+  const store = useStore();
   const lang = useSelector(state => state.locale.lang);
   const cn = bem('BasketTool');
   
+  //сброс страницы при переходе на главную
+  const handleHomeClick = () => {
+    store.actions.catalog.setPage(1); // сброс на первую страницу
+  };
+
   return (
     <div className={cn()}>
-      <Link to="/" className={cn('home-link')}>{t('home')}</Link>
+      <Link to="/" 
+        className={cn('home-link')} 
+        onClick={handleHomeClick}>
+        {t('home')}
+      </Link>
       <button className={cn('action')} onClick={onOpen}>
         <Cart className={cn('icon')} />
         <span className={cn('total')}>
