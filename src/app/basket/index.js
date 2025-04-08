57d38029a58c6c48d570ab805091683f981967ef
Alex Tyuzhin <alexey.tyuzhin@gmail.com>
2025-04-08 13:37:38 +0300

    final

--- src/app/basket/index.js
+++ src/app/basket/index.js
@@ -5,6 +5,7 @@ import ModalLayout from '../../components/modal-layout';
 import BasketTotal from '../../components/basket-total';
 import useStore from '../../store/use-store';
 import useSelector from '../../store/use-selector';
+import { useLanguage } from '../../store/language-context';
 
 function Basket() {
   const store = useStore();
@@ -37,8 +38,10 @@ function Basket() {
     ),
   };
 
+  const { translate } = useLanguage();
+
   return (
-    <ModalLayout title="Корзина" onClose={callbacks.closeModal}>
+    <ModalLayout title={translate('cart')} onClose={callbacks.closeModal}>
       <List list={select.list} renderItem={renders.itemBasket} />
       <BasketTotal sum={select.sum} />
     </ModalLayout>
--- src/store/translations.js
+++ src/store/translations.js
@@ -4,11 +4,13 @@ export const translations = {
     main: 'Главная',
     delete: 'Удалить',
     add: 'Добавить',
+    cart: 'Корзина',
   },
   en: {
     shop: 'Shop',
     main: 'Main',
     delete: 'Delete',
     add: 'Add',
+    cart: 'Cart',
   },
 };
