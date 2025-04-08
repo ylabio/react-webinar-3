69549e6d2e20ad3f297e78df1320521a7ca68eed
Yau1heni <lodbrok9@gmail.com>
2025-04-08 14:14:44 +0300

    refactor: moving pagination options to Store

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,10 +1,8 @@
 import { memo } from 'react';
 import PropTypes from 'prop-types';
-import Item from '../item';
 import './style.css';
 
 function List({ list = [], renderItem = item => {} }) {
-
   return (
     <ul className="List">
       {list.map(item => (
