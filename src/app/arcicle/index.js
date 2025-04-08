eff04aad4f34a4a14ebd427ef16641be3c20b932
Kochkina Ekaterina <gudrum1983@gmail.com>
2025-04-08 13:36:27 +0200

    Фикс 4 - добавила смену языка на странице товара

--- src/app/arcicle/index.js
+++ src/app/arcicle/index.js
@@ -3,7 +3,7 @@ import PageLayout from '../../components/page-layout';
 import Head from '../../components/head';
 import BasketTool from '../../components/basket-tool';
 import useStore from '../../store/use-store';
-import { Link, useParams } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 import ControlsPanel from "../../components/controls-panel";
 import ArticleCard from "../../components/article-card";
 import useSelector from "../../store/use-selector";
@@ -11,6 +11,9 @@ import HomeLink from "../../components/home-link";
 import { useDictionary } from '../../translations/useDictionary';
 import { useHomeLink } from '../../hooks/useHomeLink';
 import { useLabels } from '../../translations/useLabels';
+import { OPTIONS_LANG } from '../../constants';
+import ButtonsLang from '../../components/buttons-lang';
+import { buildLocationWithNewLang } from '../../utils';
 
 function Article() {
   const store = useStore();
@@ -18,9 +21,9 @@ function Article() {
   const id = params.id;
 
   const {lang} = useDictionary();
-  const linkHome = useHomeLink(lang)
-
-  const {labelsNavigation, labelsArticle, labelsBasketTools } = useLabels();
+  const linkHome = useHomeLink(lang);
+  const navigate = useNavigate();
+  const {labelsNavigation, labelsArticle, labelsBasketTools} = useLabels();
 
   const select = useSelector(state => ({
     article: state.article,
@@ -41,20 +44,31 @@ function Article() {
     }, [store]),
     // Открытие модалки корзины
     openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store]),
+    handleLangChange: useCallback(
+      (newLang) => {
+        if (newLang === lang) return;
+
+        navigate(buildLocationWithNewLang(newLang, location));
+      },
+      [navigate, location, lang],
+    ),
   };
 
   return select.article.article.title ? (
     <PageLayout>
-      <Head title={select.article.article.title}/>
+      <Head title={select.article.article.title}>
+        <ButtonsLang currentLang={lang} options={OPTIONS_LANG} onLangChange={callbacks.handleLangChange}/>
+      </Head>
       <ControlsPanel>
         <nav>
           <HomeLink link={linkHome} label={labelsNavigation.linkHome}/>
         </nav>
 
-        <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount} sum={select.sum} labels={labelsBasketTools}/>
+        <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount} sum={select.sum}
+                    labels={labelsBasketTools}/>
       </ControlsPanel>
 
-      <ArticleCard item={select.article.article} onAdd={callbacks.addToBasket} labels={labelsArticle} />
+      <ArticleCard item={select.article.article} onAdd={callbacks.addToBasket} labels={labelsArticle}/>
 
 
     </PageLayout>
@@ -62,7 +76,8 @@ function Article() {
     <Head title={'Загрузка'}/>
     <ControlsPanel>
       <Link to={{pathname: "/"}}>Главная</Link>
-      <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount} sum={select.sum} labels={labelsBasketTools} />
+      <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount} sum={select.sum}
+                  labels={labelsBasketTools}/>
     </ControlsPanel>
 
     <h1> Загрузка </h1>
