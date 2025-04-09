fa83f7607a0c967143f559f7d2439a99dd00c7bb
SempaiDarcy <mr_darcy_1@mail.ru>
2025-04-09 23:39:39 +0400

    feat: implement login logic in LoginPage

--- src/app/login/index.js
+++ src/app/login/index.js
@@ -2,13 +2,16 @@ import { memo, useState } from 'react';
 import Input from '../../components/input';
 import Button from '../../components/button';
 import PageLayout from '../../components/page-layout';
+import { useNavigate } from 'react-router-dom';
+import useStore from '../../hooks/use-store';
 
 function LoginPage() {
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-
-  const handleSubmit = e => {
+  const navigate = useNavigate();
+  const store = useStore();
+  const handleSubmit = async e => {
     e.preventDefault();
 
     if (!login || !password) {
@@ -17,8 +20,15 @@ function LoginPage() {
     }
 
     setError('');
-    console.log('Логин:', login, 'Пароль:', password);
-    // следующая итерация — отправка запроса
+
+    const success = await store.actions.user.login(login, password);
+
+    if (success) {
+      navigate('/profile');
+    } else {
+      const err = store.getState().user.error;
+      setError(err || 'Ошибка авторизации');
+    }
   };
 
   return (
