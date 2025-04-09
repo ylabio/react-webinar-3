b4f3fc8c5f56ef2a94d1369a9fa692b722094446
Anna Khokhrina <valdes.anya@mail.ru>
2025-04-09 14:51:26 +0300

    Added product filter by category
    Добавлен фильтр товаров по категориям

--- src/store/catalog/index.js
+++ src/store/catalog/index.js
@@ -16,6 +16,7 @@ class CatalogState extends StoreModule {
         limit: 10,
         sort: 'order',
         query: '',
+        category: '', // параметр категории
       },
       count: 0,
       waiting: false,
@@ -36,6 +37,7 @@ class CatalogState extends StoreModule {
       validParams.limit = Math.min(Number(urlParams.get('limit')) || 10, 50);
     if (urlParams.has('sort')) validParams.sort = urlParams.get('sort');
     if (urlParams.has('query')) validParams.query = urlParams.get('query');
+    if (urlParams.has('category')) validParams.category = urlParams.get('category'); // категории
     await this.setParams({ ...this.initState().params, ...validParams, ...newParams }, true);
   }
 
@@ -46,7 +48,7 @@ class CatalogState extends StoreModule {
    */
   async resetParams(newParams = {}) {
     // Итоговые параметры из начальных, из URL и из переданных явно
-    const params = { ...this.initState().params, ...newParams };
+    const params = { ...this.initState().params, ...newParams, category: '' }; //добавлен сброс категории
     // Установка параметров и загрузка данных
     await this.setParams(params);
   }
@@ -86,6 +88,10 @@ class CatalogState extends StoreModule {
       sort: params.sort,
       'search[query]': params.query,
     };
+
+    if (params.category) {
+      apiParams['search[category]'] = params.category;
+    }
 
     const response = await fetch(`/api/v1/articles?${new URLSearchParams(apiParams)}`);
     const json = await response.json();
---
+++ src/store/categories/index.js
@@ -0,0 +1,34 @@
+import StoreModule from '../module';
+
+class CategoriesState extends StoreModule {
+  initState() {
+    return {
+      list: [],
+      waiting: false,
+    };
+  }
+
+  async load() {
+    this.setState({
+      ...this.getState(),
+      waiting: true,
+    });
+
+    try {
+      const response = await fetch('/api/v1/categories?fields=_id,title,parent(_id)&limit=*');
+      const json = await response.json();
+      
+      this.setState({
+        list: json.result.items,
+        waiting: false,
+      }, 'Категории загружены');
+    } catch (e) {
+      this.setState({
+        list: [],
+        waiting: false,
+      });
+    }
+  }
+}
+
+export default CategoriesState;
