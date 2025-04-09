1d2b11c4c06726a9c21da9807b59f02e107a04a6
Ilya Pridanov <ik.front2558988@gmail.com>
2025-04-09 10:33:32 +0300

    LanguageContext

---
+++ src/locales.js
@@ -0,0 +1,24 @@
+export const translations = {
+  ru: {
+    title: "Магазин",
+    addToCart: "Добавить",
+    cart: "Корзина",
+    product: "товар",
+    main: "Главная",
+    itemSumToggleText: "Выберите количество товаров на одной странице списка",
+    empty: "пусто",
+    delete: "Удалить",
+    total: "Итого",
+  },
+  en: {
+    title: "Shop",
+    addToCart: "Add to cart",
+    cart: "Cart",
+    product: "product",
+    main: "Main",
+    itemSumToggleText: "Select the number of products on one list page",
+    empty: "empty",
+    delete: "Delete",
+    total: "Total",
+  }
+}
