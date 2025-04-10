a90e0b324d5ba387f8377df0de51f330901f3357
Kornull <u.karnialiuk@gmail.com>
2025-04-10 19:55:36 +0300

    refactor: del warning default props

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -6,11 +6,11 @@ import { numberFormat } from '../../utils';
 import Button from '../button';
 import './style.css';
 
-function Item(props) {
+function Item({ onAdd = () => {}, labelCurr = '₽', labelAdd = 'Добавить', ...props }) {
   const cn = bem('Item');
 
   const callbacks = {
-    onAdd: e => props.onAdd(props.item._id),
+    onAddItem: e => onAdd(props.item._id),
   };
 
   return (
@@ -21,9 +21,9 @@ function Item(props) {
       </div>
       <div className={cn('actions')}>
         <div className={cn('price')}>
-          {numberFormat(props.item.price)} {props.labelCurr}
+          {numberFormat(props.item.price)} {labelCurr}
         </div>
-        <Button style="primary" onClick={callbacks.onAdd} title={props.labelAdd} />
+        <Button style="primary" onClick={callbacks.onAddItem} title={labelAdd} />
       </div>
     </div>
   );
@@ -39,12 +39,6 @@ Item.propTypes = {
   onAdd: PropTypes.func,
   labelCurr: PropTypes.string,
   labelAdd: PropTypes.string,
-};
-
-Item.defaultProps = {
-  onAdd: () => {},
-  labelCurr: '₽',
-  labelAdd: 'Добавить',
 };
 
 export default memo(Item);
