a8f2eb3a0b3093f5092ffbb08b29a0eff4c2dc61
Ilya Pridanov <ik.front2558988@gmail.com>
2025-04-11 22:58:56 +0300

    feat: proile-list

---
+++ src/components/profile-list/index.js
@@ -0,0 +1,30 @@
+import { memo } from 'react';
+import PropTypes from 'prop-types';
+import { cn as bem } from '@bem-react/classname';
+import useSelector from '../../hooks/use-selector';
+import Button from '../../components/button';
+import Input from '../../components/input';
+import './style.css';
+
+function ProfileList({ children }) {
+  const cn = bem('ProfileList');
+  return (
+    <section className={cn()}>
+      <h2 className={cn("caption")}>Профиль</h2>
+      <dl className={cn("list")}>
+        <dt>Имя:</dt>
+        <dd>User 1</dd>
+        <dt>Телефон:</dt>
+        <dd>+70000000001</dd>
+        <dt>Email:</dt>
+        <dd>test_50@example.com</dd>
+      </dl>
+    </section>
+  );
+}
+
+ProfileList.propTypes = {
+  children: PropTypes.node,
+};
+
+export default memo(ProfileList);
