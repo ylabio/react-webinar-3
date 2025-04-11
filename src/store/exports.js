214ce27ab7b590770982b9cc50b9bf4060c4c209
Pawlord <Pavel23082002@mail.ru>
2025-04-11 23:17:34 +0300

    deleted profile class

--- src/store/exports.js
+++ src/store/exports.js
@@ -4,4 +4,3 @@ export { default as modals } from './modals';
 export { default as article } from './article';
 export { default as locale } from './locale';
 export { default as auth } from './auth';
-export { default as profile } from './profile';
