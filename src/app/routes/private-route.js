d360671e2b9d69b551f02f578c63b13247359daf
Kornull <u.karnialiuk@gmail.com>
2025-04-12 08:25:41 +0300

    fix: route

--- src/app/routes/private-route.js
+++ src/app/routes/private-route.js
@@ -5,7 +5,7 @@ import useSelector from "../../hooks/use-selector";
 const PrivateRoute = () => {
   const isAuthorized = useSelector((state) => state.user.isAuth);
 
-  return isAuthorized ? <Outlet /> : <Navigate replace to="/" />;
+  return isAuthorized ? <Outlet /> : <Navigate replace to="/login" />;
 };
 
 export default PrivateRoute;
