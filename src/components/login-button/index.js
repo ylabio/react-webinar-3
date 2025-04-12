a152fdf574e95bec45448f307ae85fa55b833691
Ilya Pridanov <ik.front2558988@gmail.com>
2025-04-12 22:33:25 +0300

    fix: login-design

--- src/components/login-button/index.js
+++ src/components/login-button/index.js
@@ -1,6 +1,5 @@
 import { memo } from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import useTranslate from '../../hooks/use-translate';
 import useSelector from '../../hooks/use-selector';
 import useStore from '../../hooks/use-store';
@@ -13,16 +12,18 @@ function LoginButton() {
 
   return (
     <div className="LoginButton">
-      {user ? (
-        <>
-          <Link to="/profile">{user.profile.name}</Link>
-          <button onClick={() => store.actions.auth.signOut()}>
-            {t('head.exit')}
-          </button>
-        </>
-      ) : (
-        <Link to="/login">{t('head.entry')}</Link>
-      )}
+      <div className="LoginButton-container">
+        {user ? (
+          <>
+            <Link to="/profile">{user.profile.name}</Link>
+            <button onClick={() => store.actions.auth.signOut()}>
+              {t('head.exit')}
+            </button>
+          </>
+        ) : (
+          <Link to="/login">{t('head.entry')}</Link>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/login-form/index.js
+++ src/components/login-form/index.js
@@ -8,6 +8,7 @@ import Input from '../../components/input';
 import './style.css';
 
 function LoginForm() {
+
   const cn = bem('LoginForm');
   const store = useStore();
   const { issues, waiting } = useSelector(state => state.auth);
@@ -21,14 +22,18 @@ function LoginForm() {
   return (
     <form onSubmit={handleSubmit} className={cn()} >
       <h2 className={cn("caption")}>Вход</h2>
-      <Input value={login} onChange={setLogin} placeholder="Логин" delay={1000}
-        theme={'small'} />
-      <Input value={password} onChange={setPassword} type="password" delay={1000}
-        theme={'small'} />
+      <label>
+        <span>Логин</span>
+        <Input value={login} onChange={setLogin} type="text" placeholder="Введите логин" delay={1000}
+          theme={'small'} />
+      </label>
+      <label>
+      <span>Пароль</span>
+        <Input value={password} onChange={setPassword} type="password" placeholder="Введите пароль" delay={1000}
+          theme={'small'} />
+      </label>
       {issues && <div className={cn("error")}>{issues}</div>}
-      <Button type="submit" disabled={waiting} style="primary">
-        {waiting ? 'Загрузка...' : 'Войти'}
-      </Button>
+      <Button type="submit" disabled={waiting} style="primary" title={waiting ? 'Загрузка...' : 'Войти'}/>
     </form>
   );
 }
