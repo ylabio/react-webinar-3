345ae81c80e68f6c6f286d8961419bd36e790974
Kornull <u.karnialiuk@gmail.com>
2025-04-13 10:18:39 +0300

    refactor routes

--- src/app/routes/private-route.js
+++ src/app/routes/private-route.js
@@ -1,26 +1,11 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 import useSelector from '../../hooks/use-selector';
 
-import { LOCAL_USER_KEY } from '../../constants';
+const PrivateRoute = ({ Component }) => {
+  const isAuthorized = useSelector(state => state.user.isAuth);
 
-const PrivateRoute = () => {
-  const select = useSelector(state => ({
-    isAuthorized: state.user.isAuth,
-    isUserLeaved: state.user.isUserLeave,
-  }));
-
-  const userLocaleData = JSON.parse(localStorage.getItem(LOCAL_USER_KEY));
-
-  if (userLocaleData && userLocaleData.token.length > 0) {
-    return <Outlet />;
-  }
-
-  if (select.isUserLeaved) {
-    return <Navigate replace to="/" />;
-  }
-
-  return select.isAuthorized ? <Outlet /> : <Navigate replace to="/login" />;
+  return isAuthorized ? <Component /> : <Navigate replace to="/login" />;
 };
 
 export default PrivateRoute;
--- src/app/routes/public-route.js
+++ src/app/routes/public-route.js
@@ -1,11 +1,11 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate  } from 'react-router-dom';
 
 import useSelector from '../../hooks/use-selector';
 
-const PublicRoute = () => {
+const PublicRoute = ({Component}) => {
   const isAuthorized = useSelector(state => state.user.isAuth);
 
-  return isAuthorized ? <Navigate replace to="/" /> : <Outlet />;
+  return isAuthorized ? <Navigate replace to="/" /> : <Component />;
 };
 
 export default PublicRoute;
