7529ba5615a9bede0b0407deb000534ebeb05aac
dinar1122 <gert.olh@mail.ru>
2025-04-13 05:11:31 +0300

    удалил ненужную логику в профиле

--- src/app/profile/index.js
+++ src/app/profile/index.js
@@ -10,34 +10,21 @@ import ProfileInfo from '../../components/profile-info';
 
 import AuthBar from '../../components/auth-bar';
 import useAuth from '../../hooks/use-auth';
-import useStore from '../../hooks/use-store';
 
 /**
  * Страница профиля
  */
-import { useEffect } from 'react';
 
 function Profile() {
-  const store = useStore();
   const { t } = useTranslate();
 
-  const { token } = useSelector(state => state.user);
-const memoizedToken = useMemo(() => token, [token]);
-  
   const { isAuth, loading, profileData } = useSelector(state => ({
     isAuth: state.user.isAuth,
     loading: state.profile.loading,
-    profileData: state.profile.data || state.user.user
+    profileData: state.profile.data || state.user.user,
   }));
-  
 
   useAuth();
-
-  useEffect(() => {
-    if (isAuth && token) {
-      store.actions.profile.loadProfile(token);
-    }
-  }, [ memoizedToken]);
 
   if (!isAuth) return null;
 
@@ -48,11 +35,7 @@ const memoizedToken = useMemo(() => token, [token]);
       </Head>
       <PageLayout>
         <Navigation />
-        {loading ? (
-          <div>Загрузка...</div>
-        ) : (
-          <ProfileInfo user={profileData} t={t} />
-        )}
+        {loading ? <div>Загрузка...</div> : <ProfileInfo user={profileData} t={t} />}
       </PageLayout>
     </>
   );
