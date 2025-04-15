73105de90d5bdf20b16442e0a09a444485ed7b2f
Korotkov <149740711+Dgerimus@users.noreply.github.com>
2025-04-15 17:02:13 +0300

    -Исправил страницу логина по дизайну

--- src/app/login/index.js
+++ src/app/login/index.js
@@ -51,48 +51,54 @@ function Login() {
       </Head>
       <PageLayout>
         <Navigation />
-        <h2 className="login-head">{t('login.title')}</h2>
-        <form className="login-from" onSubmit={onSubmit}>
-          <div>
-            <p>{t('login.login')}</p>
-            <Input
-              name="login"
-              noDebounce={true}
-              value={form.login}
-              onChange={value => handleInputChange(value, 'login')}
-              placeholder={t('login.loginPlaceholder') || 'Login'}
-              theme="small"
+        <h2 className="login__head">{t('login.title')}</h2>
+        <form className="login" onSubmit={onSubmit}>
+          <div className="login__fieldset">
+            <div className="login__field">
+              <p>{t('login.login')}</p>
+              <Input
+                name="login"
+                noDebounce={true}
+                value={form.login}
+                onChange={value => handleInputChange(value, 'login')}
+                placeholder={t('login.loginPlaceholder') || 'Login'}
+                theme="small"
+              />
+            </div>
+            <div className="login__field">
+              <p>{t('login.password')}</p>
+              <Input
+                name="password"
+                type="password"
+                noDebounce={true}
+                value={form.password}
+                onChange={value => handleInputChange(value, 'password')}
+                placeholder={t('login.passwordPlaceholder') || 'Password'}
+                theme="small"
+              />
+            </div>
+          </div>
+
+          {auth.error?.issues?.length > 0 && (
+            <div className="login__error">
+              {auth.error.issues.map((issue, i) => (
+                <div key={i}>{issue.message}</div>
+              ))}
+            </div>
+          )}
+
+          <div
+            className={`login__btn ${auth.error?.issues?.length > 0 ? 'login__btn_with-error' : ''}`}
+          >
+            <Button
+              type="submit"
+              title={
+                auth.loading ? t('login.loading') || 'Loading...' : t('login.button') || 'Войти'
+              }
+              style="primary"
+              disabled={auth.loading}
             />
           </div>
-          <div>
-            <p>{t('login.password')}</p>
-            <Input
-              name="password"
-              type="password"
-              noDebounce={true}
-              value={form.password}
-              onChange={value => handleInputChange(value, 'password')}
-              placeholder={t('login.passwordPlaceholder') || 'Password'}
-              theme="small"
-            />
-          </div>
-          <div>
-            {auth.error?.issues?.length > 0 && (
-              <div className="login-errors">
-                {auth.error.issues.map((issue, i) => (
-                  <div key={i} style={{ color: '#D43B3B' }}>
-                    {issue.message}
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-          <Button
-            type="submit"
-            title={auth.loading ? t('login.loading') || 'Loading...' : t('login.button') || 'Войти'}
-            style="primary"
-            disabled={auth.loading}
-          />
         </form>
       </PageLayout>
     </div>
