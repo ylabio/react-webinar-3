cb766f5cc9c0ed9bc26421c2229e961992bfc7dc
SempaiDarcy <mr_darcy_1@mail.ru>
2025-04-18 01:42:17 +0400

    feat: refactor list-to-tree function for better readability and performance

--- src/utils/list-to-tree/index.js
+++ src/utils/list-to-tree/index.js
@@ -5,31 +5,26 @@
  * @returns {Array} Корневые узлы
  */
 export default function listToTree(list, key = '_id') {
-  let trees = {};
-  let roots = {};
-  for (const item of list) {
-    // Добавление элемента в индекс узлов и создание свойства children
-    if (!trees[item[key]]) {
-      trees[item[key]] = item;
-      trees[item[key]].children = [];
-      // Ещё никто не ссылался, поэтому пока считаем корнем
-      roots[item[key]] = trees[item[key]];
-    } else {
-      trees[item[key]] = Object.assign(trees[item[key]], item);
-    }
+  const map = {};
+  const roots = [];
 
-    // Если элемент имеет родителя, то добавляем его в подчиненные родителя
-    if (item.parent?.[key]) {
-      // Если родителя ещё нет в индексе, то индекс создаётся, ведь _id родителя известен
-      if (!trees[item.parent[key]]) {
-        trees[item.parent[key]] = { children: [] };
-        roots[item.parent[key]] = trees[item.parent[key]];
-      }
-      // Добавления в подчиненные родителя
-      trees[item.parent[key]].children.push(trees[item[key]]);
-      // Так как элемент добавлен к родителю, то он уже не является корневым
-      if (roots[item[key]]) delete roots[item[key]];
+  // Индексируем комментарии по id
+  for (const item of list) {
+    if (!item[key]) continue; // защита от пустых _id
+    map[item[key]] = { ...item, children: [] };
+  }
+
+  // Формируем дерево
+  for (const item of list) {
+    const id = item[key];
+    const parentId = item.parent?.[key];
+
+    if (parentId && map[parentId]) {
+      map[parentId].children.push(map[id]);
+    } else {
+      roots.push(map[id]);
     }
   }
-  return Object.values(roots);
+
+  return roots;
 }
