f665d7d4dbae7ddb0802113ba7d88d790751294c
DogeBogi <sobakichkusakich@gmail.com>
2025-04-18 12:47:02 +0300

    Update index.js

--- src/utils/exclude/index.js
+++ src/utils/exclude/index.js
@@ -1,7 +1,7 @@
 import isPlainObject from '../is-plain-object';
 
 /**
- * Возвращает объект, в котором не будет свойств, совпадающих со вторым объектом
+ * Возвращает объект, в котором не будет свойств, совпадающих со вторым объектом 123
  * @param objectSrc {Object} Исходный объект
  * @param objectExc {Object} Объект-маска, вырезаемый из objectSrc
  * @returns {Object} Новый объект
