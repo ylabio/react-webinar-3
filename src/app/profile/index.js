8136ae6fd42a7cdce035ae1143aff05926b17224
Pawlord <Pavel23082002@mail.ru>
2025-04-20 13:21:31 +0300

    some fixes

--- src/app/profile/index.js
+++ src/app/profile/index.js
@@ -24,7 +24,7 @@ function Profile() {
     waiting: state.profile.waiting,
   }));
 
-  const { t } = useTranslate();
+  const t = useTranslate();
 
   return (
     <>
@@ -37,7 +37,7 @@ function Profile() {
       <PageLayout>
         <Navigation />
         <Spinner active={select.waiting}>
-          <ProfileCard data={select.profile} />
+          <ProfileCard data={select.profile} t={t} />
         </Spinner>
       </PageLayout>
     </>
